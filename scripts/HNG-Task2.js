1b56187caa5e2c46ab4a0b9add2760f09e137411
Nerdyxils <56302195+Nerdyxils@users.noreply.github.com>
2020-06-03 00:08:42 +0100

    Added a javascript file
    a javascript file that prints my name, id and language

---
+++ scripts/HNG-Task2.js
@@ -0,0 +1,7 @@
+var myInfo = () => {
+    var name = 'Abiodun Silas Ayomide';
+    var id = 'HNG-04406';
+    var lang = 'Javascript'
+return "Hello world! this is " + name + " with HNGi7 ID " + id + " using " + lang + ' for stage 2 task' ; 
+}
+document.write(myInfo());
