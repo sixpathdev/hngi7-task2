ebd2e02e237d1892ff2781efee97754111adc744
Adeyemi Timilehin <adeyemitimmy@gmail.com>
2020-06-03 00:03:15 +0100

    intern_script1

---
+++ scripts/intern_script1.php
@@ -0,0 +1,7 @@
+<?php
+header('Content-Type: text/event-stream');
+header('Cache-Control: no-cache');
+
+echo "Hello World, this is Adeyemi Timilehin Mike with HNGi7 ID 02251 using php for stage 2 task\n\n";
+flush();
+?>
