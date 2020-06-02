fce39d0fc95cbc38d4cac171bc3f139bd54e875f
wisdomosara <50260832+wisdomosara@users.noreply.github.com>
2020-06-03 00:34:08 +0100

    Update wisdomosaraHNG7task2.js

--- scripts/wisdomosaraHNG7task2.js
+++ scripts/wisdomosaraHNG7task2.js
@@ -1,1 +1,10 @@
 
+let fname = "Osara Wisdom";
+let id = "HNG-04550";
+let language = "JavaScript";
+
+function getGreeting(){
+  return `Hello World, this is ${fname} with HNGi7 ID ${id} using ${language} for stage 2 task`
+}
+
+getGreeting()
