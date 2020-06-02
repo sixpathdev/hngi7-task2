a52c351d265a7bbe4dcb5d692b5ef5d7eee7c6e9
AnyaDaniel <30692059+AnyaDaniel@users.noreply.github.com>
2020-06-02 23:51:03 +0100

    Update Dani_script.php

--- Dani_script.php
+++ Dani_script.php
@@ -1,5 +1,5 @@
 
 
 <?php
-echo "Hello World! I am Daniel Anya, an Intern with the HNG tech. Id: HNG-01846";
+echo "Hello World! I am Daniel Anya, an Intern with the HNG tech. Id: HNG-01846, using PHP for stage 2 task";
 ?>
