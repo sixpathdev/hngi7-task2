8cfa028388ef0f35ed35e761611a338e55bd671c
ccanyanwu <eme42c@gmail.com>
2020-06-02 20:10:11 +0100

    ccanyanwu script

---
+++ scripts/ccanyanwu_script.js
@@ -0,0 +1,14 @@
+
+ //function that returns my full details as a string 
+ const myDetails = () => {
+   //variables to hold my data 
+   const name = 'Chukwuemeka Collins Anyanwu',
+         id = 'HNG-00278',
+         language = 'JavaScript';
+         
+         //returns a string containing details 
+   return `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`;
+ }
+ 
+ //prints this output to the console 
+console.log(myDetails());
