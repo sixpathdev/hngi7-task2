dc9a2dd9f68e3e6e123e825c4e63914ca1e13c95
raph941 <raph941@gmail.com>
2020-06-02 19:40:51 +0100

    feat: added Ralph_script.py

--- scripts/Ralph_script.py
+++ scripts/Ralph_script.py
@@ -2,9 +2,8 @@ def MyFunc():
     full_name = "Ehindero Raphael Ayo"
     hng_id = "HNG-00314"
     language = "python"
-    output = f'Hello World, this is {full_name} with HNGi7 ID {hng_id} using {language} for stage 2 task'
 
-    return output
+    return f'Hello World, this is {full_name} with HNGi7 ID {hng_id} using {language} for stage 2 task'
 
 
 MyFunc()
