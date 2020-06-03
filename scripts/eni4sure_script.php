0147f04418e9686d4f64cee38a3e57e54254310b
Eniola Osabiya <eosabiya@gmail.com>
2020-06-03 16:42:40 +0100

    Added email update

--- scripts/eni4sure_script.php
+++ scripts/eni4sure_script.php
@@ -1,12 +1,6 @@
 <?php
 
-	$details = array();
-	$details['fullname'] = "Eniola Osabiya";
-	$details['id'] = "HNG-00143";
-	$details['email_address'] = "eosabiya@gmail.com";
-	$details['language_used'] = "PHP";
-
-	$message = "Hello World, this is ". $details['fullname'] ." with HNGi7 ID ". $details['id'] ." using ". $details['language_used'] ." for stage 2 task";
+	$message = "Hello World, this is Eniola Osabiya with HNGi7 ID HNG-00143 and email eosabiya@gmail.com using PHP for stage 2 task";
 
 	echo($message);
 ?>
