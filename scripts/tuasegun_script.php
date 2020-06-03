280b17874d2fe607d98a7dfd4e2160ed6b6ac8a8
Tuasegun <Tuase360@gmail.com>
2020-06-03 21:12:06 +0100

    script commit

---
+++ scripts/tuasegun_script.php
@@ -0,0 +1,8 @@
+ <?php
+$full_name = "Oguntuase Oluwasegun";
+$hng_id = "HNG-02208";
+$lang = "PHP";
+
+echo "Hello World, this is $full_name with HNGi7 ID $hng_id using  $lang  for stage 2 task";
+
+?>
