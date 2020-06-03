86b96b46c990e45b04e6dcd7e1985c7538193e17
Ademola Babs <demolafadumo@gmail.com>
2020-06-03 11:57:14 +0100

    Create Demola_script.py
    Ademola Babs
    Username - Demola
    My Script

---
+++ scripts/Demola_script.py
@@ -0,0 +1,7 @@
+name = "Ademola Babs"
+id = "HNG-01367"
+email = "demolafadumo@gmail.com"
+language = "Python"
+status = "pass"
+output = ("Hello World, this is " + name + " with HNGi7 ID " + id + " and email " +
+      email + " using Python for stage 2 task.")
