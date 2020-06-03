50e7d240d507a8af9050b3e3064ad4e888ce0295
chima2116 <ajikechima@gmail.com>
2020-06-03 23:50:56 +0100

    feat: added AjikeChimaobi.js

---
+++ AjikeChimaobi.js
@@ -0,0 +1,5 @@
+const name = "Ajike chimaobi"
+const hng_id = "HNG-O6593"
+const email = "ajikechima@gmail.com"
+
+console.log(`Hello World, this is ${name} with HNG17 ID ${hng_id} for stage 2 task`);
