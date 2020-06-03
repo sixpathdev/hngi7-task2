f151b57bfc684eeb9e9a79c645503b6188644c56
aeskay <56758277+aeskay@users.noreply.github.com>
2020-06-03 19:05:06 +0100

    Update aeskay_script.js
    This is the updated version

--- scripts/aeskay_script.js
+++ scripts/aeskay_script.js
@@ -1,2 +1,9 @@
-const aeskay =  "Hello World, this is Samuel Alalade with HNGi7 ID HNG-01192 and email alaladesambluez@gmail.com using Javascript for stage 2 task";
+const info = {
+  "name": "Samuel Alalade", 
+  "id": "HNG-01192",
+  "email": "alaladesambluez@gmail.com",
+  "language": "Javascript"
+};
+const {name, id, email, language} = info;
+const aeskay =  "Hello World, this is " + name + " with HNGi7 ID " + id + " and email " + email + " using " + language + " for stage 2 task";
 console.log(aeskay);
