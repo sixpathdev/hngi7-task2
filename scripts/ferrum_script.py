470ddbd7acf1b12041781cd5d10cf05b68878730
Alabi OluwaFeranmi <alabiemmanuelferanmi@gmail.com>
2020-06-03 12:56:10 +0100

    adding script

---
+++ scripts/ferrum_script.py
@@ -0,0 +1,12 @@
+# HNG task 1
+# team flash 
+# "Hello World, this is [full name] with HNGi7 ID [ID] using [language] for stage 2 task"
+
+name = "Alabi Feranmi"
+id = "HNG-01822"
+email = "alabiemmanuelferanmi@gmail.com"
+language = "Python"
+
+output = f"Hello World, this is {name} with HNGi7 ID {id} using {language} for stage 2 task"
+
+print(output)
