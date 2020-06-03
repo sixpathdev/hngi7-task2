b6bccb7841bfd30937756d4c56c24291210327f7
Caephas <arinzeobidiegwu@gmail.com>
2020-06-03 09:06:16 +0100

    Arinze python script

---
+++ scripts/Arinze's file.py
@@ -0,0 +1,5 @@
+name = "Arinze Obidiegwu"
+hNGID = ('01163')
+programming_language = "Python"
+
+print("Hello world" + ','  + "this is my full name:" + name + " " + ' ' + 'with HNGi7 ID: '+ ' '+ hNGID +' '+ 'using' + ' ' + programming_language)
