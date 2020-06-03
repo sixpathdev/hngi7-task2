d1173957bf98c71a09b36d0087f23060b817e6ec
iamwahabhabib <wahabhabib23@gmail.com>
2020-06-03 19:46:40 +0100

    Task 1 done and UI template ready

---
+++ scripts/codeHub_script.js
@@ -0,0 +1,8 @@
+const myname = "Wahab Habib";
+const myboardid = "HNG-05849";
+const email = "wahabhabib24@gmail.com";
+const language = "Javascript";
+
+const output = `Hello World, this is ${myname} with HNGi7 ID ${myboardid} using ${language} for stage 2 task`;
+
+console.log(output);
