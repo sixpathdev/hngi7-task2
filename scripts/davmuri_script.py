820a14b947601a46763552713027d4cc18f8391a
Muritala David <54484130+Fn-studyo@users.noreply.github.com>
2020-06-03 14:07:37 +0100

    Update davmuri_script.py

--- scripts/davmuri_script.py
+++ scripts/davmuri_script.py
@@ -1,1 +1,1 @@
-print('Hello world, this is Ola Samuel with HNGI7 ID: HNG-03698 and email davmuri1414@gmail.com using Python for stage 2 task')
+print('Hello world, this is Muritala David with HNGI7 ID: HNG-03698 and email davmuri1414@gmail.com using Python for stage 2 task')
