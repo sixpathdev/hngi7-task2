52e1665938814a2d01af8d5a95242371f671cdf0
E-STAT <owojori.tolulope@gmail.com>
2020-06-03 08:20:06 +0100

    Ernest task script with python

---
+++ scripts/Ernest.py
@@ -0,0 +1,8 @@
+def task(Name, hng_id, language):
+    output = "Hello World this is {} with HNGi7 ID {} using {} for stage 2 task".format(Name, hng_id,language)
+    return output
+
+
+print(task(Name = "Owojori Ernest Tolulope", hng_id = "HNG-01824", language="Python"))
+
+    
