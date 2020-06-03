a63d8172fca403571ba78b8b2d417fc99d680a4c
Joshua Odejide <48139470+joshuaodejide@users.noreply.github.com>
2020-06-03 14:16:32 +0100

    Create Jay-Dev-intern.php

---
+++ Jay-Dev-intern.php
@@ -0,0 +1,6 @@
+<?php
+$internname = "Joshua Odejide";
+$internID = "HNG-02291";
+$internmail ="joshuaodejidme@gmail.com";
+echo "Hello World, this is " .$internname . " with HNGi7 ID " .$internID . "using PHP for stage 2 task";
+?>
