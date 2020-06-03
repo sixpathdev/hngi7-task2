034f1782ce93409a1f48e8a773f6083b55f010d8
Taiwo Oyewole <oyewoletaiwo94@gmail.com>
2020-06-03 14:44:38 +0100

    feat: Added 00365_taiwo_script.js

---
+++ scripts/00365_taiwo_script.js
@@ -0,0 +1,6 @@
+let name = 'Taiwo Oyewole';
+let id = 'HNG-00365';
+let language = 'JavaScript';
+let email_address = 'oyewoletaiwo94@gmail.com';
+let output = `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task ${email_address}`;
+console.log(output);
