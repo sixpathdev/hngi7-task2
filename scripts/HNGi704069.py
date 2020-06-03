4113314936730f1b9c91c1604afc6c6af4693192
CrazyChickenDev <dannychukz15@gmail.com>
2020-06-03 20:38:34 +0100

    fixed email

--- scripts/HNGi704069.py
+++ scripts/HNGi704069.py
@@ -2,7 +2,7 @@
 #TEAM FLASH
 
 def hngi7stage_1():
-    print("Hello World, this is Nwaobi Daniel with HNGi7 ID HNG-04069 using Python for stage 2 task")
+    print("Hello World, this is Nwaobi Daniel with HNGi7 ID HNG-04069 and email dannychukz15@gmail.com using Python for stage 2 task")
 
 if __name__ == '__main__':
     hngi7stage_1()
