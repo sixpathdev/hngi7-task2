1d4b2589ded9603bc6966443260926b3060d84cb
Jimoh Lawal Olafimimihan <dmainman4sucess@gmail.com>
2020-06-03 19:28:33 +0100

    Update cyberphym707_script.py

--- scripts/cyberphym707_script.py
+++ scripts/cyberphym707_script.py
@@ -1,7 +1,7 @@
-full_name = "Jimoh Lawal"
-id = "HNG-01277"
+name = "Jimoh Lawal"
+hng_id = "HNG-01277"
 email = "dmainman4sucess@gmail.com"
 language = "Python"
 
 #output
-print("Hello World, this is " + full_name + " with HNGi7 ID " + id + " using " + language + " for stage 2 task. ")
+print("Hello World, this is " + name + " with HNGi7 ID " + hng_id + " using " + language + " for stage 2 task. ")
