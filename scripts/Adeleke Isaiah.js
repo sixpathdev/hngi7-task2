f0f3b518d538d6627456464c88aa9e75a04b78dd
Adeleke Isaiah <iadeleke01@gmail.com>
2020-06-03 13:15:09 +0100

    Add files via upload

---
+++ scripts/Adeleke Isaiah.js
@@ -0,0 +1,11 @@
+const fullName = "Adeleke Isaiah";
+const ID = "HNG-00931";
+const email = "iadeleke01@gmail.com";
+const language = "Javascript";
+const status = "Pass";
+
+function intro() {
+    return `Hello world, this is ${fullName }, with  HNGi7 ID: ${ID} and Email: ${email} using ${language } for Stage 2 task`
+}
+
+console.log(intro())
