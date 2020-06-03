36977415659be7c8fd4dd67bc65be646dbf761cb
Stein-Hakase <62748317+Stein-Hakase@users.noreply.github.com>
2020-06-03 23:03:10 +0700

    Changing document.write to console.log()

--- scripts/Schnitter-script.js
+++ scripts/Schnitter-script.js
@@ -1,1 +1,1 @@
-document.write("Hello World, this is Fakhreddine Hariz with HNGi7 ID ID HNG-03410 using JavaScript for stage 2 task")
+console.log("Hello World, this is Fakhreddine Hariz with HNGi7 ID ID HNG-03410 using JavaScript for stage 2 task")
