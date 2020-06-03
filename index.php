282c789ada2ab7b8f5b5c42543506bd2b7349c73
Muhideen Mujeeb Adeoye <41507005+Mujhtech@users.noreply.github.com>
2020-06-03 16:40:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
- echo "Hello World, this is Muhideen Mujeeb Adeoye with HNGi7 ID HNG-05967 using PHP for stage 2 task";
+$name = "Muhideen Mujeeb Adeoye";
+$email = "mujeeb.muhideen@gmail.com";
+$language = "PHP";
+ echo "Hello World, this is Muhideen Mujeeb Adeoye with HNGi7 ID HNG-05967 and email mujeeb.muhideen@gmail.com using PHP for stage 2 task";
 ?>
