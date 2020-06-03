fdff5bae45f2482554420d857e74514067c718b9
Michaelzy27 <55640053+Michaelzy27@users.noreply.github.com>
2020-06-03 13:38:15 +0100

    Create michaelzy_script.php

---
+++ scripts/michaelzy_script.php
@@ -0,0 +1,11 @@
+<?php
+$myname = "Umeh Nnaemeka Michael";
+
+$hngid = "HNG-02364";
+
+$email = "umehmichael27@gmail.com";
+
+$language = "php";
+
+print "Hello World, this is $myname with HNGi7 ID $hngid using $language for stage 2 task";
+?>
