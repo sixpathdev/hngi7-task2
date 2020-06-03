178509133806b284d3d132b5f8e296d4b940f1d9
Nsikak Inyangabia <nsikanny@gmail.com>
2020-06-03 13:44:20 +0100

    my php script for task 2

---
+++ scripts/nsikanny_script.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1> my first PHP page </h1>
+
+<?php
+$txt = "Hello World"
+$name = "Nsikak Inyangabia"
+$id = "HNG-03848"
+$LANG = "PHP"
+$email = "nsikanny@gmail.com"
+
+echo $txt. "This is" $name. "with HNGi7 ID" $id. "using" $LANG. "for stage 2 task";
+?>
