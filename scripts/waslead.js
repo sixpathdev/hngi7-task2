4e0b595ddebe1450b274be52021ae00fcd1b9911
was-coder <wasleadwears4u2c@gmail.com>
2020-06-03 21:24:27 +0400

    move email up a variable

--- scripts/waslead.js
+++ scripts/waslead.js
@@ -1,6 +1,6 @@
 let name = "ADELAKUN WASIU OLAIYA";
 let id = "HNG-00057";
-let language = "JAVASCRIPT";
 let email = "wasleadwears4u2c@gmail.com";
+let language = "JAVASCRIPT";
 
 console.log ("Hello World, this is " + name + " with HNGi7 ID " + id + " using " + language + " for stage 2 task.");
