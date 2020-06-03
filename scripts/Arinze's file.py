c1e1cdfc857ad60bfd5e1df8d7f7a01061f50c96
Caephas <arinzeobidiegwu@gmail.com>
2020-06-03 10:06:00 +0100

    Arinze added email address

--- scripts/Arinze's file.py
+++ scripts/Arinze's file.py
@@ -1,5 +1,6 @@
 name = "Arinze Obidiegwu"
 hNGID = ('HNG-01163')
 programming_language = "Python"
+email_address = 'arinzeobidiegwu@gmail.com'
 
-print("Hello world" + ','  + "this is my full name:" + name + " " + ' ' + 'with HNGi7 ID: '+ ' '+ hNGID +' '+ 'using' + ' ' + programming_language + ' ' + 'for stage 2 task')
+print("Hello world" + ','  + "this is my full name:" + name + " " + ' ' + 'with HNGi7 ID: '+ ' '+ hNGID +' '+ 'using' + ' ' + programming_language + ' ' + 'for stage 2 task' + ' ' + 'my email address is: ' + email_address)
