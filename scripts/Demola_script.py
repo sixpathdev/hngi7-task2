8f24164530d5d9cefa9a0d1a2d2d76ee2a68b2fb
Ademola Babs <48495111+demolaf@users.noreply.github.com>
2020-06-03 17:23:08 +0100

    Update Demola_script.py

--- scripts/Demola_script.py
+++ scripts/Demola_script.py
@@ -3,10 +3,8 @@ id = "HNG-01367"
 email = "demolafadumo@gmail.com"
 language = "Python"
 
-def myfunc():
-      print("Hello World, this is " + name + " with HNGi7 ID " + id + " and email " +
-            email + " using Python for stage 2 task.")
+print("Hello World, this is " + name + " with HNGi7 ID " + id + " using " + language + " for stage 2 task." + email)
 
      
-myfunc()
+
 
