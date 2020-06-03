3bd66fe9627129cf8e6e870cce263dd7fb7f9446
annonBytes@gmail.com <beinmonyudaniel@gmail.com>
2020-06-03 17:30:43 +0100

    Added the script file and output my details

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+const intern = {
+    fullName: 'Ockiya Beinmonyu Daniel',
+    language: 'javascript',
+    id: 'HNG-05057',
+    email: 'annonbytes@gmail',
+}
+
+console.log(`Hello world, this is ${intern.fullName} with HNGi7 ID ${intern.id} using ${intern.language} for stage 2 task`);
