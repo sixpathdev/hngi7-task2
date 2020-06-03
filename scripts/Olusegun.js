b003a621877c77ec451827e7fcc860a14aa3088c
Olusegun1 <66229581+Olusegun1@users.noreply.github.com>
2020-06-03 17:42:53 +0100

    Add files via upload

---
+++ scripts/Olusegun.js
@@ -0,0 +1,14 @@
+const name = "Olusegun Stephen Daramola";
+const id = "HNG-03217";
+const email = "olusegundaramola900@yahoo.com";
+const language = "javascript";
+console.log(
+  `Hello World,this is
+    ${name}
+     with HNGi7 ID
+    ${id} 
+    
+     I'm using 
+    ${language} 
+     for stage 2 task`
+);
