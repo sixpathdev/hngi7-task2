0d86933073d7ae2c33fbb9fbd5558adb37c640da
wisdomosara <50260832+wisdomosara@users.noreply.github.com>
2020-06-03 07:29:12 +0100

    Update wisdomosaraHNG7task2.js

--- scripts/wisdomosaraHNG7task2.js
+++ scripts/wisdomosaraHNG7task2.js
@@ -1,10 +1,18 @@
 
+
+  
+//initializing important variables
 let fname = "Osara Wisdom";
 let id = "HNG-04550";
 let language = "JavaScript";
 
+//defining the function
 function getGreeting(){
-  return `Hello World, this is ${fname} with HNGi7 ID ${id} using ${language} for stage 2 task`
+  return `Hello World, this is ${fname} with HNGi7 ID ${id} using ${language} for stage 2 task.`
 }
 
+//calling the function incase index.php cant read the document.write
 getGreeting()
+
+//outputing to the browser
+document.write(getGreeting())
