0df42e543681427c5f264a5756708fcd43e8f875
olaidegbeyide <toungbeyide9@gmail.com>
2020-06-03 20:11:28 +0100

    new codes added

---
+++ scripts/olaide_myscript.js
@@ -0,0 +1,12 @@
+var fName = 'Olaide'
+var lName = 'gbeyide'
+var id =  'HNG-01638'
+var language = 'JavaScript'
+var fullName = fName + lName
+
+function myMessage() {
+    return `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task`
+}
+var message = myMessage()
+document.write(message)
+
