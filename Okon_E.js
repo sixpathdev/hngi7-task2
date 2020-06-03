035dc42ae2e115a0ceaa3649e588954188ef87cc
Constantino <okonemmanuelmma@gmail.com>
2020-06-03 23:50:05 +0100

    Okon Emmanuels commit

---
+++ Okon_E.js
@@ -0,0 +1,6 @@
+const name = 'Okon Emmanuel'
+const id = 'HNG-05309'
+const email = 'okonemmanuelmma@gmail.com'
+const language = 'javascript'
+
+console.log(`Hello world, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage2 task`)
