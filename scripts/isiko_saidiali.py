8536458fd32197af7a94dca4a92a61b475b171d1
Isiko saidiali <isikosaidiali@gmail.com>
2020-06-03 18:23:03 +0300

    added my script

---
+++ scripts/isiko_saidiali.py
@@ -0,0 +1,11 @@
+import sys
+
+# email variable
+email = 'isikosaidiali@gmail.com'
+
+def my_info():
+    '''function returns my information'''
+    print('Hello world, this is Isiko Saidiali with HNGi7 ID HNG-01856 using Python for stage 2 task', flush=True)
+
+my_info()
+sys.stdout.flush()
