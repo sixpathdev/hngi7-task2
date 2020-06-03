9de99a0276b6e6a46ae9a7abf9be35227ab20c69
Ayoade Oluwafunmisia <ayoadefunmi59@gmail.com>
2020-06-03 18:59:07 +0100

    Update misi_script1.php

--- scripts/misi_script1.php
+++ scripts/misi_script1.php
@@ -1,9 +1,7 @@
 <?php
-$surname = "Ayoade";
-$firstname ="Oluwafunmisi";
-$fullname = $surname ." ".$firstname;
+$name = "Ayoade"." "."Oluwafunmisi";
 $hngid = "HNG-01173";
 $email = "ayoadefunmi59@gmail.com";
 $language = "PHP";
-echo "Hello World, this is ".$fullname."  with HNGi7 ID ".$hngid." using ".$language." for stage 2 task";
+echo "Hello World, this is ".$name."  with HNGi7 ID ".$hngid." using ".$language." for stage 2 task";
 ?>
