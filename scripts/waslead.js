f049dedab4e15e7e4b47275b706873a8e1ccd5c2
was-coder <wasleadwears4u2c@gmail.com>
2020-06-03 09:25:40 +0400

    made some new changes to the output

--- scripts/waslead.js
+++ scripts/waslead.js
@@ -1,5 +1,6 @@
 let name = "ADELAKUN WASIU OLAIYA";
 let id = "HNG-00057";
 let language = "JAVASCRIPT";
+let email = "wasleadwears4u2c@gmail.com";
 
-document.write ("Hello World!, this is " + name + " with HNGi7 ID of " + id + " using " + language);
+document.write ("Hello World, this is " + name + " with HNGi7 ID " + id + " using " + language + " for stage 2 task.");
