cceaa61cf2b510cdf3d08452ff7252e68cc85b8a
[d-Skillz] <[blessedmadukoma@gmail.com]>
2020-06-03 17:26:17 +0100

    feat: added blessedmadukoma_script.py

---
+++ scripts/blessedmadukoma_script.py
@@ -0,0 +1,6 @@
+name = "Blessed Madukoma"
+hngi7_ID = "HNG-06453"
+language = "Python"
+email = "blessedmadukoma@gmail.com"
+
+print("Hello World, this is " + name + " with HNGi7 ID "+ hngi7_ID + " using "+ language +" for stage 2 task")
