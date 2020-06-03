6b202783d7ea489dd96189f666ee26f857f84020
Jaymahn07 <58877982+Jaymahn07@users.noreply.github.com>
2020-06-03 22:30:20 +0100

    Delete Joshua2.o.py

--- scripts/Joshua2.o.py
+++
@@ -1,7 +0,0 @@
-import json
-Full_name = 'Joshua Pereira'
-HNGi7_Id = 'HNG-04334'
-EmailAddress = 'bolapereira57@gmail.com'
-Language = 'Python'
-print(json.dumps(" Hello World " + " This is " + Full_name + " with " + HNGi7_Id + " and Email "+ EmailAddress +" Using " + Language + " For Stage 2 Task "))
-
