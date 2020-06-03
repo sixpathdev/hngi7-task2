9bad392c3cb14ab3f7b69544561c29504b7530b0
Abere Fejiro <aberefejiro@gmail.com>
2020-06-03 18:52:07 +0100

    Some changes

--- scripts/fejiroabere_script.php
+++ scripts/fejiroabere_script.php
@@ -1,17 +1,21 @@
 <?php
   //My first PHP code
   /**
-     * variable fname is my full name
+     * variable name is my full name
      * variable ID is my HNG internship ID
-     * variable language is the language I used to write this code
      * varaible email is my email address
+     * variable language is the language I used to write this code
+     * 
      */
+    $name = "Abere Oghenefejiro Stephanie";
+    $ID = "HNG-02591";
     $email = "aberefejiro@gmail.com";
+    $language = "PHP";
 
-    function intro($fname, $ID, $language) {
+    function intro($name, $ID, $language) {
 
       //This is the string that would be displayed on the screen
-      return ("Hello World, this is $fname with HNGi7 ID $ID using $language for stage 2 task.");
+      return ("Hello World, this is $name with HNGi7 ID $ID using $language for stage 2 task.");
 
     }
     
