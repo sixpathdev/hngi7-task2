4c4639c1ae30e7f330c793ab9c9ac2f36905d64b
Oguh Kelechi <kelechi.oguh505@gmail.com>
2020-06-03 15:06:28 +0100

    added kaycee_script.py

---
+++ scripts/kaycee_script.py
@@ -0,0 +1,10 @@
+def myOutput():
+    fullname = "Oguh Kelechi Henry"
+    ID = "HNG-03565"
+    language = "Python"
+    email = "Kelechi.oguh505@gmil.com"
+
+    output = "Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task".format(fullname, ID, language)
+    return output
+
+print(myOutput())
