2a88b82d0743bbb5e800e6860d90b3eb26ebb0e4
Adeyemi Timilehin <adeyemitimmy@gmail.com>
2020-06-03 10:47:13 +0100

    Adeyemi_script1
    Modify my php file

--- scripts/AdeyemiTimilehin_script1.php
+++ scripts/AdeyemiTimilehin_script1.php
@@ -1,6 +1,14 @@
 <?php
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
+$myObj->name = "Adeyemi Timilehin";
+$myObj->ID = 02251;
+$myObj->language = "php";
+$myObj->Email="adeyemitimmy@gmail.com"
+  
+$myJSON = json_encode($myObj);
+
+echo $myJSON;
 
 echo "Hello World, this is Adeyemi Timilehin Mike with HNGi7 ID 02251 using php for stage 2 task\n\n";
 flush();
