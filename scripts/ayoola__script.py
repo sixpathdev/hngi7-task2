da90aab8b1517289b8af83c2f1e0f3d113894634
Olaleye Ayoola <52134849+olaleyeayoola@users.noreply.github.com>
2020-06-03 14:13:22 +0100

    Update ayoola__script.py

--- scripts/ayoola__script.py
+++ scripts/ayoola__script.py
@@ -4,7 +4,7 @@ def main():
     language = 'python'
     email = 'olaleyeayoola5@gmail.com'
     output = f'Hello World, this is {full_name} with HNGi7 ID {ID} using {language} for stage 2 task'
-    return(output)
+    return output
 
 
 if __name__ == "__main__":
