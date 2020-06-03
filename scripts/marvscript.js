b1b081559930daebb44ffd3e6d1bf0bd6607e27b
Afolabi Marvelous Sunday <afosmarvel@gmail.com>
2020-06-03 09:56:28 -0700

    Update marvscript.js

--- scripts/marvscript.js
+++ scripts/marvscript.js
@@ -3,5 +3,5 @@ const id = 'HNG-01058';
 const email = 'afosmarvel@gmail.com'
 const language = 'javascript'
 
-document.write("Hello World, this is " + name + " with HNGi7 ID " + id + 
+console.log("Hello World, this is " + name + " with HNGi7 ID " + id + 
 " using " + language  + " for stage 2 task")
