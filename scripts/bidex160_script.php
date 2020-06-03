f2e96259678f3a8b7d2fa08a86f2946f284f8ae3
Tajudeen Wariz <thabidemi@gmail.com>
2020-06-03 20:26:22 +0100

    Update bidex160_script.php

--- scripts/bidex160_script.php
+++ scripts/bidex160_script.php
@@ -2,8 +2,9 @@
 
 $name_var = "Tajudeen Wariz";
 $id_var = "HNG-01680";
-$language_var = "php";
 $email_var = "thabidemi@gmail.com";
+$language_var = "php";
+
 
 print "Hello World, this is $name_var with HNGi7 ID $id_var using $language_var for stage 2 task ";
 
