867313343d163740a947fac832fb153cfac9375c
Xemicolon <1vibenation@gmail.com>
2020-06-03 09:39:07 +0100

    Xemicolon's js script for task 2

--- scripts/xemicolon.js
+++ scripts/xemicolon.js
@@ -1,5 +1,6 @@
 function helloWorld(name, id, language) {
   let output = `Hello world, this is ${name} with HNGi7 ID ${id} using ${language}.`;
+  console.log(output);
   return document.write(output);
 }
 
