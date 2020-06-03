39b9cca8241c16d74aed16592312885b8774e2c6
Ibrahim Ouedraogo <brahim.oued@gmail.com>
2020-06-03 15:39:31 +0000

    feat: Adding my script to the team-flash project

--- scripts/ibrahimoued_script.php
+++ scripts/ibrahimoued_script.php
@@ -1,9 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IbrahimOued stage 1 task</title>
+  </head>
+  <body>
+    <?php
 
-/// Basic code, very easy to understand, hope it'll make it :)
-$id = "HNG-04112";
-$name = "Ouedraogo Ibrahim";
-$email = "brahim.oued@gmail.com";
-$language = "JavaScript";
+    /// Basic code, very easy to understand, hope it'll make it :)
+    $id = "HNG-04112";
+    $name = "Ouedraogo Ibrahim";
+    $email = "brahim.oued@gmail.com";
+    $language = "JavaScript";
 
-echo "Hello World, this is $name with HNGi7 ID $id using $language for stage 2 task";
+    echo "Hello World, this is $name with HNGi7 ID $id using $language for stage 2 task";
+
+    ?>
+  </body>
+</html>
