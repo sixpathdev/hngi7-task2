180428780a36a143a1be86cd75a9ce4dcdf53846
Xemicolon <1vibenation@gmail.com>
2020-06-03 08:37:46 +0100

    Xemicolon's Script for task 2

---
+++ scripts/xemicolon.js
@@ -0,0 +1,6 @@
+function helloWorld(name, id, language) {
+  let output = `Hello world, this is ${name} with HNGi7 ID ${id} using ${language}.`;
+  return document.write(output);
+}
+
+helloWorld("Agunloye Oluwatobiloba", "02614", "NodeJS");
