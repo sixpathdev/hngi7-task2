8a179671959a03c817d40db5f41affc2f1a82d16
Nomuoja Oghenemaro <38343496+gosteve2705@users.noreply.github.com>
2020-06-03 12:11:32 +0100

    updated output var

--- scripts/nomuoja-maro.js
+++ scripts/nomuoja-maro.js
@@ -1,11 +1,13 @@
 //jshint esversion:6
-const  fullName = "Nomuoja Oghenemaro";
+const  name = "Nomuoja Oghenemaro";
 const  hngId = "HNG-06080";
 const  language = 'Javascript';
 const email = 'nomuojamaro@gmail.com';
+var output = task(name,hngId,language,email);
 
-console.log(task(fullName,hngId,language,email));
+console.log(output);
 
-function task (fullName,hngId,language,email){
-  return `Hello World, this is ${fullName} with HNGi7 ID ${hngId} and email ${email} using ${language} for stage 2 task`;
+function task (name,hngId,language,email){
+  
+  return `Hello World, this is ${name} with HNGi7 ID ${hngId} and email ${email} using ${language} for stage 2 task`;
 }
