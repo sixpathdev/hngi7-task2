95be206aa4ba78e092de4f2dde6aae1929eb1e07
Victor Peter <ukokjnr@gmail.com>
2020-06-03 17:39:50 +0100

    added my email and changed the variable name for my name from fullName to name

--- scripts/mytest.js
+++ scripts/mytest.js
@@ -1,5 +1,6 @@
-var fullName = "Victor Peter Ukok";
+var name = "Victor Ukok";
 var hngID = "HNG-03805";
+var email = "ukokjnr@gmail.com"
 var language = "Javascript";
 
 console.log(`Hello World, this is ${fullName} with HNGi7 ID ${hngID} using ${language} for stage 2 task`)
