eeb01b5344341befd34272db2b7e6cc938fa6fa4
was-coder <wasleadwears4u2c@gmail.com>
2020-06-03 17:40:37 +0400

    made an edit to the email

--- scripts/waslead.js
+++ scripts/waslead.js
@@ -3,4 +3,4 @@ let id = "HNG-00057";
 let language = "JAVASCRIPT";
 let email = "wasleadwears4u2c@gmail.com";
 
-document.write ("Hello World, this is " + name + " with HNGi7 ID " + id + " using " + language + " for stage 2 task.");
+console.log ("Hello World, this is " + name + " with HNGi7 ID " + id + " using " + language + " for stage 2 task.");
