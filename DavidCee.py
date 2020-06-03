36d7aad96486484ed08776f5f1d81b2c0ef5953d
David-N-Cee <66029306+David-N-Cee@users.noreply.github.com>
2020-06-04 00:17:43 +0100

    Create DavidCee.py

---
+++ DavidCee.py
@@ -0,0 +1,11 @@
+#This is my HNGi7 STAGE 2 TASK
+
+name = "David, Nwachukwuyem Chukwuyem" #Write my full name
+myid = "HNG-03163" #Write my HNGi7 ID
+language = "Python" #The language I'm programming with
+email = "davidchukwuyem@gmail.com" #This is my e-mail address on HNGi7
+
+def myfunc():
+  print("Hello World, this is " + name + " with HNGi7 ID " + myid + " using " + language + " for stage 2 task")
+
+myfunc()
