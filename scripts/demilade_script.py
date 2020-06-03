bc9a47a2a0bf2cc66a81d0d274a7dfcc22147915
Demilade <demiladeoladugba@gmail.com>
2020-06-03 13:04:50 +0100

    added email

--- scripts/demilade_script.py
+++ scripts/demilade_script.py
@@ -1,2 +1,7 @@
+full_name = "Oladugba Demilade Olawale"
+ID = "HNG-00976"
+language = "Python"
+email = "demiladeoladugba@gmail.com"
+
 print("Hello World, this is {full_name} with HNGi7 ID {ID} using {language} for stage 2 task".format(
-    full_name="Oladugba Demilade Olawale", ID="HNG-00976", language="python"))
+    full_name=full_name, ID=ID, language=language))
