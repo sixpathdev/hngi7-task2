323a5adb17b4f95b7749ea25b97b4abc2e2a1d26
deola-dev <abdulwahabadeola@gmail.com>
2020-06-03 11:47:54 +0000

    adeola2058-HNG-02915 script

---
+++ scripts/HNG-02915-script.js
@@ -0,0 +1,3 @@
+let name = "Abdulwahab Adeola", id = "HNG-02915", email = "abdulwahabadeola@gmail.com", language = "javascript";
+let output = `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`;
+document.write(output);
