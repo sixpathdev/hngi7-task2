15c80bf2428019a5cf6dc74588a8845bfa6ff74e
Tomisin <ogidantomisin@gmail.com>
2020-06-03 12:48:07 +0100

    Task 2 script

---
+++ scripts/Oluwatomisin.py
@@ -0,0 +1,9 @@
+hello= 'Hello World'
+name = 'Oluwatomisin Ogidan' # My name
+myid = 'HNG-01174'  # HNG Id
+Language = 'Python' # The language i used
+email = 'feranmidareyy@gmail.com'  # my email address
+def myintroduction():
+    print(hello + ", this is "+ name + " with HNGi7 ID "+ myid + " and email "+ email +" using " + Language+ " for stage 2 task")
+
+myintroduction()
