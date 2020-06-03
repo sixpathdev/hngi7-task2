bb14dd09bee6b11ed9ab1704864c86286cb73af1
Adeola Abdulwahab <40144698+deola-dev@users.noreply.github.com>
2020-06-03 21:59:35 +0000

    Update HNG-02915-script.js

--- scripts/HNG-02915-script.js
+++ scripts/HNG-02915-script.js
@@ -1,3 +1,6 @@
-let name = "Abdulwahab Adeola", id = "HNG-02915", email = "abdulwahabadeola@gmail.com", language = "javascript";
-let output = `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`;
-document.write(output);
+const name = "Abdulwahab Adeola";
+const id = "HNG-02915";
+const email = "abdulwahabadeola@gmail.com";
+const language = "javascript";
+const output = `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`;
+console.log(output);
