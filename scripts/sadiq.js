b0a28fe78a437939e6fbfd05686e6abdf3ddd9dc
Abubakar Sambo <sadiqasg@gmail.com>
2020-06-03 12:31:07 +0100

    feat: added my script file containing my details

---
+++ scripts/sadiq.js
@@ -0,0 +1,3 @@
+const myDetails = {"name":"Abubakar Sadiq Sambo", "id":"HNG-03972", "email":"sadiqasg@gmail.com", "language":"JavaScript"};
+
+console.log(`Hello World, this is ${myDetails.name} with HNGi7 ID ${myDetails.id} using ${myDetails.language} for stage 2 task`);
