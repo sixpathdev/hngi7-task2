78f0bf0de2bfc3e41e0f04058e321279cb0ed093
E-STAT <owojori.tolulope@gmail.com>
2020-06-03 21:06:31 +0100

    Variable Name rearranged

--- scripts/Ernest.py
+++ scripts/Ernest.py
@@ -2,10 +2,12 @@ def task(Name, hng_id, language):
     output = "Hello World this is {} with HNGi7 ID {} using {} for stage 2 task".format(Name, hng_id,language)
     return output
 
-Name = "Owojori Ernest Tolulope"
-hng_id = "HNG-01824"
-language = "Python"
+Name_user = "Owojori Ernest Tolulope"
+hng_id_user = "HNG-01824"
 email = "owojori.tolulope@gmail.com"
-print(task(Name = Name, hng_id = hng_id, language=language))
+language = "Python"
+
+print(task(Name = Name_user, hng_id = hng_id_user, language=language))
+
 
     
