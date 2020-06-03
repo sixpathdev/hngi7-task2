1322438c80e662f4f0d7a6729a4ff0af17992df7
lydiane <kengnelydiane684@gmail.com>
2020-06-03 17:51:10 +0100

    Update lydiane.py

--- scripts/lydiane.py
+++ scripts/lydiane.py
@@ -1,9 +1,8 @@
 file = "lydiane.py"
-output = "Hello World, this is Massabe Lydiane with HNGi7 ID HNG-00371 and email Kengnelydiane684@gmail.com using language python for stage 2 task"
+output = "Hello World, this is Massabe Lydiane with HNGi7 ID HNG-00371 using language python for stage 2 task"
 name = "Massabe Lydiane"
-Id = "HNG-00371"
+id = "HNG-00371"
 email = "kengnelydiane684@gmail.com"
 language = "python"
-status = "Pass"
 
-print ("Hello World, this is " + name +" with HNGi7 ID "+Id + " using language " +language +" for stage 2 task")
+print ("Hello World, this is " + name +" with HNGi7 ID "+id + " using language " +language +" for stage 2 task")
