70e8ce263b43d1e18e5e7db958dd3eadd5413eb2
devvyky <okanemechibuikem@gmail.com>
2020-06-03 11:40:51 +0100

    added my script

---
+++ scripts/devvyky_script.js
@@ -0,0 +1,10 @@
+// Hello World, this is fullname with HNGi7 ID [ID]
+const fullName = "Okaneme Victor";
+const hngi7ID = 4215;
+
+introFunction = () => {
+  const intro = `Hello World, this is ${fullName} with HNGi7 ID 0${hngi7ID}`;
+  return console.log(intro);
+};
+
+introFunction();
