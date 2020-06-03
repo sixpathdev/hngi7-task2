c451acc55cd48041511961df32b04694458aabb6
raph941 <raph941@gmail.com>
2020-06-03 18:42:35 +0100

    feat: modified Ralph_script.py

--- scripts/Ralph_script.py
+++ scripts/Ralph_script.py
@@ -1,9 +1,8 @@
-def MyFunc():
-    full_name = "Ehindero Raphael Ayo"
-    hng_id = "HNG-00314"
-    language = "python"
+full_name = "Ehindero Raphael Ayo"
+hng_id = "HNG-00314"
+language = "python"
+email = "raph941@gmail.com"
 
-    return f'Hello World, this is {full_name} with HNGi7 ID {hng_id} using {language} for stage 2 task'
+output = f'Hello World, this is {full_name} with HNGi7 ID {hng_id} using {language} for stage 2 task'
 
-# output
-print(MyFunc())
+print(output)
