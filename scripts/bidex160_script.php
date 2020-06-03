f1e04c6da86b5afbac5e4b3471d19312e706fa32
horlabidemi <thabidemi@gmail.com>
2020-06-03 20:15:24 +0100

    bidex160 commit

---
+++ scripts/bidex160_script.php
@@ -0,0 +1,10 @@
+<?php
+
+$name_var = "Tajudeen Wariz";
+$id_var = "HNG-01680";
+$language_var = "php";
+$email_var = "thabidemi@gmail.com";
+
+print "Hello World, this is $name_var with HNGi7 ID $id_var using $language_var for stage 2 task ";
+
+?>
