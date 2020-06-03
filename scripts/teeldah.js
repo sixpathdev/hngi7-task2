d654c46212f3aeb5931af0ffd81f5b15c0377913
teelda <0812874+teelda@users.noreply.github.com>
2020-06-04 01:44:16 +1200

    Create teeldah.js

---
+++ scripts/teeldah.js
@@ -0,0 +1,13 @@
+const myDetails =() =>{
+	const info ={
+		fullName : "ANASHIE MATILDA IPEH",
+		HngId: "HNG-06306",
+		language: "javascript",
+		email: "matildaipeh@gmail.com",
+		status: "pass",
+	}
+
+	return 'Hello World! This is' + "" + info.fullName + "" + 'with Hngi7 ID' + info.HngId + "" + 'using' + info.language + "" + 'for stage 2 task.';
+	
+}
+ console.log(myDetails());
