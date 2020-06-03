42d8a22b824e3efea4ccbf2a473eec316ac008cc
Azuka Emeshili <49103652+zizi2shot@users.noreply.github.com>
2020-06-03 22:18:00 +0100

    updating my file

--- scripts/zizarari_script.php
+++ scripts/zizarari_script.php
@@ -1,5 +1,12 @@
 <!-- HNG task2 -->
+<!-- email variable here is for the team lead contact us -->
 
 <?php
-echo "Hello World, this is Azuka Emeshili with HNGi7 ID: HNG-05950 using php for stage 2 task.";
+$name = "Azuka Emeshili";
+$id = "HNG-05950";
+$email = "azukaemeshili@gmail.com";
+$lang = "php";
+
+
+echo "Hello World, this is . $name . with HNGi7 ID: . $id .  using . $lang . for stage 2 task.";
 ?>
