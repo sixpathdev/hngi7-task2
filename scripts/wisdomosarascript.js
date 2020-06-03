637d10263268fae6540b3853d8b52e3a5d93c12b
wisdomosara <50260832+wisdomosara@users.noreply.github.com>
2020-06-03 19:33:14 +0100

    Update wisdomosarascript.js

--- scripts/wisdomosarascript.js
+++ scripts/wisdomosarascript.js
@@ -1,17 +1,9 @@
-
-
-  
-//initializing important variables
 let name = "Osara Wisdom";
 let id = "HNG-04550";
-let language = "JavaScript";
 let email = "wisdomosara@gmail.com"
+let language = "JavaScript";
 
-//defining the function
 function getGreeting(){
   return `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task.`
 }
-
-
-//outputing to the browser
-document.write(getGreeting())
+console.log(getGreeting())
