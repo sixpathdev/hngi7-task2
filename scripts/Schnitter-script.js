94d79a93bd3fe87bd52ed60a7bea893de7dd0957
Stein-Hakase <62748317+Stein-Hakase@users.noreply.github.com>
2020-06-03 23:06:24 +0700

    Update Schnitter-script.js

--- scripts/Schnitter-script.js
+++ scripts/Schnitter-script.js
@@ -1,1 +1,1 @@
-console.log("Hello World, this is Fakhreddine Hariz with HNGi7 ID ID HNG-03410 using JavaScript for stage 2 task")
+console.log("Hello World, this is Fakhreddine Hariz with HNGi7 ID HNG-03410 using JavaScript for stage 2 task")
