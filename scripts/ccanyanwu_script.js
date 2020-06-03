6ae7dd26995e93ab2485606bd498d69874b85076
ccanyanwu <eme42c@gmail.com>
2020-06-03 17:35:58 +0100

    Followed stated format by team lead

--- scripts/ccanyanwu_script.js
+++ scripts/ccanyanwu_script.js
@@ -4,10 +4,9 @@
    const bio ={
      name : 'Chukwuemeka Collins Anyanwu',
      id : 'HNG-00278',
-     email : 'eme42c@gmail.com',
      lang : 'JavaScript'
    } 
-         
+    const email = 'eme42c@gmail.com';     
          //returns a string containing details 
    return `Hello World, this is ${bio.name} with HNGi7 ID ${bio.id} using ${bio.lang} for stage 2 task`;
  }
