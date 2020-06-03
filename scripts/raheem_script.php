4294db9032d9f640a8c3107c73863cbd579497be
inioluwa25-hub <inioluwaraheem@gmail.com>
2020-06-03 22:07:52 +0100

    raheem php script

---
+++ scripts/raheem_script.php
@@ -0,0 +1,11 @@
+<?php
+$name = "Inioluwa Raheem";
+$email ="inioluwaraheem@gmail.com";
+$language ="php";
+
+
+echo "Hello World," + name + "with HNGi7 ID" + "" + " using " + language  + " for stage 2 task";
+
+?>
+
+
