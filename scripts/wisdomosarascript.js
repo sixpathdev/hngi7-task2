7b0ebe426103cd300bae37290b490c4c2181f2bb
wisdomosara <50260832+wisdomosara@users.noreply.github.com>
2020-06-03 11:24:36 +0100

    Update and rename myscript.js to wisdomosarascript.js

--- scripts/wisdomosarascript.js
+++ scripts/wisdomosarascript.js
@@ -2,18 +2,16 @@
 
   
 //initializing important variables
-let fname = "Osara Wisdom";
+let name = "Osara Wisdom";
 let id = "HNG-04550";
 let language = "JavaScript";
 let email = "wisdomosara@gmail.com"
 
 //defining the function
 function getGreeting(){
-  return `Hello World, this is ${fname} with HNGi7 ID ${id} using ${language} for stage 2 task.`
+  return `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task.`
 }
 
-//calling the function incase index.php cant read the document.write
-getGreeting()
 
 //outputing to the browser
 document.write(getGreeting())
