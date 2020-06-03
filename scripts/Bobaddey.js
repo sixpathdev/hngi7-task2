767b4c52779c4dddb52b526022efb65530f5145c
Bobaddey <addeybob@gmail.com>
2020-06-03 12:07:42 +0000

    Bobaddey JS file

--- scripts/Bobaddey.js
+++ scripts/Bobaddey.js
@@ -2,10 +2,12 @@ const output = () =>{
     const info ={
         fullName : "Adewusi Bob Adedayo",
         hngId: "HNG-01900",
-        language: "javascript"
+        language: "javascript",
+        email: "addeybob@gmail.com",
+        status:"Pass",
     }
 
-    return 'Hello World, this is '  + info.fullName  + ' with HNG ID  ' + info.hngId  + ' using ' + info.language + ' for stage 2 task.';
+    return 'Hello World, this is '  + info.fullName  + ' with HNG ID ' + info.hngId  + ' using ' + info.language + ' for stage 2 task.';
 }
 
 console.log(output());
