7bfc2f40b06d0c2b055a46fd87bae18a5db13199
moh-x <mo.xodeeq@gmail.com>
2020-06-04 00:24:41 +0100

    xodeeq.py added by Babatunde Adenowo

---
+++ scripts/xodeeq.py
@@ -0,0 +1,6 @@
+name = "Babatunde Adenowo"
+hng_id = "HNG-01893"
+email = "mo.xodeeq@gmail.com"
+language = "Python"
+
+print("Hello World, this is {name} with HNGi7 ID {hng_id} using {language} for stage 2 task".format(name=name, hng_id=hng_id, language=language))
