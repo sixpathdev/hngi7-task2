608fdef054e9abbc75124cf19db6f34ff5eb2220
modupe-giwa <57496243+modupe-giwa@users.noreply.github.com>
2020-06-03 17:37:26 +0100

    Update modupeolagiwa-HNG-00473.js

--- scripts/modupeolagiwa-HNG-00473.js
+++ scripts/modupeolagiwa-HNG-00473.js
@@ -3,4 +3,4 @@ const Language = "JavaScript";
 const user = "00473";
 var email = "gmodupe11@gmail.com";
  
-document.write("Hello World, this is "+ name +" with HNGi7 ID HNG-"+ user +" using "+ Language +" for Stage 2 task");
+console.log("Hello World, this is "+ name +" with HNGi7 ID HNG-"+ user +" using "+ Language +" for Stage 2 task");
