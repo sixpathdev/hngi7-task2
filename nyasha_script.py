731702482d810228161ab8b49dcbea5f0c9b7bd1
Tanyah8 <66224574+Tanyah8@users.noreply.github.com>
2020-06-03 19:51:28 +0200

    Add files via upload

---
+++ nyasha_script.py
@@ -0,0 +1,6 @@
+full_name = "Nyasha Mawarire"
+id = "HNG-02775"
+language = "Python"
+email= "ntmungofa@gmail.com"
+
+print ("Hello World, this is " + full_name + " " + "with HNGi7 ID: " + id + " " + "and email "+ email + " " +"using " + language + " "+ "for stage 2 task." )
