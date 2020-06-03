70cd104e0e30828490af86ed0bb5f51688483916
Ademola Babs <48495111+demolaf@users.noreply.github.com>
2020-06-03 12:47:21 +0100

    Update Demola_script.py

--- scripts/Demola_script.py
+++ scripts/Demola_script.py
@@ -1,7 +1,12 @@
-name = "Ademola Babs"
+name = "Ademola (Fadumo) Babs"
 id = "HNG-01367"
 email = "demolafadumo@gmail.com"
 language = "Python"
-status = "pass"
-output = ("Hello World, this is " + name + " with HNGi7 ID " + id + " and email " +
-      email + " using Python for stage 2 task.")
+
+def myfunc():
+      print("Hello World, this is " + name + " with HNGi7 ID " + id + " and email " +
+            email + " using Python for stage 2 task.")
+
+     
+myfunc()
+
