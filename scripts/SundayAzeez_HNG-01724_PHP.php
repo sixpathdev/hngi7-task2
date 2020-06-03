e99085b703cdc0db7abffe429bbe43c5ea85a62f
Arisekolat <64399351+Arisekolat@users.noreply.github.com>
2020-06-03 01:48:59 -0700

    Added email

--- scripts/SundayAzeez_HNG-01724_PHP.php
+++ scripts/SundayAzeez_HNG-01724_PHP.php
@@ -1,1 +1,11 @@
+<script>
+  var myinfo = {
+    "name" : "Azeez Sunday",
+    "id" : "HNG-01724",
+    "language" : "Javascript",
+    "email" : "azeezsunday.a@gmail.com",
+  };
 
+  document.write("Hello world, this is " + myinfo.name + " with HNGi7 ID " + myinfo.id + " using  " + myinfo.language + " for stage 2 task");
+
+</script>
