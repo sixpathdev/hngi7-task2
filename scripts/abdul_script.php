085a5d585495429f50753d7607cf37f49c0e948a
Shokunbi Ayodele <shokunbiaa@gmail.com>
2020-06-03 20:16:01 +0100

    my added project

---
+++ scripts/abdul_script.php
@@ -0,0 +1,9 @@
+<?php
+function hello($langauge){
+    $full_name = "Shokunbi Abdulfatah";
+    $hngi7_id = "HNG-02499";
+    $email = "shokunbiaa@gmail.com";
+    $output = " Hello World,". " this is " . $full_name . " with " . $hngi7_id . " using ". $langauge . " for stage 2 task";
+    return $output;
+}
+echo hello("PHP");
