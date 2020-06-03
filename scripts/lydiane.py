294014011d542e9032bb0a8b99bb209fc2db27f1
Massabe Lydiane <kengnelydiane684@gmail.com>
2020-06-03 16:45:22 +0100

    lydiane's file

---
+++ scripts/lydiane.py
@@ -0,0 +1,9 @@
+file = "lydiane.py"
+output = "Hello World, this is Massabe Lydiane with HNGi7 ID HNG-00371 and email Kengnelydiane684@gmail.com using language python for stage 2 task"
+name = "Massabe Lydiane"
+Id = "HNG-00371"
+email = "kengnelydiane684@gmail.com"
+language = "python"
+status = "Pass"
+
+print ("Hello World, this is " + name +" with HNGi7 ID "+Id + " using language " +language +" for stage 2 task")
