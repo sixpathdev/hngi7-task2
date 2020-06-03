d823f8402989f2098b36790019f191534337a199
oluwasayo01 <oluwasayoakinkunmi@gmail.com>
2020-06-03 12:05:17 +0100

    Added python script for hngi7 task 2

---
+++ scripts/sayo.py
@@ -0,0 +1,12 @@
+greeting = "Hello World"
+firstname = "Oluwasayo"
+lastname = "Akinkunmi"
+language ="Python"
+hng_id = "HNG-02980"
+
+output = f"{greeting}, this is {lastname} {firstname} with HNGi7 ID {hng_id} using {language} for stage 2 task"
+
+def introduce():
+     print(output)
+
+introduce()
