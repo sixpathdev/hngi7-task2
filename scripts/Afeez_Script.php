984c661a6810088a9d67b5ab907dc3992f17e41f
Afeezco470 <66228476+Afeezco470@users.noreply.github.com>
2020-06-03 17:08:59 +0100

    Create Afeez_Script.php

---
+++ scripts/Afeez_Script.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+// This is my HNGi7 stage 2 task
+$txt = "Hello World";
+$name = "Durosinmi Afeez Adekunle"; //My full name
+$hngid = "HNG-03143"; //HNGi7 ID
+$lang = "PHP";
+$email = "Durosinmiafeez1@gmail.com";
+
+echo $txt. ", this is " . $name . " with HNGi7 ID " . $hngid . " using " .$lang. " for stage 2 task";
+?>
+
+</body>
+</html>
