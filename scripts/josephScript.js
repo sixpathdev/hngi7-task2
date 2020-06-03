46402faa592afcf47f5127ff84ed6b31a2ec16e5
coolheaded1 <55092350+coolheaded1@users.noreply.github.com>
2020-06-03 13:13:28 +0100

    Add files via upload
    Adding of script

---
+++ scripts/josephScript.js
@@ -0,0 +1,7 @@
+const name = "Samuel Joseph";
+const hngid = "HNG-04565";
+const lang = "Javascript";
+Task = () => {
+ document.write(`Hello World, I am ${name} with HNGi7 ID ${hngid} using ${lang} for task 2.`);
+}
+Task();
