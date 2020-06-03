7690fde915e25fa5917c8955091199e380b49547
Alabi Feranmi <57694834+ferrumie@users.noreply.github.com>
2020-06-03 19:18:00 +0100

    removing comments

--- scripts/ferrum_script.py
+++ scripts/ferrum_script.py
@@ -1,7 +1,3 @@
-# HNG task 1
-# team flash 
-# "Hello World, this is [full name] with HNGi7 ID [ID] using [language] for stage 2 task"
-
 name = "Alabi Feranmi"
 id = "HNG-01822"
 email = "alabiemmanuelferanmi@gmail.com"
