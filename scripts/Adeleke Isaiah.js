4737109abc01bfe41b873d031fcb165f86d1b16f
Adeleke Isaiah <iadeleke01@gmail.com>
2020-06-03 20:46:17 +0100

    Update Adeleke Isaiah.js

--- scripts/Adeleke Isaiah.js
+++ scripts/Adeleke Isaiah.js
@@ -1,10 +1,10 @@
-const fullName = "Adeleke Isaiah";
-const iD = "HNG-00931";
+const name = "Adeleke Isaiah";
+const id = "HNG-00931";
 const email = "iadeleke01@gmail.com";
 const language = "Javascript";
 
-function intro() {
-    return `Hello world, this is ${fullName }, with  HNGi7 ID: ${iD} and Email: ${email} using ${language } for Stage 2 task`
-}
 
-console.log(intro())
+
+console.log("Hello World, this is " + name + " with HNGi7 ID " + id + " and  Email "  +  email + 
+
+" using " + language  + " for stage 2 task")
