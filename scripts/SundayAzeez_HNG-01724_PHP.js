fe7a88353b30bfbd16bdf1e4e45bf14867c5208e
Arisekolat <64399351+Arisekolat@users.noreply.github.com>
2020-06-03 18:37:53 +0100

    Update SundayAzeez_HNG-01724_PHP.js

--- scripts/SundayAzeez_HNG-01724_PHP.js
+++ scripts/SundayAzeez_HNG-01724_PHP.js
@@ -2,8 +2,8 @@
   var myinfo = {
     "name" : "Azeez Sunday",
     "id" : "HNG-01724",
-    "language" : "Javascript",
     "email" : "azeezsunday.a@gmail.com",
+    "language" : "Javascript",
   };
 
   document.write("Hello world, this is " + myinfo.name + " with HNGi7 ID " + myinfo.id + " using  " + myinfo.language + " for stage 2 task");
