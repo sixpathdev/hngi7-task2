d04881798128366616b2158518c69fe02bdf943c
fafo-hub <62049344+fafo-hub@users.noreply.github.com>
2020-06-03 16:00:11 -0700

    Update teefafooo_script.js

--- scripts/teefafooo_script.js
+++ scripts/teefafooo_script.js
@@ -1,1 +1,5 @@
-document.write("Hello World, this is Fafolahan Olatunde Samuel with HNGi7 ID 03012 using javascript for stage 2 task.");
+ const name = " Fafolahan Olatunde Samuel";  
+ const hng_ID = " HNG-03012";
+ const mail = "fafolahanolatunde@gmail.com"
+ const language = " Javascript";
+ console.log("Hello World, this is" + name + " with HNGi7 ID" + hng_ID + " using" + language + " for stage 2 task");
