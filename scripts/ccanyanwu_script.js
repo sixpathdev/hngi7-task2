90418bfc64eb22114af914564ecffd1dfe0780e3
ccanyanwu <eme42c@gmail.com>
2020-06-03 09:15:28 +0100

    added email

--- scripts/ccanyanwu_script.js
+++ scripts/ccanyanwu_script.js
@@ -4,10 +4,11 @@
    //variables to hold my data 
    const name = 'Chukwuemeka Collins Anyanwu',
          id = 'HNG-00278',
+         email = 'eme42c@gmail.com',
          language = 'JavaScript';
          
          //returns a string containing details 
-   return `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`;
+   return `Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`;
  }
  
  //prints this output to the browser
