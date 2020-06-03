dc20444bc35d141f4860c45eba605fd07455a95c
KAZEEM <kazeemnasiff@gmail.com>
2020-06-03 13:41:10 +0100

    Changed the script file extension to .js and updated it with my details

---
+++ scripts/sixpathdev_script.js
@@ -0,0 +1,14 @@
+
+const intern={
+	firstName: "Kazeem",
+	lastName:"Nasiff",
+	id:"HNG-03415",
+	language: "javascript",
+	email: "kazeemnasiff@gmail.com"
+}
+console.log(intern)
+	const fullname=intern.firstName+" "+intern.lastName;
+	const id=intern.id;
+	const language=intern.language; 
+
+console.log ("Hello world!"+" "+ "I am"+" "+fullname+","+" "+"ID of:"+" "+id+" "+"using"+" "+language+" "+"for stage 2 task");
