97482a1f4cbf9592fd5bf3efcfd68a6584973e47
Oluwatimilehin Makinde <temak@Oluwatimilehins-MacBook-Pro.local>
2020-06-03 23:36:56 +0100

    timi js task script, kindly review

---
+++ scripts/temak-HNG-06553.js
@@ -0,0 +1,7 @@
+const name = "Oluwatimilehin Makinde";
+const id = "HNG-06553";
+const email = "makindeoluwatimilehin@gmail.com";
+let language = "JavaScript";
+let greeting = "Hello World, this is " + name + " with HNGi7 ID " + id + " and email " + email + " using " + language + " for stage 2 task";
+
+console.log(greeting);
