01228bc926fa77de8c1645db95a085faa2716252
oluwasayo <oluwasayoakinkunmi@gmail.com>
2020-06-03 22:53:41 +0100

    refactor code as per requirement

---
+++ scripts/sayo_script.py
@@ -0,0 +1,5 @@
+
+def introduce(greeting, name, hng_id, language):
+    print(f"{greeting}, this is {name} with HNGi7 ID {hng_id} using {language} for stage 2 task")
+
+introduce("Hello World", "Akinkunmi Oluwasayo", "HNG-02980", "Python" )
