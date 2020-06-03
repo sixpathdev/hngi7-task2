1e51cfb9892479073db49d9c4686e3f2ad741552
was-coder <wasleadwears4u2c@gmail.com>
2020-06-03 09:05:08 +0400

    created a new html and js file of my information

---
+++ scripts/waslead.js
@@ -0,0 +1,5 @@
+let name = "ADELAKUN WASIU OLAIYA";
+let id = "HNG-00057";
+let language = "JAVASCRIPT";
+
+document.write ("Hello World!, this is " + name + " with HNGi7 ID of " + id + " using " + language);
