0f6b6cdfbeb8b563c73f820f3ec4eef647a6cf53
Samson8 <41994892+Samson8@users.noreply.github.com>
2020-06-03 16:57:50 +0100

    HNGi7 Task
    My HNGi7 task one for team flash

---
+++ scripts/HngTaskOne.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class HngTaskOne {
+
+    public static void main(String[] args) {
+        String fullname= "Samson D. Alejolowo";
+        String hng_id="HNG-03839";
+        String language="Java";
+        String status="Pass";
+        String email="samsondamilola22@gmail.com";
+        System.out.printf("Hello World, this is %s with HNGi7 ID %s, using %s for stage 2 task", fullname, hng_id, language);
+    }
+}
