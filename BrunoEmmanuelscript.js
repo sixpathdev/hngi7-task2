764be71b95c42284e8a8f2aab5e244fe03d9a5e3
Emmanuel Bruno <66222337+Embruno@users.noreply.github.com>
2020-06-03 10:55:37 -1100

    Delete BrunoEmmanuelscript.js

--- BrunoEmmanuelscript.js
+++
@@ -1,5 +0,0 @@
-const name = 'Bruno Emmanuel';
-const hng_id = 'HNG-02510';
-const email = 'emmabruno03@gmail.com';
-const language ='javascript';
-console.log_("Hello world, this is "+ name +" with HNGi7 "+ hng_id + "using" + language + "for stage 2 task")
