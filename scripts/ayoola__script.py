7312b0bb01421fa1a287aed7e215865861c323a0
Olaleye Ayoola <olaleyeayoola5@gmail.com>
2020-06-03 09:19:36 +0100

    Ayoola Script

---
+++ scripts/ayoola__script.py
@@ -0,0 +1,10 @@
+def main():
+    full_name = 'Ayoola Olaleye'
+    ID = 'HNG-03145'
+    language = 'python'
+
+    print(f'Hello World, this is {full_name} with HNGi7 ID {ID} using {language} for stage 2 task')
+
+
+if __name__ == "__main__":
+    main()
