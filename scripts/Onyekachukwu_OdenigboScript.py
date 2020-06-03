36f627f26f08e0813ea5e61313f1010624a220c0
Onyinteldev <66255427+Onyinteldev@users.noreply.github.com>
2020-06-03 16:50:53 +0100

    Add files via upload

---
+++ scripts/Onyekachukwu_OdenigboScript.py
@@ -0,0 +1,6 @@
+name = 'Onyekachukwu Odenigbo'
+id = 'HNG-02748'
+language = 'Python'
+email = 'onyekagodenigbo@gmail.com'
+
+print(f'Hello World, this is {name} with HNGi7 ID {id} using {language} for stage 2 task.')
