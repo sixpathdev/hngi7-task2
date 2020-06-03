59ac05f621545f37580ad836ac05bda966ba879e
CrazyChickenDev <dannychukz15@gmail.com>
2020-06-03 17:19:40 +0100

    created Python file

---
+++ scripts/HNGi704069.py
@@ -0,0 +1,8 @@
+#HNGi7 TASK FOR STAGE 2
+#TEAM FLASH
+
+def hngi7stage_1():
+    print("Hello World, this is Nwaobi Daniel with HNGi7 ID HNG-04069 using Python for stage 2 task")
+
+if __name__ == '__main__':
+    hngi7stage_1()
