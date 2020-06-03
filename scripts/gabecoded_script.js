a86294898dcc800f8448a9e6b698eefee96e6c33
Raji Gabriel <52663008+Gabecoded@users.noreply.github.com>
2020-06-03 11:42:56 +0100

    Update gabecoded_script.js

--- scripts/gabecoded_script.js
+++ scripts/gabecoded_script.js
@@ -1,5 +1,2 @@
-<<<<<<< Updated upstream
-document.write("Hello World, this is Gabecoded with HNGi7 ID: HNG-00100 using JavaScript for stage 1 task");
-=======
-document.write("Hello world, this is Gabecoded with HNGi7 ID: HNG-00100 using JavaScript for stage 1 task");
->>>>>>> Stashed changes
+var str = ""Hello World, this is Gabecoded with HNGi7 ID: HNG-00100 using JavaScript for stage 1 task.";
+console.log(str);
