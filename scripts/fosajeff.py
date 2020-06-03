d9bb6f844b881f2a46c6468c519b295942228fb5
Jeffrey <okoobohefosa@gmail.com>
2020-06-03 22:16:35 +0100

    add script

---
+++ scripts/fosajeff.py
@@ -0,0 +1,7 @@
+name = "Okooboh Efosa Jeffrey"
+hng_id = "HNG-00387"
+email = "okoobohefosa@gmail.com"
+language = "Python"
+output = "Hello World, this is " + name + " with HNGi7 ID " + hng_id + " using " + language + " for stage 2 task"
+
+print(output)
