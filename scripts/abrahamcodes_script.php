e74c7f85721b0a9b81b73c83b8a11a96567e3eaf
Ugwu Abraham Chidozie <jonasbraham@gmail.com>
2020-06-03 10:03:37 +0100

    created script for abrahamcodes hng task 2

---
+++ scripts/abrahamcodes_script.php
@@ -0,0 +1,31 @@
+<?php
+
+// Variable for First name
+$first_name = "Chidozie";
+
+// Variable for Middle name
+$middle_name = "Abraham";
+
+// Variable for Last name
+$last_name = "Ugwu";
+
+// Variable for Full name
+$full_name = $last_name ." ".$middle_name ." ". $first_name;
+
+//Variable for ID
+$id = "HNG-00457";
+
+//Variable for Language
+$language = "PHP";
+
+// Output to be displayed
+$output = "Hello World, this is $full_name with  HNGi7 ID $id using $language for stage 2 task";
+
+// Output printed
+echo $output;
+
+
+?>
+
+
+ 
