366d3c4de3695bf51d6838db363dd7dc87633393
Nsikanny <66219353+Nsikanny@users.noreply.github.com>
2020-06-04 00:04:35 +0100

    Update nsikanny_script.php

--- scripts/nsikanny_script.php
+++ scripts/nsikanny_script.php
@@ -1,9 +1,8 @@
 <?php
-$txt = "Hello World"
 $name = "Nsikak Inyangabia"
 $id = "HNG-03848"
 $LANG = "PHP"
 $email = "nsikanny@gmail.com"
 
-echo $txt. "This is" $name. "with HNGi7 ID" $id. "using" $LANG. "for stage 2 task";
+echo "Hello World" "This is" $name. "with HNGi7 ID" $id. "using" $LANG. "for stage 2 task";
 ?>
