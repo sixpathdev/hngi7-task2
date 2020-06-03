d8e1874087e2738b7502a223f17671c58d9b0885
Ibrahim Ouedraogo <brahim.oued@gmail.com>
2020-06-03 15:31:27 +0000

    First attempt of the task 2

---
+++ scripts/ibrahimoued_script.php
@@ -0,0 +1,9 @@
+<?php
+
+/// Basic code, very easy to understand, hope it'll make it :)
+$id = "HNG-04112";
+$name = "Ouedraogo Ibrahim";
+$email = "brahim.oued@gmail.com";
+$language = "JavaScript";
+
+echo "Hello World, this is $name with HNGi7 ID $id using $language for stage 2 task";
