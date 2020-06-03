7b655ad7f2b214e2d8c2a8087371a4d085ffa0ba
Heccubernny <ipom4eva@gmail.com>
2020-06-03 04:09:49 +0100

    My submission on the task 2 project heccubernny-HNG-03910 script

---
+++ scripts/heccubernny-HNG-03910.py
@@ -0,0 +1,10 @@
+class mydetails:
+	def __init__(self, name,ID,language):
+		self.name = name
+		self.ID = ID
+		self.language = language
+
+r = mydetails("Ikubanni Paul", "HNG-03910", "Python")
+print("Hello World, this is",r.name,"with HNGi7 ID",r.ID,"using",r.language,"for stage 2")
+
+		
