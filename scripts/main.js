482a5faf5e99494a99b9cd19f39b5b4abf286191
Luqmonac56 <Luqmonac56@gmail.com>
2020-06-03 16:00:18 +0100

    my script

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+var work = function (name, email, language, hngId){
+	return `Hello world, this is ${name} with HNGi7 ID ${hngId} and email ${email} using ${language} for stage 2 task`
+}
+
+console.log(work("Abdullahi Luqman" , "luqmonac56@gmail.com", "javascript", "HNG-03395"))
