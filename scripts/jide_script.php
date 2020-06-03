4c19db05da568a6cac452bbb586f00680f511926
Oladejo Olajide <programmerolajide@gmail.com>
2020-06-03 21:34:18 +0100

    added my script jide_script.php in the script directory

---
+++ scripts/jide_script.php
@@ -0,0 +1,11 @@
+<?php
+
+  $name = 'Oladejo Olajide';
+  $id = 'HNG-00248';
+  $language = 'PHP';
+  $email = 'programmerolajide@gmail.com';
+  
+  echo 'Hello World, this is '.$name.' with HNGi7 ID '.$id.' using '.$language.' for stage 2 task';
+ 
+
+?>
