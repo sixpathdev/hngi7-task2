7e8b99d6d4749974caec475b5276009160070d7c
David-N-Cee <66029306+David-N-Cee@users.noreply.github.com>
2020-06-03 01:31:15 +0100

    Create DavidCee_Script.py

---
+++ scripts/DavidCee_Script.py
@@ -0,0 +1,9 @@
+hello = "Hello World"
+
+my_name = " David, Nwachukwu Chukwuyem"
+
+hngi7_ID = "HNG-03163"
+
+language = "Python"
+
+output = f'{hello}, this is {my_name} with HNGi7 ID {hngi7_ID} using {language} for stage 2 task'
