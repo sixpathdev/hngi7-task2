917f0ce7952d852020871374caea3ae621bedc26
Ademola Babs <48495111+demolaf@users.noreply.github.com>
2020-06-03 17:33:36 +0100

    Update Demola_script.py

--- scripts/Demola_script.py
+++ scripts/Demola_script.py
@@ -3,7 +3,7 @@ id = "HNG-01367"
 email = "demolafadumo@gmail.com"
 language = "Python"
 
-print("Hello World, this is " + name + " with HNGi7 ID " + id + " using " + language + " for stage 2 task." + email)
+print("Hello World, this is " + name + " with HNGi7 ID " + id + " using " + language + " for stage 2 task")
 
      
 
