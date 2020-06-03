b0efe700ecae514b7256d90caa3c0c7b6c021f9c
pecpaker <umehpeculiar20@gmail.com>
2020-06-03 17:59:23 +0100

    Culiarity Stage Two Task

---
+++ scripts/HNG-03197.js
@@ -0,0 +1,4 @@
+const name = "Peculiar C.Umeh"
+const hng_id = "HNG-03197"
+const email = "peculiarumeh02@gmail.com"
+console.log(`Hello World, this is ${name} with HNGi7 ID ${hng_id} for stage 2 task`);
