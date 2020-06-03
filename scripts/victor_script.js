58e8cf6fa18cc459cd36d8d0ba1e7d23fefcad35
Victor Adeleke AFOLAYAN <adelekeafolayan@gmail.com>
2020-06-04 00:43:06 +0100

    feat:added victor_script.js

--- scripts/victor_script.js
+++ scripts/victor_script.js
@@ -2,7 +2,7 @@
 
 var name= "Victor Adeleke AFOLAYAN";
 var id= "HNGi7 ID HNG-02523";
-var email= "adelekeafolayan@gmail.com"
+var email= "adelekeafolayan@gmail.com";
 var language= "javaScript";
 
 var fullText = "Hello World, this is " + name + " with " + id + " using " + language + " for stage 2 task";
