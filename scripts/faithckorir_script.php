25e582e05a24ca62a7d8af7fab0bde4979731b9c
Faith Korir <faithckorir@gmail.com>
2020-06-04 02:24:37 +0300

    Task 2 Completed

---
+++ scripts/faithckorir_script.php
@@ -0,0 +1,9 @@
+<?php
+$myName = "Faith Chepkemoi Korir";
+$myId = "HNG-00062";
+$myEmail = "faithckorir@gmail.com";
+$language = "PHP";
+
+
+echo "Hello World, this is " .$myName. " with HNGi7 ID " .$myId. " using " .$language. " for stage 2 task";
+
