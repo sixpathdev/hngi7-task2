a16e4de41a7a729e31c0b40e3af10685c7cdad22
Nerdyxils <56302195+Nerdyxils@users.noreply.github.com>
2020-06-03 16:16:15 +0100

    Added email address

--- scripts/HNG-Task2.js
+++ scripts/HNG-Task2.js
@@ -2,6 +2,7 @@ var myInfo = () => {
     var name = 'Abiodun Silas Ayomide';
     var id = 'HNG-04406';
     var lang = 'Javascript'
+    var email = 'silasabiodun25@gmail.com
 return "Hello world! this is " + name + " with HNGi7 ID " + id + " using " + lang + ' for stage 2 task' ; 
 }
 document.write(myInfo());
