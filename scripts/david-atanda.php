7d55518a56f89c7b72fb8e50c7d33d04000cdab3
David Atanda <atandadave@gmail.com>
2020-06-03 14:36:45 +0100

    stage 1 task

---
+++ scripts/david-atanda.php
@@ -0,0 +1,6 @@
+<?php
+$internname = "Atanda David";
+$internID = "HNG-04072";
+$internmail ="atandadave@gmail.com";
+echo "Hello World, this is " .$internname . " with HNGi7 ID " .$internID . "using PHP for stage 2 task";
+?>
