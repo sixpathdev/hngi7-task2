c8d05a32ac3a9be5140db9f5182af5acdf3d5366
JayBeloved <63831361+JayBeloved@users.noreply.github.com>
2020-06-03 21:47:17 +0100

    Update Jaybeloved's_Script

---
+++ scripts/JayBeloved_Script.py
@@ -0,0 +1,6 @@
+myname = "Lawal John J."
+hngi_id = "HNG-04115"
+email  = "info.jaybeloved@gmail.com"
+language_used = "python"
+
+print("Hello World, this is " + myname + " with HNGi7 ID " + hngi_id + "  using " + language_used +" for stage 2 task")
