b026f0433c8b9074b197287de4f6dd585320df7a
Mveliso Mtana <36462899+Mveliso42@users.noreply.github.com>
2020-06-04 01:34:49 +0200

    mmtana
    Updated

---
+++ scripts/mmtana.py
@@ -0,0 +1,6 @@
+name = 'Mveliso Mtana'
+hng_id = 'HNG - 02711'
+email = 'mtanamveliso@gmail.com'
+language = 'python'
+
+print('Hello World, this is' + ' ' + name + ' ' + 'with HNGi7 ID' +' '+ hng_id +' ' + 'using' +' '+ language +' '+ 'for stage 2 task.')
