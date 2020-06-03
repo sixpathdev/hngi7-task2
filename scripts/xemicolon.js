d4e584206d8e10832d5548d9f4ecde459f490a37
Xemicolon <1vibenation@gmail.com>
2020-06-03 19:07:27 +0100

    xemicolon_script

--- scripts/xemicolon.js
+++ scripts/xemicolon.js
@@ -6,7 +6,7 @@ const details = {
 };
 
 function helloWorld() {
-  let output = `Hello world, this is ${details.fullName} with HNGi7 ID ${details.hng_id} using ${details.language} for the stage 2 task.`;
+  let output = `Hello world, this is ${details.fullName} with HNGi7 ID ${details.hng_id} and email ${details.email} using ${details.language} for the stage 2 task.`;
   console.log(output);
 }
 
