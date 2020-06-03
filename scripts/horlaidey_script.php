2bc8d937478a78d47299a4ef333e4e123e7c4b07
horlaidey <salaudeenolaboy@gmail.com>
2020-06-03 13:22:03 +0100

    Desc: I just added a php script to the repository

---
+++ scripts/horlaidey_script.php
@@ -0,0 +1,9 @@
+<?php
+$name = "Salaudeen Taofeek Olaidey";
+$id = "HNG-01019";
+$language = "PHP";
+$email = "salaudeenolaboy@gmail.com";
+
+echo "Hello world, this is $name with HNGi7 ID: $id using $language for stage 2 task"
+
+?>
