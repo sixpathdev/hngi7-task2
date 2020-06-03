8df571da509078b54ac83642f834e4616c351028
coolheaded1 <55092350+coolheaded1@users.noreply.github.com>
2020-06-03 15:43:50 +0100

    Update josephScript.js

--- scripts/josephScript.js
+++ scripts/josephScript.js
@@ -3,6 +3,6 @@ const hngid = "HNG-04565";
 const lang = "Javascript";
 const email = "coolheaded79@gmail.com";
 Task = () => {
- document.write(`Hello World, I am ${name} with HNGi7 ID ${hngid} and email ${email} using ${lang} for task 2.`);
+ document.write(`Hello World, I am ${name} with HNGi7 ID ${hngid} and email ${email} using ${lang} for stage 2 task`);
 }
 Task();
