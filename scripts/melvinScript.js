b4dfa9af835cf044badf14dcf5b63d7da1822c5f
MelvinManni <melvinnnamani@gmail.com>
2020-06-03 21:04:27 +0100

    Variable ordering

--- scripts/melvinScript.js
+++ scripts/melvinScript.js
@@ -1,6 +1,6 @@
 const name = 'Nnamani Kosisochukwu Melvin';
-const email = 'melvinnnamani@gmail.com';
 const hng_id = 'HNG-01617';
+const email = 'melvinnnamani@gmail.com';
 const language = 'JavaScript';
 
 let output = `Hello World, this is ${name} with HNGi7 ID ${hng_id} using ${language} for stage 2 task`;
