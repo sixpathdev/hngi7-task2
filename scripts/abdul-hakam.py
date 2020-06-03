18b0f6c1b96f6b83402d9876c33a992d10a94b10
FameC <35351094+FameC@users.noreply.github.com>
2020-06-03 09:45:26 +0100

    Update abdul-hakam.py

--- scripts/abdul-hakam.py
+++ scripts/abdul-hakam.py
@@ -3,5 +3,6 @@
 full_name = 'Abdul-hakam Abuda Oshiogwe'
 hng_id ='HNG-02429'
 lang = 'Python' 
+email_adr = 'princefame1@gmail.com'
 
 print(f"Hello World, this is {full_name} with HNGi7 ID {hng_id} using {lang} for stage 2 task")
