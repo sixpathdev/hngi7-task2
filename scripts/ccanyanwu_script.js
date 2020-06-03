0825ac3cbf7bd1614a9ce8df6e46ed5d3fb8fa5c
ccanyanwu <eme42c@gmail.com>
2020-06-03 18:47:28 +0100

    Followed team stipulated instruction

--- scripts/ccanyanwu_script.js
+++ scripts/ccanyanwu_script.js
@@ -1,14 +1,15 @@
-//function that returns my full details as a string 
+ //function that returns my full details as a string 
  const myDetails = () => {
-   //object to hold my data 
-   const bio ={
-     name : 'Chukwuemeka Collins Anyanwu',
-     id : 'HNG-00278',
-     lang : 'JavaScript'
-   } 
-    const email = 'eme42c@gmail.com';     
+   //variable to hold my data 
+   
+    const name = 'Chukwuemeka Collins Anyanwu',
+          id ='HNG-00278',
+          email = 'eme42c@gmail.com',
+          lang = 'JavaScript';
+  
+         
          //returns a string containing details 
-   return `Hello World, this is ${bio.name} with HNGi7 ID ${bio.id} using ${bio.lang} for stage 2 task`;
+   return `Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task`;
  }
  
  //prints this output to the browser
