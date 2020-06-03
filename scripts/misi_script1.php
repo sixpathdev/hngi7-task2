9c7d174262456a996dbabebcc2fe5bc39e3456b4
misi <ayoadefunmi59@gmail.com>
2020-06-03 16:21:13 +0100

    feat: added misi_script1.php

---
+++ scripts/misi_script1.php
@@ -0,0 +1,9 @@
+<?php
+$surname = "Ayoade";
+$firstname ="Oluwafunmisi";
+$fullname = $surname ." ".$firstname;
+$hngid = "HNG-01173";
+$email = "ayoadefunmi59@gmail.com";
+$language = "PHP";
+echo "Hello World, this is ".$fullname."  with HNGi7 ID ".$hngid." using ".$language." for stage 2 task";
+?>
