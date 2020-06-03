364c643f7b63e0058177429f03a412c851c4d65a
Jaymahn07 <58877982+Jaymahn07@users.noreply.github.com>
2020-06-03 09:33:44 +0100

    Create Joshua2.o.py

---
+++ scripts/Joshua2.o.py
@@ -0,0 +1,6 @@
+import json
+Full_name = 'Joshua Pereira'
+HNGi7_Id = 'HNG-04334'
+Language = 'Python'
+print(json.dumps(" Hello World " + " This is " + Full_name + " with " + HNGI7_Id + " Using " + Language + " For Stage 2 Task "))
+
