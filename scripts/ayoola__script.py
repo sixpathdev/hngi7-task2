062f640a73e0185d14f3041f1471ed853061cf72
Olaleye Ayoola <olaleyeayoola5@gmail.com>
2020-06-03 10:00:50 +0100

    Ayoola Script

--- scripts/ayoola__script.py
+++ scripts/ayoola__script.py
@@ -2,9 +2,10 @@ def main():
     full_name = 'Ayoola Olaleye'
     ID = 'HNG-03145'
     language = 'python'
+    email = 'olaleyeayoola5@gmail.com'
 
-    print(f'Hello World, this is {full_name} with HNGi7 ID {ID} using {language} for stage 2 task')
+    return(f'Hello World, this is {full_name} with HNGi7 ID {ID} using {language} for stage 2 task')
 
 
 if __name__ == "__main__":
-    main()
+    print(main())
