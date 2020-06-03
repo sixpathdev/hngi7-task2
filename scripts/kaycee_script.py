c89264362370928a0a2a5094bd32e12410254ed2
Oguh_Kelechi <62163885+Kelechi-oguh@users.noreply.github.com>
2020-06-03 15:10:43 +0100

    Update kaycee_script.py

--- scripts/kaycee_script.py
+++ scripts/kaycee_script.py
@@ -2,7 +2,7 @@ def myOutput():
     fullname = "Oguh Kelechi Henry"
     ID = "HNG-03565"
     language = "Python"
-    email = "Kelechi.oguh505@gmil.com"
+    email = "Kelechi.oguh505@gmail.com"
 
     output = "Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task".format(fullname, ID, language)
     return output
