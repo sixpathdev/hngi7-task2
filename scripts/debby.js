a03a36151e67abb728c95aebc674ffdcc12895c1
harobedsuip <66274313+harobedsuip@users.noreply.github.com>
2020-06-03 21:24:44 +0100

    task2
    this is my task coded with javascript.

---
+++ scripts/debby.js
@@ -0,0 +1,11 @@
+const person = () => {
+    const me = {
+        fullName: 'Deborah Pius',
+        ID: 'HNG-04925',
+        language: 'Javascript',
+        email: 'hasuip11@gmail.com'
+    }
+    return 'Hello World, this is '+ me.fullName +' with HNGi7 ID '+ me.ID +' using '+ me.language +' for stage 2 task';
+};
+
+console.log(person());
