5f5cd14930dd5f211291f35d3fe3cab5c02cd67b
otitoju <otitojuoluwapelumi@gmail.com>
2020-06-03 05:27:37 +0100

    feature add otitoju file

---
+++ scripts/otitoju_script.js
@@ -0,0 +1,5 @@
+function taskTwo(fullname, id) {
+    let output = `Hello World, this is ${fullname} with HNGi7 ID ${id} using Node js for stage 2 task.`
+    process.stdout.write(output)
+}
+taskTwo("OTITOJU JOHN OLUWAPELUMI", "HNG-01044")
