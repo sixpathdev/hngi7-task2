b606e10f43f14a85bf39ca14e29256f358d3ccbf
ShokunbiAyodele <66224802+ShokunbiAyodele@users.noreply.github.com>
2020-06-04 00:27:11 +0100

    Update abdul_script.php

--- scripts/abdul_script.php
+++ scripts/abdul_script.php
@@ -3,7 +3,7 @@ function hello($langauge){
     $full_name = "Shokunbi Abdulfatah";
     $hngi7_id = "HNG-02499";
     $email = "shokunbiaa@gmail.com";
-    $output = " Hello World,". " this is " . $full_name . " with " . $hngi7_id . " using ". $langauge . " for stage 2 task";
+    $output = " Hello World,". " this is " . $full_name . " with HNGi7 ID " . $hngi7_id . " using ". $langauge . " for stage 2 task";
     return $output;
 }
 echo hello("PHP");
