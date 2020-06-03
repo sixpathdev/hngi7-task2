e0e010433b2b30efd8c5cd6baeecfa0a120764d1
Akinola Raphael <54055273+Ethical-Ralph@users.noreply.github.com>
2020-06-03 19:38:58 +0100

    Create HNG-05150.js

---
+++ scripts/HNG-05150.js
@@ -0,0 +1,10 @@
+const name = 'Akinola Raphael';
+const hngId = 'HNG-05150';
+const email = 'rakinola90@gmail.com';
+const language = 'javascript';
+
+const output = () => {
+  return `Hello World, this is ${name} with HNGi7 ID ${hngId} using ${language} for stage 2 task`
+}
+
+console.log(output())
