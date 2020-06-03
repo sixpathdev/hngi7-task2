31951a10d00980aa8d7274bea540c961d8a59985
Afolabi Marvelous Sunday <afosmarvel@gmail.com>
2020-06-03 04:39:27 -0700

    Add files via upload

---
+++ marvscript.js
@@ -0,0 +1,7 @@
+const name = 'Afolabi Marvelous Sunday';
+const id = 'HNG-01058';
+const email = 'afosmarvel@gmail.com'
+const language = 'javascript'
+
+document.write("Hello World, this is " + name + " with HNGi7 ID " + id + 
+" using " + language  + " for stage 2 task")
