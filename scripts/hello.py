215b6dc8565e7c4973307ba615ad15746398997d
Gerald-Izuchukwu <geraldlouisugwunna@gmail.com>
2020-06-03 21:07:55 +0100

    added gerald hello.py script

--- scripts/hello.py
+++ scripts/hello.py
@@ -4,4 +4,4 @@ hngi7_ID = "HNG-05974"
 
 email = "geraldlouisugwunna@gmail.com"
 
-print(f'{greeting}, this is {full_name}, with HNGi7 ID:{hngi7_ID} and email: {email} using python for stage 2 task ')
+print(f'Hello World this is {full_name}, with HNGi7 ID:{hngi7_ID} and email: {email} using python for stage 2 task ')
