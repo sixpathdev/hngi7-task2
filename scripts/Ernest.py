98a12b51deaead92aa3e4ecbe3e58cffdbbde58b
E-STAT <owojori.tolulope@gmail.com>
2020-06-03 10:46:29 +0100

    Ernest taskscript  with email added

--- scripts/Ernest.py
+++ scripts/Ernest.py
@@ -2,7 +2,10 @@ def task(Name, hng_id, language):
     output = "Hello World this is {} with HNGi7 ID {} using {} for stage 2 task".format(Name, hng_id,language)
     return output
 
-
-print(task(Name = "Owojori Ernest Tolulope", hng_id = "HNG-01824", language="Python"))
+Name = "Owojori Ernest Tolulope"
+hng_id = "HNG-01824"
+language = "Python"
+email = "owojori.tolulope@gmail.com"
+print(task(Name = Name, hng_id = hng_id, language=language))
 
     
