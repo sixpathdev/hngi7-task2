e696f58d3e832ffc246086ce91308c88ccbe3ca7
Adeyemi Timilehin <adeyemitimmy@gmail.com>
2020-06-04 04:24:11 +0100

    Help me merge

---
+++ scripts/AdeyemiTimilehin-HNDi702251_script1.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+
+
+
+$name="Adeyemi Timilehin Mike";
+$id="02251";
+$language="php";
+$email="adeyemitimmy@gmail.com";
+
+
+
+echo "\nHello world,this is $name with HNG i7 ID $id using $language for stage 2
+
+flush();
+?>
