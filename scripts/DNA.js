a981d7382f1a1148e779ea1a1f8b976c3af48891
Davidtariq96 <61972732+Davidtariq96@users.noreply.github.com>
2020-06-04 18:26:15 +0000

    Email updated, according to stipulated format

--- scripts/DNA.js
+++ scripts/DNA.js
@@ -1,8 +1,8 @@
 
 let myInfo =() =>{
+    let email= "samikendrickjnr@gmail.com";
     let name = "David Samuel Uche";
     let ID = "HNG-01778";
-    let email= "samikendrickjnr@gmail.com";
     let language= "Javascript";
 
     return `Hello World, this is ${name} with HNGi7 ID ${ID} using ${language} for stage 2 task`
