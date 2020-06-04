47230cc73fcf70ac3a07171627cfe7a2130cc262
ccanyanwu <eme42c@gmail.com>
2020-06-04 18:42:06 +0100

    Email at the top

--- scripts/ccanyanwu_script.js
+++ scripts/ccanyanwu_script.js
@@ -1,16 +1,13 @@
- //function that returns my full details as a string 
+//email 
+const email = 'eme42c@gmail.com';
+//function that returns my full details as a string 
  const myDetails = () => {
-   //variable to hold my data 
-   
-    const name = 'Chukwuemeka Collins Anyanwu',
-          id ='HNG-00278',
-          email = 'eme42c@gmail.com',
-          lang = 'JavaScript';
-  
-         
+   //variables to hold my data 
+   const name = 'Chukwuemeka Collins Anyanwu',
+         id = 'HNG-00278',
+         lang = 'JavaScript';
          //returns a string containing details 
    return `Hello World, this is ${name} with HNGi7 ID ${id} using ${lang} for stage 2 task`;
  }
- 
  //prints this output to the browser
-console.log(myDetails());
+document.write(myDetails());
