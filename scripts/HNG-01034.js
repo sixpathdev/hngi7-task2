0b85075b55d2d15f9a85275b77da09a1c4fc2c8b
sjdammy <dami.seweje@cicod.com>
2020-06-04 11:33:21 +0100

    added script file for hng01034

---
+++ scripts/HNG-01034.js
@@ -0,0 +1,7 @@
+const name = 'Seweje Damilola',
+  email = 'sewejed@gmail.com',
+  hngId = 'HNG-01034',
+  language = 'JavaScript',
+  output = `Hello World, this is ${name} with HNGi7 ID ${hngId} using ${language} for stage 2 task`;
+
+console.log(output);
