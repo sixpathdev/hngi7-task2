feebd3e8268ac9a19f9868af87710a1f7ee09e02
fidelity129 <31078526+fidelity129@users.noreply.github.com>
2020-06-04 01:33:26 +0100

    Create FidelStyle.js

---
+++ FidelStyle.js
@@ -0,0 +1,13 @@
+var greet = "hello world"
+var name = "UZOIGWE IFEANYI FIDEL "
+var email = "fidmikaelz@gmail.com"
+var hngId = "hng-04523"
+var lang = "JavaScript"
+var task = "stage 2 task"
+
+document.write (greet)
+document.write (name)
+document.write (email)
+document.write (hngId)
+document.write (lang)
+document.write (task)
