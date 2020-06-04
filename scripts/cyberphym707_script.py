c267cadbc0b6b99bb995939eeff1493e650779cd
Jimoh Lawal Olafimimihan <dmainman4sucess@gmail.com>
2020-06-04 23:19:47 +0100

    Delete cyberphym707_script.py

--- scripts/cyberphym707_script.py
+++
@@ -1,8 +0,0 @@
-name = ('Jimoh Lawal')
-hng_id = ('HNG-01227')
-language = ('Python')
-email = ('dmainman4sucess@gmail.com')
-
-
-print('Hello World, this is ' + name + ' with HNGi7 ID ' +
-      hng_id + ' using ' + language + ' for stage 2 task email ' + email)
