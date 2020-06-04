78df7ad2b5b106e185f56a1b7960288e94387554
Bongi <66295440+Bongi-buzzyb@users.noreply.github.com>
2020-06-04 16:49:24 +0200

    Add files via upload

---
+++ scripts/HNG-01263.js
@@ -0,0 +1,12 @@
+const Me = {
+    name: 'Bongi Mkhonza',
+    ID: 'HNG-01263',
+    email: 'bongi.buzzyb@outlook.com',
+    language: 'Javascrpit',
+};
+
+const Text = () => {
+    return `Hello World, this is ${Me.name} with HNGi7 ID ${Me.ID} using ${Me.language} for stage 2 task.`
+  }
+  
+  console.log(Text())
