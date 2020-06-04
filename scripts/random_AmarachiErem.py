d180d3f341964cc6173d2be09b65468d8a490749
Wiamer <erem.amarachi@gmail.com>
2020-06-04 23:08:40 +0100

    update email

--- scripts/random_AmarachiErem.py
+++ scripts/random_AmarachiErem.py
@@ -3,4 +3,6 @@ name = "Amarachi Erem"
 hng_id = "HNG-04424"
 lang = "Python"
 
-print(f"Hello World, this is {name} with HNGi7 ID {hng_id} using {lang} for stage 2 task")
+print(f"Hello World, this is {name} with HNGi7 ID {hng_id} using {lang} for stage 2 task email {email}")
+
+
