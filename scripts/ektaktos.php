026272b51688a7fb6dbfd5532145d1231b222270
Wale Timothy <awtim01@gmail.com>
2020-06-04 09:47:13 +0100

    commited my own scripts file

---
+++ scripts/ektaktos.php
@@ -0,0 +1,7 @@
+<?php
+    $my_name = 'Wale Timothy';
+    $my_hng_id = 'HNG-00124';
+    $my_email = 'alabiwaletimothy@gmail.com';
+    $my_language = 'Php';
+    echo "Hello World, this is {$my_name} with HNGi7 ID {$my_hng_id} and email {$my_email} using {$my_language} for stage 2 task";
+?>
