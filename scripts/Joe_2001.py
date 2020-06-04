cd2264d3649d6f8a5bf03249a31c687e2568c05c
eriteria <51262474+eriteria@users.noreply.github.com>
2020-06-04 19:50:13 +0100

    new file:   scripts/Joe_2001.py

---
+++ scripts/Joe_2001.py
@@ -0,0 +1,6 @@
+name = 'John Joseph'
+hng_id = 'HNG-04194'
+language = 'Python'
+email = 'joseph.john.ali.2001@gmail.com'
+output = 'Hello World, this is '+ name + ' with HNGi7 ID ' + hng_id + ' using ' + language +' for stage 2 task'
+print(output)
