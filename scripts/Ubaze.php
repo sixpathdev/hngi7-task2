abce402973c0bbb62d99de050430ebfd379ec1d9
U-baze <52576900+U-baze@users.noreply.github.com>
2020-06-04 20:46:01 +0100

    Delete Ubaze.php

--- scripts/Ubaze.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$name ="Ubaydullah Onabanjo";
-$hng_id ="HNG-01640";
-$email ="adenolaonabanjo@gmail.com";
-$language ="PHP";
-
-echo "Hello World, this is ".$name ." with HNGi7-ID :: ". $hng_id ." using php for stage 2 task";
- ?>
