4babe72ca093bf9f53ab6592b447bc98ec3250e6
Oluwatoyin Daramola <temyyvickk@gmail.com>
2020-06-04 06:22:49 -0700

    added Oluwatoyin.js

---
+++ scripts/Oluwatoyin.js
@@ -0,0 +1,6 @@
+let name = "Daramola Oluwatoyin Victoria";
+let hid =  "HNG-00476";
+let lang = "javaScript";
+let mail = "temyyvickk@gmail.com"
+
+console.log(`Hello World, this is ${name} with HNGi7 ID ${hid} using ${lang} for stage 2 task`);
