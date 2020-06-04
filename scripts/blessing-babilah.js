4dfcd5a3dec0a1e13d2c148c42f8ab384bad3851
Blessing M. Babilah <mbamah1999@gmail.com>
2020-06-04 15:23:15 +0000

    made some changes to name and email

--- scripts/blessing-babilah.js
+++ scripts/blessing-babilah.js
@@ -1,7 +1,7 @@
 
-const fullname  = 'Blessing Mbamah Babilah';
-const id = 'HNG-00958';
 const mail = 'mbamah1999@gmail.com';
+const fullname  = 'Blessing Babilah';
+const id = 'HNG-00958';
 const pLanguage = 'javascript';
 const message = `Hello world, this is ${fullname} with HNGID: ${id} using ${pLanguage} for stage 2 task.`
 console.log(message);
