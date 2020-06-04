50c8ceaccb37e3c894bec291e085ef77574ce4e9
viviydesign <66270014+viviydesign@users.noreply.github.com>
2020-06-05 00:49:36 +0100

    Update viviydesign_script.py

--- scripts/viviydesign_script.py
+++ scripts/viviydesign_script.py
@@ -3,4 +3,4 @@ name="Ofodum Ogechukwu"
 hng_id="HNG-04571"
 language="Python"
 
-print("Hello World, this is "+ name+ " with HNGi7 ID "+hng_id+" using " + language + " for stage 2:")
+print("Hello World, this is "+ name+ " with HNGi7 ID "+hng_id+" using " + language + " for stage 2 task. Email"+ email)
