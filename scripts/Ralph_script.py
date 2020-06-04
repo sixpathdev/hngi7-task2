6633a8e2a9a6a86d1e5acc18416e9d82643d9121
raph941 <raph941@gmail.com>
2020-06-04 19:48:00 +0100

    fix: placed email variable on line1

--- scripts/Ralph_script.py
+++ scripts/Ralph_script.py
@@ -1,6 +1,6 @@
+email = "raph941@gmail.com"
 full_name = "Ehindero Raphael Ayo"
 hng_id = "HNG-00314"
-email = "raph941@gmail.com"
 language = "python"
 
 output = f'Hello World, this is {full_name} with HNGi7 ID {hng_id} using {language} for stage 2 task'
