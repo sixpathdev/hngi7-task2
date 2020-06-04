d79456da07e18cef6e7335ba427d134c845a458e
Damilare <41183714+Dreybest@users.noreply.github.com>
2020-06-04 01:39:42 +0100

    Add files via upload

---
+++ scripts/dreybest_script.js
@@ -0,0 +1,5 @@
+const name = "Damilare Idowu";
+const hng_id = "HNG-06075"
+const email = "aideedrey@gmail.com"
+
+console.log("Hello World, this is " + name + " with HNGi7 ID " + hng_id + " using javascript for stage task 2")
