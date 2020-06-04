006dc05a002eb433ea56d48bd12be0afca170c47
CrazyChickenDev <dannychukz15@gmail.com>
2020-06-04 15:59:17 +0100

    corrected mail position

--- scripts/Nwaobi-Daniel_script.py
+++ scripts/Nwaobi-Daniel_script.py
@@ -1,9 +1,8 @@
 #HNGi7 TASK FOR STAGE 2
 #TEAM FLASH
-
+email = "dannychukz15@gmail.com"
 full_name = "Nwaobi Daniel"
 my_id = "HNG-04069"
-email = "dannychukz15@gmail.com"
 language = "Python"
 
 print("Hello World, this is " + full_name + " with HNGi7 ID " + my_id + " using " + language + " for stage 2 task")
