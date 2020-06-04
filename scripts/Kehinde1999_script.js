13c09249890ea48a079c6e55aff9fa371ba91d99
Oyewale Kehinde <42590579+oyewalekehinde@users.noreply.github.com>
2020-06-04 09:38:29 +0100

    Create Kehinde1999_script.js

---
+++ scripts/Kehinde1999_script.js
@@ -0,0 +1,7 @@
+let fullName = 'Kehinde Oluwaseun Oyewale';
+let id = 'HNG-05604';
+let email = 'oyewalekehinde34@gmail.com'
+let language = 'JavaScript';
+let output = `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task.`;
+
+console.log(output);
