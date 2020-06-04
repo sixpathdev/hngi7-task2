07610eba0b382c1a1ea1327621f02cc28f857585
Wale Timothy <awtim01@gmail.com>
2020-06-04 11:53:40 +0100

    Update HNG-00124.php

--- scripts/HNG-00124.php
+++ scripts/HNG-00124.php
@@ -3,5 +3,5 @@
     $my_hng_id = 'HNG-00124';
     $my_email = 'alabiwaletimothy@gmail.com';
     $my_language = 'Php';
-    echo "Hello World, this is {$my_name} with HNGi7 ID {$my_hng_id} and email {$my_email} using {$my_language} for stage 2 task";
+    echo "Hello World, this is {$my_name} with HNGi7 ID {$my_hng_id} using {$my_language} for stage 2 task";
 ?>
