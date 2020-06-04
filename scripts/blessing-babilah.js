2a269e66860cd5a77833a1b1c14894f4fffa9d07
Blessing M. Babilah <mbamah1999@gmail.com>
2020-06-04 07:54:35 +0000

    team task2

---
+++ scripts/blessing-babilah.js
@@ -0,0 +1,8 @@
+
+const fullname  = 'Blessing Mbamah Babilah';
+const id = 'HNG-00958';
+const mail = 'mbamah1999@gmail.com';
+const pLanguage = 'javascript';
+const message = `Hello world, this is ${fullname} with HNGID: ${id} using ${pLanguage} for stage 2 task.`
+console.log(message);
+
