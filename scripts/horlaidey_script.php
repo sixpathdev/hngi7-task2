ae0ecf3989436ed7098f20cf29fe55a1208b2c88
salaudeen olaidey <salaudeenolaboy@gmail.com>
2020-06-04 22:14:42 +0100

    Update horlaidey_script.php

--- scripts/horlaidey_script.php
+++ scripts/horlaidey_script.php
@@ -3,6 +3,6 @@ $email = "salaudeenolaboy@gmail.com";
 $_name = "Salaudeen Taofeek Olaidey";
 $id = "HNG-01019";
 $language = "PHP";
-echo "Hello World, this is $_name with HNGi7 ID $id using $language for stage 2 task"
+echo "Hello World, this is $_name with HNGi7 ID $id using $language for stage 2 task email $email"
 
 ?>
