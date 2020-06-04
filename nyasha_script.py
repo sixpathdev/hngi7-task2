d862c946e2acda4f8db8de90bf693e8aa234bbb4
Tanyah8 <ntmungofa@gmail.com>
2020-06-04 11:33:58 +0200

    Routine Dev: Replaced Python Scripts with Javascript

--- nyasha_script.py
+++
@@ -1,6 +0,0 @@
-full_name = "Nyasha Mawarire"
-id_num = "HNG-02775"
-language = "Python"
-mail= "ntmungofa@gmail.com"
-
-print ("Hello World, this is " + full_name + " " + "with HNGi7 ID: " + id_num + " " + "and email "+ mail + " " +"using " + language + " "+ "for stage 2 task." )
---
+++ scripts/tanyah8_script.js
@@ -0,0 +1,15 @@
+const full_name = "Nyasha Mawarire";
+const hng_id = "HNG-02775";
+const language = "Javascript";
+const email = "ntmungofa@gmail.com";
+const message = `Hello World, this is ${full_name} with HNGi7 ID ${hng_id} and email ${email} using ${language} for stage 2 task.`;
+
+function log(val) {
+    console.log(val);
+}
+
+function write(val) {
+    document.write(val);
+}
+
+log(message);
