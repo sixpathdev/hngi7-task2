aca25820d549bafb0537d21d830087742aa84e7f
Mukaila Semiu <48639762+Semiteagain@users.noreply.github.com>
2020-06-04 05:10:12 +0100

    Update semiteagain.js

--- scripts/semiteagain.js
+++ scripts/semiteagain.js
@@ -2,5 +2,5 @@ const fullNameMy = "Mukaila Semiu";
 const id = "HNG-06255";
 const email = "semiteagain@gmail.com";
 const language = "javascript";
-const output = `Hello World, this is ${fullNameMy} with HNGi7 ID ${id} using ${language} for stage 2 task`;
+const output = `Hello World, this is ${fullNameMy} with HNGi7 ID ${id} and ${email} using ${language} for stage 2 task`;
 console.log(output);
