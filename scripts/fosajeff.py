e5783fac3d361b3f806aea91ede91b830ad0b181
Efosa Jeffrey <54464097+fosajeff@users.noreply.github.com>
2020-06-04 20:38:56 +0100

    update script

--- scripts/fosajeff.py
+++ scripts/fosajeff.py
@@ -1,6 +1,6 @@
-name = "Okooboh Efosa Jeffrey"
-hng_id = "HNG-00387"
 email = "okoobohefosa@gmail.com"
+name = "Okooboh Efosa"
+hng_id = "HNG-00387"
 language = "Python"
 output = "Hello World, this is " + name + " with HNGi7 ID " + hng_id + " using " + language + " for stage 2 task"
 
