17000d0400142de7ea71cf66b7a536fd5167c893
Dlama istifanus <dlamaistifanus0@gmail.com>
2020-06-04 13:27:52 +0100

    my task

---
+++ scripts/dlama.js
@@ -0,0 +1,9 @@
+var firstName = 'Dlama';
+
+var lastName = 'Istifanus';
+
+var hngId = 'HNG-04224';
+
+
+alert('Hello World , this is ' + lastName + ' ' + firstName + ' with HNGi7 ID ' + 
+hngId + ' using javascript for stage 2 task');
