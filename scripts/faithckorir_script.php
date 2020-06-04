e3efcc295340bdb29d09a6d3a2e98de58200e64f
faithckorir <faithckorir@gmail.com>
2020-06-04 13:48:45 +0300

    Delete faithckorir_script.php

--- scripts/faithckorir_script.php
+++
@@ -1,9 +0,0 @@
-<?php
-$myName = "Faith Chepkemoi Korir";
-$myId = "HNG-00062";
-$myEmail = "faithckorir@gmail.com";
-$language = "PHP";
-
-
-echo "Hello World, this is " .$myName. " with HNGi7 ID " .$myId. " using " .$language. " for stage 2 task";
-
