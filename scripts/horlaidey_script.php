a666e302563777ef90854c103fba53234bace9de
salaudeen olaidey <salaudeenolaboy@gmail.com>
2020-06-04 22:23:44 +0100

    Delete horlaidey_script.php

--- scripts/horlaidey_script.php
+++
@@ -1,8 +0,0 @@
-<?php
-$email = "salaudeenolaboy@gmail.com";
-$_name = "Salaudeen Taofeek Olaidey";
-$id = "HNG-01019";
-$language = "PHP";
-echo "Hello World, this is $_name with HNGi7 ID $id using $language for stage 2 task email $email"
-
-?>
