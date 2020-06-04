aca7b9b5f0e712bd2288cfd8326f99400dc327de
obute-agbo <obutemoses5@gmail.com>
2020-06-04 20:40:50 +0100

    'mosiac05_data_position_changed'

--- scripts/mosiac05.php
+++ scripts/mosiac05.php
@@ -1,9 +1,9 @@
 <?php
 
+$email='obutemoses05@gmail.com';
 $name='Obute Moses';
 $id='HNG-05246';
 $language='PHP';
-$email='obutemoses05@gmail.com';
 
 echo "Hello World, this is " . $name . " with HNGi7 ID ". $id ." using ".$language." for stage 2 task";
 
