c75e8ce4da4a72f46621bf542db14613e91db604
Olaitan.A <52823454+moa-mel@users.noreply.github.com>
2020-06-04 22:35:17 +0100

    task2
    HNG task 2 project

---
+++ scripts/task2.js
@@ -0,0 +1,9 @@
+  var myinfo ={
+    "name" : "Olaitan Akano",
+    "id"   : "HNG-06138",
+    "language" : "Javascript",
+    "email" : "olaitanmaryakano@gmail.com", 
+    };
+    
+    document.write("Hello world, this is " + myinfo.name + "with HNGi7 ID" + myinfo.id + "using " + myinfo.language + "for stage 2 task");
+   
