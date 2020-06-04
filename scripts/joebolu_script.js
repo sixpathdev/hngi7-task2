b2227a67c854d0db699648aee5da626ac9a177d9
Joe-Bolu <66218895+Joe-Bolu@users.noreply.github.com>
2020-06-04 15:51:21 +0100

    adding script for stage 2 task

---
+++ scripts/joebolu_script.js
@@ -0,0 +1,6 @@
+const name = 'Boluwatife Babajimi-Joseph';
+const hng_id = 'HNG-00228';
+const email = 'josh@joebolu.com';
+const language = 'javascript';
+console.log("Hello World, this is "+name+" with HNGi7 ID "+hng_id+" using "+language+" for stage 2 task");
+ 
