0b3f95fcc8b945b6e985addbf33637b49fa3bfd9
Paula-25 <paulaulor@yahoo.com>
2020-06-04 20:47:12 +0100

    Updated

--- scripts/onyinye_script.py
+++ scripts/onyinye_script.py
@@ -1,8 +1,8 @@
 def my_script():
-    name = "Ulor Paula Onyinyechukwu"
     email = "paulaulor@yahoo.com"
+    name = "Ulor Paula Onyinyechukwu"
     hng_id = "HNG-01463"
     lang = "Python"
-    print("Hello World, this is {name} with HNGi7 ID {hng_id} using {lang} for stage 2 task")
+    print(f"Hello World, this is {name} with HNGi7 ID {hng_id} using {lang} for stage 2 task")
 
 
