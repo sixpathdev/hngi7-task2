1c67fedafae0efd484d121e14e03b630c9c17605
Ajibade Olufemi <ajibadeolufemi94@gmail.com>
2020-06-04 21:00:03 +0100

    Another change

--- scripts/Justfemi_script.py
+++ scripts/Justfemi_script.py
@@ -3,4 +3,4 @@ fullName = "Ajibade Olufemi"
 ID = "HNG-01458"
 language = "python"
 
-print(f"Hello World, this is {fullName} with HNGi7 ID {ID} using {language} for stage 2 task.")
+print(f"Hello World, this is {fullName} with HNGi7 ID {ID} using {language} for stage 2 task {email}")
