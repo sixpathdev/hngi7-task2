e4e163454b7b628c815d8b1a317e97fcaf5c63bd
Oluwaseun Michael Oluwatomisin <62293658+ooluwaseunmicheal@users.noreply.github.com>
2020-06-04 18:41:39 +0100

    Update miketask.js

--- scripts/miketask.js
+++ scripts/miketask.js
@@ -1,16 +1,14 @@
  
 // Declaring Object Variable
 var mikeTask = {
-                Name: "Oluwatomisin Oluwaseun Michael",
-                ID: "HNG-04643",
                 Email: "ooluwaseunmicheal@gmail.com",
-                Language: "JavaScript",
-                Stage: 2
+                Name: "Oluwaseun Oluwatomisin",
+                ID: "HNG-04643",
+                Language: "JavaScript"   
             }
 
 //output of result
 
 console.log("Hello World, this is  " +mikeTask.Name +
             " with HNGi7 ID " +mikeTask.ID +
-            " using " +mikeTask.Language + " for Stage "
-            +mikeTask.Stage + " task");
+            " using " +mikeTask.Language + " for Stage 2 task");
