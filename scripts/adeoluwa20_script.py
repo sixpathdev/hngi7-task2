3106a7d242e4c0e6a32ad5061e602aaea4ce40cd
adeoluwa20 <aogbogboade@gmail.com>
2020-06-04 21:44:52 +0100

    feat: added adeoluwa20_script.py

--- scripts/adeoluwa20_script.py
+++ scripts/adeoluwa20_script.py
@@ -2,4 +2,4 @@ email = "aogbogboade@gmail.com"
 id = "HNG-01991"
 name = "Gbogboade Adeoluwa"
 language = "python"
-print ('Hello World, this is ' + 'name' + 'with HNGi7 ID' + 'id' + 'using' + 'language' + 'for stage 2 task')
+print ('Hello World, this is ' + name + ' with HNGi7 ID ' + id + ' using ' + language + ' for stage 2 task')
