0f010225a578ab5c0bbcd8e9f47f6c1566ee438f
Doyin Awosanya <didoloan@gmail.com>
2020-06-04 21:24:46 +0100

    new format

--- scripts/doyin.js
+++ scripts/doyin.js
@@ -3,4 +3,4 @@ const fullname  =   'Awosanya Awosanya';
 const hngid     =   'HNG-02674';
 const language  =   'JavaScript';
 
-console.log(`Hello World, this is ${fullname} with HNGi7 ID ${hngid} using ${language} for stage 2 task.`);
+console.log(`Hello World, this is ${fullname} with HNGi7 ID ${hngid} using ${language} for stage 2 task email ${email}`);
