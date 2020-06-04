cb192cff0e5de824f2fc03711c97b92c45c9f785
eazytimzy <61963475+eazytimzy@users.noreply.github.com>
2020-06-04 20:43:46 +0100

    Latest update on eazytimzy.js file

--- scripts/eazytimzy.js
+++ scripts/eazytimzy.js
@@ -2,7 +2,7 @@ let email = "adetunjitimilehin500@gmail.com"
 let MyName = "Adetunji Timilehin";
 let ID  = "HNG-02894";
 let language = "Javascript";
-let MyDetails = ` Hello World, this is ${MyName} with HNGi7 ID ${ID} using ${language} for stage 2 task.`;
+let MyDetails = `Hello World, this is ${MyName} with HNGi7 ID ${ID} using ${language} for stage 2 task.`;
 console.log(MyDetails);
 
 
