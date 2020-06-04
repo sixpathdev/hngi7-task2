a71529e64e1da4f47dd5e84e8a7934699c20214d
Ockiya Beinmonyu Daniel <beinmonyudaniel@gmail.com>
2020-06-04 22:27:12 +0100

    Updated Script to include email in the output

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,6 @@ const fullName = 'Ockiya Beinmonyu Daniel';
 const id = 'HNG-05057';
 const email = 'annonbytes@gmail.com'
 const language = 'JavaScript';
-let internInfo = `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task.`;
+let internInfo = `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task email ${email}.`;
 
 console.log(internInfo);
