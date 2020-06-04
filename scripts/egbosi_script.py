52df5f03d22f1375083aeba9183b02fb8b665d9d
KC <egbosikelechi@gmail.com>
2020-06-04 23:24:31 +0100

    chore: add setup script

--- scripts/egbosi_script.py
+++ scripts/egbosi_script.py
@@ -3,6 +3,6 @@ full_name = "Egbosi Kelechi"
 hng_id = "HNG-05609"
 language = "python"
 
-output = f'Hello World, this is {full_name} with HNGi7 ID {hng_id} using {language} for stage 2 task'
+output = f'Hello World, this is {full_name} with HNGi7 ID {hng_id} using {language} for stage 2 task email {email}'
 
 print(output)
