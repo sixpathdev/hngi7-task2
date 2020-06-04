86613e98d50e74d21037eb7ceb2859b70e868b99
Tanyah8 <ntmungofa@gmail.com>
2020-06-04 23:39:12 +0200

    changed the order of stuff

--- scripts/tanyah8_script.js
+++ scripts/tanyah8_script.js
@@ -1,15 +1,15 @@
 const email = "ntmungofa@gmail.com";
 const full_name = "Nyasha Mawarire";
-const hng_id = "HNG-02775";
-const language = "Javascript";
-const message = `Hello World, this is ${full_name} with HNGi7 ID ${hng_id} using ${language} for stage 2 task.`;
+const myhng_id = "HNG-02775";
+const languages = "Javascript";
+const message = `Hello World, this is ${full_name} with HNGi7 ID ${myhng_id} using ${languages} for stage 2 task ${email}.`;
 
 function log(val) {
     console.log(val);
 }
 
-function write(val) {
-    document.write(val);
-}
+// function write(val) {
+//     document.write(val);
+
 
 log(message);
