d87fe8b0787ef0c015383207f524350a0a35348f
storyengineer <66221766+storyengineer@users.noreply.github.com>
2020-06-04 12:04:42 -0700

    Add files via upload

---
+++ scripts/ekekalu_script.py
@@ -0,0 +1,6 @@
+email ="ekendukwe@gmail.com"
+name ="Eke Kalu"
+hngid ="HNG-01097"
+language ="python"
+
+print("Hello World" + "," + " this is " + name + " with HNGi7 ID " + hngid + " using " + language + " for stage 2 task")
