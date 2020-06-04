9f963cf9db66d498b986a634e13daa33e9339ca7
teelda <0812874+teelda@users.noreply.github.com>
2020-06-05 08:44:53 +1200

    Delete teeldah.js

--- scripts/teeldah.js
+++
@@ -1,5 +0,0 @@
-const name = "ANASHIE MATILDA IPEH",
-const Hng_Id = "HNG-06306",
-const email = "matildaipeh@gmail.com",
-	
-console.log('Hello World, this is ${name} with HNGi7 ID ${hng_id} for stage 2 task');
