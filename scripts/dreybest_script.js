124516dd6d0b183df6b5a73a3bc3b0b96521852b
Damilare <41183714+Dreybest@users.noreply.github.com>
2020-06-04 21:28:04 +0100

    Update dreybest_script.js

--- scripts/dreybest_script.js
+++ scripts/dreybest_script.js
@@ -1,6 +1,6 @@
+const email = 'aideedrey@gmail.com';
 const name = 'Damilare Idowu';
 const hng_id = 'HNG-06075';
-const email = 'aideedrey@gmail.com';
 const lang = 'javascript';
 
-console.log("Hello World, this is " + name + " with HNGi7 ID " + hng_id + " using " + lang + " for task 2" );
+console.log("Hello World, this is " + name + " with HNGi7 ID " + hng_id + " using " + lang + " for stage 2 task email" + email);
