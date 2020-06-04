09daf4295ed97052dfc6f1d53216ecc5615f946f
oluwaseun <jidielori77@gmail.com>
2020-06-04 23:11:30 +0100

    changed format

--- scripts/oluwaseun_script.py
+++ scripts/oluwaseun_script.py
@@ -3,4 +3,4 @@ Id ="HNG-04216"
 email="jideilori77@gmail.com"
 language="python"
 
-print(f"Hello World this is {name} with HNGi7 ID:{Id} and email: {email} using {language} for stage 2 task")
+print(f"Hello World, this is {name} with HNGi7 ID {Id} using {language} for stage 2 task email {email}")
