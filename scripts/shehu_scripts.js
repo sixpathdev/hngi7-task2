fad570510b028edc4fcd493e861ada8cff8c4be2
Alli-B <allib2089@gmail.com>
2020-06-04 16:19:12 +0100

    Add to script file Shehu_script in JS

---
+++ scripts/shehu_scripts.js
@@ -0,0 +1,6 @@
+let MyName = "Alli-Balogun Shehu";
+let MyHng_id = "HNG-02278";
+let MyEmail = "allib2089@gmail.com";
+let Mylang = "javaScript";
+
+console.log(`Hello World, this is ${MyName} with HNGi7 ID ${MyHng_id} using ${Mylang} for stage 2 task`);
