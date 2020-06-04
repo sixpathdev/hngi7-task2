0fe403ef855ed4cc34896e308ca65705c806a413
Aaron Damilola <aarondamilola1998@gmail.com>
2020-06-04 10:13:07 -0700

    feat: added Aaron69.js script file

---
+++ scripts/Aaron69.js
@@ -0,0 +1,6 @@
+const name = "Aaron Damilola Seun";
+const hngId = "HNG-O4927";
+const email = "aarondamilola1998@gmail.com";
+const language = "javascript";
+
+console.log(`Hello World, this is ${name} with HNGi7 ID ${hngId} using ${language} for stage 2 task`);
