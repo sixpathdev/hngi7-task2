1d97608efe38c98cc846b7a751d8c69c8cd36053
Mukaila Semiu <semiteagain@gmail.com>
2020-06-04 02:11:05 +0100

    add changes to the name variable

--- scripts/semiteagain.js
+++ scripts/semiteagain.js
@@ -1,6 +1,6 @@
-const name = "Mukaila Semiu";
+const fullNameMy = "Mukaila Semiu";
 const id = "HNG-06255";
 const email = "semiteagain@gmail.com";
 const language = "javascript";
-const output = `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`;
+const output = `Hello World, this is ${fullNameMy} with HNGi7 ID ${id} using ${language} for stage 2 task`;
 console.log(output);
