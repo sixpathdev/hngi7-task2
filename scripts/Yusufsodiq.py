a687861729a6d25855d38eb025c8a01b8f8cd124
surdman-coder <66185908+surdman-coder@users.noreply.github.com>
2020-06-04 02:09:44 +0100

    Create Yusufsodiq.py

---
+++ scripts/Yusufsodiq.py
@@ -0,0 +1,5 @@
+ full_name= 'Yusuf Sodiq'
+ ID= 'HNG-02857'
+ email= 'yusufsodiq22@gmail.com'
+ lang= 'python'
+ print('Hello world, This is '+full_name+' with HNG_ID '+ID+' using the language '+lang+' for task 2 and '+email+' is my email address')
