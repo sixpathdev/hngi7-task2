59a6e988728d9d6d912632c4dbd0add110fc6e09
MfonAbasi Isaac <mfonabasiisaac@gmail.com>
2020-06-04 17:03:30 +0100

    Commiting to the team

---
+++ scripts/mfonabasiisaac.php
@@ -0,0 +1,9 @@
+<?php 
+ 
+  $name = "MFONABASI ISAAC UDOBIA";
+  $id = "HNGi7 ID-06522";
+  $email = "mfonabasiisaac@gmail.com";
+  $language = "php";
+echo 'Hello World, this is  ' . $name  . ' with  ' . $id  . ' using ' .$language  . ' for stage 2 task '. "";
+
+?>
