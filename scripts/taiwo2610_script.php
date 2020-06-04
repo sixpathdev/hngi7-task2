f4376f5b4974a5eaa0fe5cb3879ca3e3c77149a4
Taiwo Agunbiade <66220037+Taiwo2610@users.noreply.github.com>
2020-06-04 07:36:15 -1200

    I added my script.php for task 2

---
+++ scripts/taiwo2610_script.php
@@ -0,0 +1,10 @@
+// My stage 2 task for HNGi7 internship programme
+<?php
+
+  $characterName = "Taiwo Agunbiade";
+  $characterID = "HNG-01266";
+  $characterEmail = "tdagunbiade@gmail.com";
+  $characterLanguage = "php";
+  echo "<h1>Hello World, this is $characterName with HNGi7 ID $characterID using $charcterLanguage for stage 2 task</h1>";
+
+ ?>
