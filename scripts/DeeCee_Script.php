1fa65457ce518e05130e24b8b8ebb4a1721efc67
David-N-Cee <66029306+David-N-Cee@users.noreply.github.com>
2020-06-04 23:40:17 +0100

    It's Not Easy

---
+++ scripts/DeeCee_Script.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$my_emil = "davidchukwuyem@gmail.com";
+$my_name = "David Chukwuyem";
+$hng_id = "HNG-03163";
+$prog_lang = "PHP";
+echo "Hello World, this is " . $my_name . " with HNGi7 ID " . $hng_id . " using " . $prog_lang . " for stage 2 task";
+?>
+</body>
+</html>
