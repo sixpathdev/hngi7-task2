d1733ff3f43962c2198be220319b4e822931ea33
Muhammad UI <muhammadbmw69@gmail.com>
2020-06-04 01:10:41 +0100

    Added muhammad_ui script.js

---
+++ scripts/muhammad_ui.js
@@ -0,0 +1,6 @@
+const name = "Muhammad Ibrahim Umar";
+const hngId = "HNG-01904";
+const email = "muhammadbmw69@gmail.com";
+const language = "Javascript";
+let output = `Hello World, this is ${name} with HNGi7 ID ${hngId} using ${language} for stage 2 task`;
+console.log(output);
