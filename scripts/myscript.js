1d0f8c3618ac7c00f7e90710583ffb7fb18f9881
ansroid <ansledozie@gmail.com>
2020-06-04 20:57:03 +0100

    test: updating myscript with hng-02851 to reflect the new format

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,7 +1,5 @@
-const name = 'Anslem Emeshindu';
-
-const id = 'HNG-02851';
-
 const email = 'ansledozie@gmail.com';
-
-console.log("Hello World,this is "+ Anslem , Emeshindu + " with HNGi7 ID HNG-02851 using JavaScript for stage 2 task")
+const name = 'Anslem Emeshindu';
+const id = 'HNG-02851';
+const language = 'Javascript'
+console.log("Hello World,this is "+Anslem,Emeshindu+" with HNGi7 ID HNG-02851 using JavaScript for stage 2 task")
