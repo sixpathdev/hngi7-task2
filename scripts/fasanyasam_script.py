bc997396ad9de9da644780c7a96325ef8d988f83
fasanyasam <fasanyasam@yahoo.com>
2020-06-04 04:55:54 +0100

    My details

---
+++ scripts/fasanyasam_script.py
@@ -0,0 +1,8 @@
+
+fullname= "Fasanya Samuel"
+hng_id = "HNG-02920"
+language= "Python"
+
+myemail= "fasanyasam@yahoo.com"
+
+print(f"Hello World, this is {fullname} with HNGi7 ID {hng_id} using {language} for stage 2 task.")
