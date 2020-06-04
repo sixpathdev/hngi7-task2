eb3026a00709127ce1f1a1290019995947f695ee
tundeLayan <tunderoxyafolayan@gmail.com>
2020-06-04 18:45:38 +0100

    Afolayan Olatunde's commit for task 2

---
+++ scripts/tundeLayan_script.js
@@ -0,0 +1,8 @@
+const name = "Afolayan Olatunde";
+const id = "HNG-04770";
+const email = "tunderoxyafolayan@gmail.com";
+const language = "Javascript";
+
+const myHngDets = () => `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`;
+
+console.log(myHngDets());
