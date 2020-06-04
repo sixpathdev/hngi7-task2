3fa87c14547e5c632aa72a1a043d5a6ff63b7c89
Kennedykp <46505884+Kennedykp@users.noreply.github.com>
2020-06-04 20:14:39 +0100

    Add files via upload
    kennedy js

---
+++ scripts/kennedy.js
@@ -0,0 +1,11 @@
+let name = 'Kennedy Nwokeocha';
+
+let id = 'HNG-05212';
+
+let email = 'kennedykc55@gmail.com';
+
+let language = 'javascript';
+
+const details = "Hello Everyone, i am " + name + " with HNGi7 ID " + id + " and i am currently using " + language + " for the task on stage 2. ";
+
+console.log(details);
