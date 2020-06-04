7bc3a27974a75117a5383f0ddbadd09b290877de
addy360 <abbyboy360@gmail.com>
2020-06-04 10:32:00 +0300

    Added addy360.js script

---
+++ scripts/addy360.js
@@ -0,0 +1,8 @@
+const full_name = 'Adilu Khalid';
+const hng_id = 'HNG-00673';
+const email = 'adiluabdul@gmail.com';
+const language = 'JavaScript';
+
+console.log(
+  `Hello World, this is ${full_name} with HNGi7 ID ${hng_id} using ${language} for stage 2 task`
+);
