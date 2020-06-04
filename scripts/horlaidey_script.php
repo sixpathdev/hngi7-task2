5053a955fbb658811bfb263353c6444a48d6d883
salaudeen olaidey <salaudeenolaboy@gmail.com>
2020-06-04 16:31:00 +0100

    Update horlaidey_script.php
    Made new change regarding the position of the email variable as instructed

--- scripts/horlaidey_script.php
+++ scripts/horlaidey_script.php
@@ -1,7 +1,7 @@
 <?php
+$email = "salaudeenolaboy@gmail.com";
 $_name = "Salaudeen Taofeek Olaidey";
 $id = "HNG-01019";
-$_email = "salaudeenolaboy@gmail.com";
 $language = "PHP";
 echo "Hello World, this is $_name with HNGi7 ID $id using $language for stage 2 task"
 
