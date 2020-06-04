5e86ddd7a1f570dd42ba30eb67c0d5dbfefe292c
Dministry <dministry356@gmail.com>
2020-06-04 07:19:28 +0100

    dministry-HNG-02488

--- scripts/HNG-02488.js
+++ scripts/HNG-02488.js
@@ -1,7 +1,7 @@
 let name = "Godwin Jacob";
 let id = "HNG-02488";
-let language = "JavaScript"
 let email = "dministry356@gmail.com";
+let language = "JavaScript"
 
 const output = `Hello World!, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`;
 console.log(output);
