fcfce87642641704e84dc4365a863c06891a5e1e
Embruno <emmabruno03@gmail.com>
2020-06-04 01:55:23 -1100

    Brunoem_script.js

---
+++ scripts/Brunoem_script.js
@@ -0,0 +1,5 @@
+const name = 'Bruno Emmanuel';
+const hng_id = 'HNG-02510';
+const email = 'emmabruno03@gmail.com';
+const language = 'javascript';
+console.log("Hello world, this is "+ name +" with HNGi7 "+ hng_id +" using "+ language +" for stage 2 task.")
