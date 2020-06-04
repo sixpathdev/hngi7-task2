54719fed62ca1dee89144cc4ce468bd8409f158a
Aliu Yusuf <aliu.yusuf@outlook.com>
2020-06-04 08:20:36 +0100

    Update my script

---
+++ scripts/yusualiu.php
@@ -0,0 +1,7 @@
+<?php
+$fullname = 'Ololade-Yusuf';
+$id= 'HNG-04383';
+$language = 'PHP';
+
+$data = "Hello World, this is ".$fullname." with HNgI7 ID ".$id." using ".$language ." for stage 2 task";
+echo $data;
