ac7c2761fc1e9780170f4eb7ac160df601ae6f82
realabdullah <abdulodesanmi@gmail.com>
2020-06-04 10:05:18 -0700

    realabdullah.js conforming with the format

--- scripts/realabdullah.js
+++ scripts/realabdullah.js
@@ -1,7 +1,7 @@
 const fullName = 'Abdullahi Odesanmi';
 const id = 'HNG-01943';
-const language = 'JavaScript';
 const mailAddress = 'abdulodesanmi@gmail.com';
+const language = 'JavaScript';
 
 const message = `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task`;
 
