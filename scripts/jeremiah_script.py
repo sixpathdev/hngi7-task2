178fff3821cbfdd35e023a5ff3b1d2e8840b382b
youngxian <38491485+youngxian@users.noreply.github.com>
2020-06-04 21:46:11 +0100

    Update jeremiah_script.py

--- scripts/jeremiah_script.py
+++ scripts/jeremiah_script.py
@@ -4,4 +4,4 @@ Email = 'jeremiahchukwu@gmail.com'
 Language = 'Python'
 
 print("Hello World, this is " + fullname + " with HNGi7 ID " +
-      Hng_id + " using " + Language + " for stage 2 task")
+      Hng_id + " using " + Language + " for stage 2 task email "+Email+"")
