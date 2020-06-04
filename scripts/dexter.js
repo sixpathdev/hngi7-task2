a44800d0627e0a8e6db1899193756ab6234625d7
Dexter-BA <60761711+Dexter-BA@users.noreply.github.com>
2020-06-04 19:42:26 +0100

    Create dexter.js

---
+++ scripts/dexter.js
@@ -0,0 +1,7 @@
+const fullName = 'Babatola Akinpelu';
+const ID = 'HNG-00797';
+const language = 'Javascript';
+const email = 'babatolaakinpelu@gmail.com';
+console.log("Hello World, this" + fullName + "with HNGi7 ID" +ID+ "using" +language+ "for stage 2 task")
+
+ 
