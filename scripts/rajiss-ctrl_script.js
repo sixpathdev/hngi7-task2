108455a41c80a3b8fda1635c156f0e7dda066a82
rajiss-ctrl <omosanjos77@gmail.com>
2020-06-04 13:56:29 -0700

    feat:rajiss-ctrl_HNG-00169script.js

--- scripts/rajiss-ctrl_script.js
+++ scripts/rajiss-ctrl_script.js
@@ -3,6 +3,6 @@ const fullname = 'Raji Sarafadeen Sanjo';
 const hngid = 'HNG-00169';
 const myproglang = 'javascript';
 const email = 'omosanjos77@gmail.com'
-const hello = `Hello World, this is ${fullname} with HNGi7 ID ${hngid} using ${myproglang} for stage 2 task.`;
+const hello = `Hello World, this is ${fullname} with HNGi7 ID ${hngid} using ${myproglang} for stage 2 task email ${email}`;
 
 console.log(hello);
