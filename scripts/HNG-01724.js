d926ef9f0ccc4595fc685a7d30ed7b3111fe7c54
Arisekolat <64399351+Arisekolat@users.noreply.github.com>
2020-06-04 10:18:11 +0100

    Update HNG-01724.js

--- scripts/HNG-01724.js
+++ scripts/HNG-01724.js
@@ -2,5 +2,5 @@ const fullname = "Azeez Sunday";
 const hngid = "HNG-01724";
 const email = "azeezsunday.a@gmail.com";
 const language = "javascript";
-const my_output = `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`;
+const my_output = `Hello World, this is ${fullname} with HNGi7 ID ${hngid} using ${language} for stage 2 task`;
 console.log(my_output);
