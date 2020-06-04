72004c34e1bfaa8f14f8eac7006f03dfe2ac6379
Nwokoye Chigozie <gregoflash05@users.noreply.github.com>
2020-06-04 08:23:12 +0100

    Create greg.php
    added greg.php file

---
+++ scripts/greg.php
@@ -0,0 +1,10 @@
+<?php
+$name='Nwokoye Chigozie';
+$id='02811';
+$programming_language= 'PHP';
+$email='gregoflash05@gmail.com';
+
+echo "Hello World, this is $name with HNGi7 ID HNG-$id using $programming_language for stage 2 task";
+
+
+?>
