bebd0c4f7f5a4c52658a37fdd40191d06abf8a1b
wisdomosara <50260832+wisdomosara@users.noreply.github.com>
2020-06-04 22:03:27 +0100

    Update wisdomosarascript.js

--- scripts/wisdomosarascript.js
+++ scripts/wisdomosarascript.js
@@ -4,6 +4,6 @@ let id = "HNG-04550";
 let language = "JavaScript";
 
 function getGreeting(){
-  return `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task.`
+  return `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task email ${email}`
 }
 console.log(getGreeting())
