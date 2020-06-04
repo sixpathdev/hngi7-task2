efc7cdfbe8f8af561de0742e8f37c959df6c293d
Mildness10 <66227187+Mildness10@users.noreply.github.com>
2020-06-04 03:15:32 +0200

    Add files via upload

---
+++ scripts/mildness_script.js
@@ -0,0 +1,5 @@
+const name ='Mildness Akomoize';
+const id ='03014';
+const email ='m.akomoize@gmail.com';
+const language ='javascript';
+console.log("Hello World, this is "+name+" with HNGi7_id "+id+" using "+language+" for stage 2 task")
