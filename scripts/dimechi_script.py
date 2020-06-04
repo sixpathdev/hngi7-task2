32b728f90a61a3b16384df4d8543bb755773c75a
dimechi <afolabinasifudeen@gmail.com>
2020-06-04 09:01:29 +0100

    feature: I added dimechi_script.py

---
+++ scripts/dimechi_script.py
@@ -0,0 +1,6 @@
+name="Afolabi Nasifudeen Oladimeji"
+id="HNG-05658"
+language="Python"
+email="afolabinasifudeen@gmail.com"
+output="Hello World, this is "+name+" with HNGi7 ID "+id+" using "+language+" for stage 2 task"
+print(output)
