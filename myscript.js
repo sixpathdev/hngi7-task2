986957d51c934f5c2de89cc9564850dbff727315
ansroid <ansledozie@gmail.com>
2020-06-04 18:44:30 +0100

    test: implementing my task in javascript

---
+++ myscript.js
@@ -0,0 +1,7 @@
+const name = 'Anslem Emeshindu';
+
+const id = 'HNG-02851';
+
+const email = 'ansledozie@gmail.com';
+
+console.log("Hello World,this is "+ Anslem , Emeshindu + " with HNGi7 ID HNG-02851 using JavaScript for stage 2 task")
