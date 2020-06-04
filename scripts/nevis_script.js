70ab046032c3b5524a8693077f354ae49093b70a
nevis-adhiambo <44494861+nevis-adhiambo@users.noreply.github.com>
2020-06-04 17:23:05 +0300

    Add files via upload

---
+++ scripts/nevis_script.js
@@ -0,0 +1,6 @@
+var name = "Nevis Adhiambo";
+var id = "HNG-03656";
+var email = "nevisadhiambo71@gmail.com"
+var language = "javascript";
+var output = ("Hello world, this is" + " " + name + " " + "with HNGi7 ID" + " " + id + " " + "using" + " " + language + " " + "for stage 2 task");
+console.log(output);
