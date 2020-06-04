73dcf6097192623d5187b9127e2a4e9403352706
Eldrige <52784114+eldrige@users.noreply.github.com>
2020-06-04 17:01:10 +0100

    Added my name (Apoh Eldrige) to the scripts folder

---
+++ scripts/apoh_ELdrige.js
@@ -0,0 +1,10 @@
+const info = ()=>{
+    let userName = 'Apoh Eldrige'
+    let internId = 'HNG-02427'
+    let emailAddress = 'peldrige8@gmail.com'
+    let language = 'Javascript'
+
+    return `Hello World, this is ${userName} with HNGi7 ID ${internId} using ${language} for stae 2 task`
+}
+
+console.log(info())
