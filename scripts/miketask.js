de3423f85f686583cf257e0e04f2a1276222c70a
Oluwaseun Michael Oluwatomisin <62293658+ooluwaseunmicheal@users.noreply.github.com>
2020-06-04 17:01:56 +0100

    Initial commit

---
+++ scripts/miketask.js
@@ -0,0 +1,16 @@
+ 
+// Declaring Object Variable
+var mikeTask = {
+                Name: "Oluwatomisin Oluwaseun Michael",
+                ID: "HNG-04643",
+                Email: "ooluwaseunmicheal@gmail.com",
+                Language: "JavaScript",
+                Stage: 2
+            }
+
+//output of result
+
+console.log("Hello World, this is  " +mikeTask.Name +
+            " with HNGi7 ID " +mikeTask.ID +
+            " using " +mikeTask.Language + " for Stage "
+            +mikeTask.Stage + " task");
