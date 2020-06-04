e5867b297e4021d02e2493b8326ba036eccf59e8
Fadare Victoria Ayomide <60330687+Ayomide26@users.noreply.github.com>
2020-06-04 12:31:49 +0100

    Add files via upload
    Ayomide06 script commit

---
+++ scripts/Ayomide061 (1).py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+FULLNAME = 'Fadare Victoria Ayomide'
+HNGID = 'HNG-02519'
+LANGUAGE = 'python'
+EMAIL = 'ayomidefadare26@gmail.com'
+print('Hello World, this is '+ FULLNAME, 'with HNGi7 ID ' + HNGID, 'using '+ LANGUAGE, 'for stage 2 task.' )
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
