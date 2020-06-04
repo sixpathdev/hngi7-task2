eb67d652fdd947b786d5a15cf944afa96c26be8f
Taiwo Agunbiade <66220037+Taiwo2610@users.noreply.github.com>
2020-06-03 22:36:52 -1200

    added Taiwo2610_script

--- scripts/Taiwo2610_srcript.php
+++ scripts/Taiwo2610_srcript.php
@@ -1,20 +1,8 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
 
-      <?php
+  <?php
           $characterName = "Taiwo Agunbiade";
           $characterNum = "HNG-02166";
           $characterEmail = "tdagunbiade@gmail.com";
           $characterLanguage = "php";
-          echo "<h1>Hello World, this is $characterName with HNGi7 ID:$characterNum using $characterLanguage for stage 2 task. </h1>";
-
-       ?>
-
-
-  </body>
-</html>
+          echo "<h1>Hello World, this is $characterName with HNGi7 ID $characterNum using $characterLanguage for stage 2 task. </h1>";
+   ?>
