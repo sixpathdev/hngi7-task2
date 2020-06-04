48910e37fda0d7301256f708e2d55ad7fa390d31
Adegboye Isaac <isaac.adegboye@yahoo.com>
2020-06-05 00:35:46 +0100

    Update iOreoluwa.php

--- scripts/iOreoluwa.php
+++ scripts/iOreoluwa.php
@@ -1,13 +1,14 @@
 <?php
 
-   $name = "Isaac Adegboye";
-   $hng_id = "HNG-03357";
-   $email = "isaac.adegboye@yahoo.com";
-   $programming_lang = "PHP";
+$name = "Isaac Adegboye";
+$hng_id = "HNG-03357";
+$email = "isaac.adegboye@yahoo.com";
+$my_lang = "PHP";
+ 
 
-function introduction($myName, $id, $myEmail, $myLang) {
+function introduction($myName, $id, $myLang, $myEmail) {
     
-    return "Hello World, this is " .$myName. " with HNGi7 ID " .$id . " using ".$myLang." for stage 2 task";
+    return "Hello World, this is " .$myName. " with HNGi7 ID " .$id . " using ".$myLang." for stage 2 task email ".$myEmail."";
 }
 
-print(introduction($name, $hng_id, $email, $programming_lang));
+print(introduction($name,$hng_id,$my_lang, $email));
