8ec322e18821b28f77d416322e1e28cf1802e468
ken.P <kennethu30@gmail.com>
2020-06-04 08:01:56 +0100

    Create myJavaScript.js

---
+++ scripts/myJavaScript.js
@@ -0,0 +1,6 @@
+var myName = 'Udeagbara Kenneth';
+var myId = 'HNG-06421';
+const myLanguage = 'javascript';
+const email = 'kennethu30@gmail.com';
+
+console.log('Hello World, this is ' +myName+ ' with HNGi7 ID ' + myId+ ' using ' + myLanguage+ ' for stage 2 task');
