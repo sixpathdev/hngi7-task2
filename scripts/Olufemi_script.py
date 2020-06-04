e0b90942ec5c6f91081a1bfe88f3e8723a0216a8
Olufemi Odedeyi <olufemiodedeyi@gmail.com>
2020-06-04 02:03:55 +0100

    Final changes: Format followed

--- scripts/Olufemi_script.py
+++ scripts/Olufemi_script.py
@@ -1,8 +1,10 @@
 def my_script():
-    fullname = "Odedeyi Olufemi"
-    ID = "HNG-01684"
-    lang = "Python"
-    email = "olufemiodedeyi@gmail.com"
 
-    print("Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task".format(fullname, ID, lang))
+    my_fullname = "Odedeyi Olufemi"
+    my_ID = "01684"
+    email_add = "olufemiodedeyi@gmail.com"
+    my_lang = "Python"
+ 
+    print("Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task".format(my_fullname, my_ID, my_lang))
+
 my_script()
