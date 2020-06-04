fe2fff4fb136c6fb57a1fb8f149b3b4c9716c4da
Oyewale Kehinde <42590579+oyewalekehinde@users.noreply.github.com>
2020-06-04 22:04:58 +0100

    Kehinde1999_script

---
+++ scripts/Kehinde1999.js
@@ -0,0 +1,19 @@
+//declare the variables
+
+const fName = "[Kehinde]";
+
+const lName = "[Oyewale]";
+
+const hngId = "[HNG-05604]";
+
+const language = "[JavaScript]";    
+
+const email= "oyewalekehinde34@gmail.com";
+
+    
+
+//output
+
+console.log(`Hello World, this is ${fName} ${lName} with HNGi7 ID ${hngId} using ${language} for stage 2 task. ${email}`);
+
+© 2020 GitHub, Inc.
