f9fbb17686fbb48d7db62d5a4c38236fdb4d3326
Emmajoe47 <54849066+Emmajoe47@users.noreply.github.com>
2020-06-04 21:17:56 +0100

    Update EmmaEfe-Task2.py

--- scripts/EmmaEfe-Task2.py
+++ scripts/EmmaEfe-Task2.py
@@ -1,6 +1,7 @@
 email = "mrefe2017@gmail.com"
 myName = "Emmanuel Efe"
 hngboard_id = "HNG-05454"
+email = "mrefe2017@gmail.com"
 language = "Python"
 
 print ("Hello World, this is " + myName " with with HNGi7 ID " + hngboard_id + " using " + language + " for stage 2 task ")
