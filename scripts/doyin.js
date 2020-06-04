3636a2d3eb675e51d0907ddff8aa8f93acd1723a
Doyin Awosanya <didoloan@gmail.com>
2020-06-04 14:32:05 +0100

    moved email to 1st line

--- scripts/doyin.js
+++ scripts/doyin.js
@@ -1,6 +1,6 @@
+const email     =   'didoloan@gmail.com';
 const fullname  =   'Awosanya Awosanya';
 const hngid     =   'HNG-02674';
-const email     =   'didoloan@gmail.com';
 const language  =   'JavaScript';
 
 console.log(`Hello World, this is ${fullname} with HNGi7 ID ${hngid} using ${language} for stage 2 task.`);
