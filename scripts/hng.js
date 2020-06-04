9d0bac246c9336cc2a4fed9ef3c2823808096c21
Abod <56658666+abodmicheal@users.noreply.github.com>
2020-06-04 12:11:59 +0100

    my script file

---
+++ scripts/hng.js
@@ -0,0 +1,11 @@
+let fullname = 'Abod micheal';
+
+let email = 'abodmicheal@gmail.com';
+
+let language = 'javascript';
+
+let hngid = 'HNG-04597';
+
+let result = "Hello World, I am " + fullname + " with HNGi7 ID: " + hnhid + " and I am using " + language  + " for the task on stage 2 ";
+
+console.log(result);
