d634c1cad13ca5d387ebfb2cbc8e2fd7d0b961e3
Mveliso Mtana <36462899+Mveliso42@users.noreply.github.com>
2020-06-04 09:18:53 +0200

    m_mtana.py
    Update

--- scripts/mmtana.py
+++ scripts/mmtana.py
@@ -3,4 +3,4 @@ hng_id = 'HNG - 02711'
 email = 'mtanamveliso@gmail.com'
 language = 'python'
 
-print('Hello World, this is' + ' ' + name + ' ' + 'with HNGi7 ID' +' '+ hng_id +' ' + 'using' +' '+ language +' '+ 'for stage 2 task.')
+print('Hello World, this is' + ' ' + name + ' ' + 'with HNGi7 ID' +' '+ hng_id +' '+ 'using' +' '+ language +' '+ 'for stage 2 task.')
