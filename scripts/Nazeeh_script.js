0beaf5a925ad3538a69238708ebaa814325f2e85
nazeeeh <43985887+nazeeeh@users.noreply.github.com>
2020-06-04 21:33:12 +0100

    email added to console

--- scripts/Nazeeh_script.js
+++ scripts/Nazeeh_script.js
@@ -4,12 +4,13 @@
   
 const profiles = 
       {
-        email: "nazihahmed55@gmail.com",
+        
         name: "Nazih Ajanah",
         id: "HNG-02408",
         language: "JavaScript",
+        email: "nazihahmed55@gmail.com",
       }
 
-console.log("Hello World, This is " + profiles.name + " with HNGi7 ID " + profiles.id + " using " + profiles.language + " for stage 2 task");
+console.log("Hello World, This is " + profiles.name + " with HNGi7 ID " + profiles.id + " using " + profiles.language + " for stage 2 task with email " + profiles.email);
 
 </script>
