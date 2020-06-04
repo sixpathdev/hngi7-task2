82e5eb616dde62cb54accf13fc3eb0d1e20fbdd8
Doyin Awosanya <didoloan@gmail.com>
2020-06-04 10:40:32 +0100

    Doyin modified his script

--- scripts/doyin.js
+++ scripts/doyin.js
@@ -3,4 +3,4 @@ const hngid     =   'HNG-02674';
 const email     =   'didoloan@gmail.com';
 const language  =   'JavaScript';
 
-console.log(`Hello World, this is ${fullname} with HNGi7 ID ${hngid} using ${language} for stage 2 task`);
+console.log(`Hello World, this is ${fullname} with HNGi7 ID ${hngid} using ${language} for stage 2 task.`);
