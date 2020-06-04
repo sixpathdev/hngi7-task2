695b5e0d92b0421c35a33244c3c3eb9289a15327
Saidox19 <47587886+Saidox19@users.noreply.github.com>
2020-06-04 02:32:17 +0100

    Added saheed_script to the file
    I wrote a python code stating my name, hng_id, and language

---
+++ scripts/saheed_script.py
@@ -0,0 +1,7 @@
+name = 'Abdulahi Saheed'
+hng_id = 'HNG-02306'
+email = 'abdulahisaheed001@gmail.com'
+language = 'python'
+
+output = f'Hello World, this is {name} with HNGi7 ID {hng_id} using {language} for stage 2 task'
+print(output)
