ae55eebe50054d92975fda76ed470c0f29b62f09
okafohenrie <okafohenrie@gmail.com>
2020-06-04 14:24:50 +0100

    made modifications to syntax in Henry_oc.js

--- scripts/Henry_oc.js
+++ scripts/Henry_oc.js
@@ -1,9 +1,11 @@
-const intro = (name, hng_id, lang) => {  
-	return console.log(`Hello World, this is ${name} with HNGi7 ID ${hng_id} using ${lang} for stage 2 task`
-	);
-}
+const name, hng_id, lang, email;
+name = 'Okafor Henry';
+hng_id = 'HNG-06037';
+language = 'Javascript';
+email = 'okafohenrie@gmail.com';
 
-intro('Okafor Henry', 'HNG-06037', 'Javascript');
+ console.log(`Hello World, this is ${name} with HNGi7 ID ${hng_id} using ${lang} for stage 2 task`);
+	
 
 
 
