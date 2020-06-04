c529fc8c1f00ffca247bd8b17c23dcdccf5441dd
sjdammy <dami.seweje@cicod.com>
2020-06-04 16:01:25 +0100

    changed email to first variable

--- scripts/HNG-01034.js
+++ scripts/HNG-01034.js
@@ -1,5 +1,6 @@
-const name = 'Seweje Damilola',
+const
   email = 'sewejed@gmail.com',
+  name = 'Seweje Damilola',
   hngId = 'HNG-01034',
   language = 'JavaScript',
   output = `Hello World, this is ${name} with HNGi7 ID ${hngId} using ${language} for stage 2 task`;
