763ffba081ecbe68927d3776fde4b99d319a9820
MfonAbasi Isaac <mfonabasiisaac@gmail.com>
2020-06-04 18:44:55 +0100

    editing my mail and name

--- scripts/mfonabasiisaac.php
+++ scripts/mfonabasiisaac.php
@@ -1,8 +1,7 @@
 <?php 
- 
-  $name = "MFONABASI ISAAC UDOBIA";
-  $id = "HNGi7 ID-06522";
   $email = "mfonabasiisaac@gmail.com";
+  $name = "MFONABASI UDOBIA";
+  $id = "HNGi7 ID-06522";
   $language = "php";
 echo 'Hello World, this is  ' . $name  . ' with  ' . $id  . ' using ' .$language  . ' for stage 2 task '. "";
 
