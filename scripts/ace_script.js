4c9caafde3dae8d4052c7a91b3acecabd5cca4aa
trini7y <okekedesmond71@gmail.com>
2020-06-04 11:14:52 +0100

    Trini7y added a script

---
+++ scripts/ace_script.js
@@ -0,0 +1,10 @@
+const task = () => {
+    const fullName = "Nwafor Kenechukwu";
+    const id       = "HNG-03576";
+    const email    = "Kenechukwu66@gmail.com";
+    const language = "JavaScript";
+
+    return `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task.`;
+}
+
+console.log(task());
