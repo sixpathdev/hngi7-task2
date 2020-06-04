9c1550313293234305f0d75d939064a3407bcbdd
obute-agbo <obutemoses5@gmail.com>
2020-06-04 21:41:48 +0100

    'mosiac05_added_email_to_output'

--- scripts/mosiac05.php
+++ scripts/mosiac05.php
@@ -5,6 +5,6 @@ $name='Obute Moses';
 $id='HNG-05246';
 $language='PHP';
 
-echo "Hello World, this is " . $name . " with HNGi7 ID ". $id ." using ".$language." for stage 2 task";
+echo "Hello World, this is " . $name . " with HNGi7 ID ". $id ." using ".$language." for stage 2 task email " . $email;
 
 ?>
