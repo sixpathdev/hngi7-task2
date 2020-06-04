3afcdab77757fee4cfbbd23ca48d5f8fc3de59b2
Gospel Theodore Orok <51316483+orokgospel@users.noreply.github.com>
2020-06-05 00:24:03 +0100

    adding my task 2 script

---
+++ scripts/orokgospelHNGi7.py
@@ -0,0 +1,5 @@
+my_name= "Gospel Orok"
+language_used= "python"
+HNG_ID="HND-02122"
+
+print("Hello world this is", my_name ,"with HNGi7 ID:", HNG_ID, "using ", language_used, "for Stage2 Task",flush=True)
