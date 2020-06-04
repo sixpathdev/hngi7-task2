8af1e6f96b1f33f90396b6ea1f1f6c17a70e7541
trini7y <okekedesmond71@gmail.com>
2020-06-04 20:14:50 +0100

    Corrected the output

--- scripts/HNG-0290.py
+++ scripts/HNG-0290.py
@@ -1,6 +1,6 @@
-fullName ="Okeke Desmond"
-HNGboard_Id ="HNG-0290"
+name ="Okeke Desmond"
+ID ="HNG-0290"
 email="okekedesmond71@gmail.com"
 language ="Python"
 
-print("Hello World, this is {0}, with HNGi7 ID {1} and email {2} using {3} for stage 2 task".format(fullName, HNGboard_Id, email, language))
+print("Hello World, this is {0}, with HNGi7 ID {1} using {2} for stage 2 task".format(name, ID, language))
