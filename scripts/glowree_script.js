9c17c5ec1e07a1c0633a73b57f43b2d8ca5b805d
Glory tefe <glorytefe@gmail.com>
2020-06-03 14:03:54 -1200

    first commit

--- scripts/glowree_script.js
+++ scripts/glowree_script.js
@@ -1,1 +1,6 @@
-<h2>"Hello World, this is Mebuiefene Tefe Glory with HNGi7 ID HNG-02840 using php for stage 2 task".</h2>
+let fullName = 'Mebuiefene Tefe Glory';
+let id = 'HNG-02840';
+let language = 'JavaScript';
+let output = `Hello World, this is ${fullName} with ${id} using ${language} to solve stage 2 task.`;
+
+console.log(output);
