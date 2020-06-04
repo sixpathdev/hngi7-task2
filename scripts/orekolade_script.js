163e5adb7f8b6e4041942a8d0cfd7ef013494665
koladeore <koladeore@gmail.com>
2020-06-04 10:52:18 +0100

    added-myscript

---
+++ scripts/orekolade_script.js
@@ -0,0 +1,10 @@
+const name = 'kolade ore';
+const hngId = 'HNG-06523';
+const email = 'koladeore@gmail.com';
+const language = 'javascript';
+
+const output = () => {
+  return `Hello World, this is ${name} with HNGi7 ID ${hngId} using ${language} for stage 2 task`
+}
+
+console.log(output())
