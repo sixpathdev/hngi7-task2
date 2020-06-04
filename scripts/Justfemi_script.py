a1b3c7d6733fdd0346f978ff32cb28966c41cd83
Ajibade Olufemi <ajibadeolufemi94@gmail.com>
2020-06-04 20:43:02 +0100

    Latest commit

--- scripts/Justfemi_script.py
+++ scripts/Justfemi_script.py
@@ -1,6 +1,6 @@
+email = "ajibadeolufemi94@gmail.com"
 fullName = "Ajibade Olufemi"
 ID = "HNG-01458"
-email = "ajibadeolufemi94@gmail.com"
 language = "python"
 
 print(f"Hello World, this is {fullName} with HNGi7 ID {ID} using {language} for stage 2 task.")
