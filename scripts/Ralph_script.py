1494f03f9d5a3b2354db8688c52cce86d0723017
raph941 <raph941@gmail.com>
2020-06-04 06:16:55 +0100

    fix: modified Ralph_script.py

--- scripts/Ralph_script.py
+++ scripts/Ralph_script.py
@@ -1,7 +1,7 @@
 full_name = "Ehindero Raphael Ayo"
 hng_id = "HNG-00314"
-language = "python"
 email = "raph941@gmail.com"
+language = "python"
 
 output = f'Hello World, this is {full_name} with HNGi7 ID {hng_id} using {language} for stage 2 task'
 
