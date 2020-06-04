55f9048f523a3c01d6625e24b2a3a3dc70dcd4c5
halimatsadiat <66258094+halimatsadiat@users.noreply.github.com>
2020-06-04 12:03:11 +0100

    Update ogidanscript.js

--- scripts/ogidanscript.js
+++ scripts/ogidanscript.js
@@ -1,7 +1,7 @@
 var info = [];
 info["myFullName"] = "Ogidan Sadiat";
 info["ID"] = "HNG-00834";
-info["language"] = "Javascript"
+info["language"] = "Javascript";
 var email = "haleymaogidan@gmail.com";
 
 console.log("Hello World, this is " + info["myFullName"] + " with HNGi7 ID " + info["ID"] + " using " + info["language"] + " for stage 2 task");
