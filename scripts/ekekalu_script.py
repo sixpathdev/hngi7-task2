b96415f66db56e19dd60a2d22e7a8ace9a952414
storyengineer <66221766+storyengineer@users.noreply.github.com>
2020-06-04 12:02:35 -0700

    Delete ekekalu_script.py

--- scripts/ekekalu_script.py
+++
@@ -1,6 +0,0 @@
-name ="Eke Kalu"
-hngid ="HNG-01097"
-email ="ekendukwe@gmail.com"
-language ="python"
-
-print("Hello World" + "," + " this is " + name + " with HNGi7 ID " + hngid + " using " + language + " for stage 2 task")
