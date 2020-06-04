c61ff54b0db739bcd071f56fb65127e3debf0444
KAZEEM <kazeemnasiff@gmail.com>
2020-06-04 07:29:26 +0100

    Changed from document.write to console.log

--- scripts/nadheefGit_script.js
+++ scripts/nadheefGit_script.js
@@ -3,12 +3,12 @@ const intern={
 	firstName: "Kazeem",
 	lastName:"Nasiff",
 	id:"HNG-03415",
-	language: "javascript",
-	email: "kazeemnasiff@gmail.com"
-}
+	email: "kazeemnasiff@gmail.com",
+	language: "javascript"
+	}
 console.log(intern)
 	const fullname=intern.firstName+" "+intern.lastName;
 	const id=intern.id;
 	const language=intern.language; 
 
-document.write ("Hello world!"+" "+ "I am"+" "+fullname+","+" "+"ID of:"+" "+id+" "+"using"+" "+language+" "+"for stage 2 task");
+console.log("Hello world!"+" "+ "I am"+" "+fullname+","+" "+"ID of:"+" "+id+" "+"using"+" "+language+" "+"for stage 2 task");
