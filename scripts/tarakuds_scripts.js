8cbd82055d357e39ecbbf5ef007766b5a7468038
tarakuds <taradebby@gmail.com>
2020-06-04 20:29:48 +0100

    new

---
+++ scripts/tarakuds_scripts.js
@@ -0,0 +1,13 @@
+var email;
+const fullname = "Omotara Kudehinbu";
+const id = "HNG-04808";
+const language = "javaScript";
+
+console.log(
+  "Hello World. My name is " +
+    fullname +
+    " with id: " +
+    id +
+    "using " +
+    language
+);
