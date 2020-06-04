5ef9e2bce94af149b5da4aaf6bce084a83ce1a3c
Akpan Godswill <godswillyhurhighnezz@gmail.com>
2020-06-04 20:32:20 +0100

    Reviewed Script, and created variables

--- scripts/Task2.py
+++ scripts/Task2.py
@@ -1,1 +1,5 @@
-print('Hello World, this is Akpan Godswill with HNGi7 ID HNG-01956 using Python for stage 2 task')
+Name = 'Akpan Godswill'
+HNGID = 'HNG-01956'
+language = 'Python'
+email='godswillyhurhighnezz@gmail.com'
+print('Hello World, this is' + ' '+ Name + ' ' + 'with HNGi7 ID' + ' '+ HNGID +' '+ 'using'+' '+ language +' '+ 'for stage 2 task.'+email)
