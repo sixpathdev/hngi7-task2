cb918f70e6431ed634ffffa5912496ef0d71e28c
Muritala David <54484130+Fn-studyo@users.noreply.github.com>
2020-06-04 10:59:23 +0100

    changes to script file

--- scripts/davmuri_script.py
+++ scripts/davmuri_script.py
@@ -1,1 +1,9 @@
-print('Hello world, this is Muritala David with HNGI7 ID: HNG-03698 and email davmuri1414@gmail.com using Python for stage 2 task')
+name = 'Muritala David'
+hng_id = 'HNG-03698'
+email = 'davmuri1414@gmail.com'
+language = 'Python'
+output = 'Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task.' %(name,hng_id,language)
+
+print(output)
+
+
