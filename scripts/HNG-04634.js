9bac5d18c94b4ddb851545604aedbee088abddd3
Olawale <olawalearcee@gmail.com>
2020-06-04 18:57:12 +0100

    update to script

--- scripts/HNG-04634.js
+++ scripts/HNG-04634.js
@@ -1,6 +1,6 @@
 let fullName = 'Olawale Akewushola';
-let hngId = 'HNG-04634';
+let hngId = '04634';
 let email = 'olawalearcee@gmail.com';
 let language = 'javascript'; 
 
-console.log(`Hello World, this is ${fullName} with HNGi7 ID ${hngId} using ${language} for stage 2 task.`);
+console.log(`Hello World, this is ${fullName} with HNGi7 ID ${hngId} and email ${email} using ${language} for stage 2 task.`);
