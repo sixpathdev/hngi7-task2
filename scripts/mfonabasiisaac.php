9818a1c614471f0547b08a426adcb55685fc790f
MfonAbasi Isaac <mfonabasiisaac@gmail.com>
2020-06-04 19:35:56 +0100

    update

---
+++ scripts/mfonabasiisaac.php
@@ -0,0 +1,6 @@
+<?php
+$email = "mfonabasiisaac@gmail.com";
+$name = "MfonAbasi Udobia";
+$id = "HNG-06522";
+$language = "PHP";
+echo "Hello World, this is " . $name . " with HNGi7 ID"." ".$id." "."and email"." " .$email." "."using ".$language." for stage 2 task";
