262dd628f94cfd4189b6ea337165386ee90afe1f
dazzy4real2000 <55817132+dazzy4real2000@users.noreply.github.com>
2020-06-04 20:34:19 +0100

    Create nwakuba.js

---
+++ scripts/nwakuba.js
@@ -0,0 +1,6 @@
+var fullName = "Nwakuba Chidozie";
+var hng_id = "HNG-00788";
+var email = "nwakubachidozie@gmail.com";
+var language = "Javascript";
+
+console.log(`Hello World, this is ${fullName} with HNGi7 ID ${hng_id} using Javascript for stage 2 task`);
