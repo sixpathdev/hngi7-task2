887b09a56222b3cdf5eddcbb5285d1eb11f13a66
Abdulfatah Ayodele <66224802+ShokunbiAyodele@users.noreply.github.com>
2020-06-05 00:52:26 +0100

    Update abdul_script.php

--- scripts/abdul_script.php
+++ scripts/abdul_script.php
@@ -4,7 +4,7 @@ function Hngi7_intro(){
     $hngi7_id = "HNG-02499";
     $language = "PHP";
     $email = "shokunbiaa@gmail.com";
-    $output = " Hello World,". " this is " . $full_name . " with HNGi7 ID " . $hngi7_id . " using ". $language . " for stage 2 task";
+    $output = " Hello World,". " this is " . $full_name . " with HNGi7 ID " . $hngi7_id . " using ". $language . " for stage 2 task" . " email " . $email;
     return $output;
 }
 echo Hngi7_intro();
