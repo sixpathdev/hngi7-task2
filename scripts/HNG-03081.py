821ce1850b1d8ba861228e626b313703158e4fd9
Oluwaseyi Ayomide <49010334+Ayomide100@users.noreply.github.com>
2020-06-04 23:57:52 +0100

    Add files via upload

---
+++ scripts/HNG-03081.py
@@ -0,0 +1,6 @@
+name = "Fasakin Oluwaseyi Ayomide"
+hng_id="HNG-03081"
+language="python"
+email="royaltechpriest@gmail.com"
+
+print("Hello World, this is ", name ,"with HNGi7 ID",hng_id,"using",language,"for the stage 2 task email",email)
