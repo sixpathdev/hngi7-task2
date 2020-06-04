f96da84d4871670061b4bf2ebaaae2ba724b62e2
Fadare Victoria Ayomide <60330687+Ayomide26@users.noreply.github.com>
2020-06-04 12:03:19 -0400

    Update Ayomide061 (1).py
    Edited file (Ayomide061)

--- scripts/Ayomide061 (1).py
+++ scripts/Ayomide061 (1).py
@@ -3,12 +3,11 @@
 
 # In[1]:
 
-
-FULLNAME = 'Fadare Victoria Ayomide'
-HNGID = 'HNG-02519'
-LANGUAGE = 'python'
 EMAIL = 'ayomidefadare26@gmail.com'
-print('Hello World, this is '+ FULLNAME, 'with HNGi7 ID ' + HNGID, 'using '+ LANGUAGE, 'for stage 2 task.' )
+FULLNAME = 'Victoria Fadare'
+HNGID = 'HNG-02519'
+LANGUAGE = 'Python'
+print('Hello World, this is '+ FULLNAME, 'with HNGi7 ID ' + HNGID, 'using '+ LANGUAGE, 'for stage 2 task' )
 
 
 # In[ ]:
