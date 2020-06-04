e8438fd780ab95e4d4778af1e9e89115fca064f3
inioluwa25-hub <inioluwaraheem@gmail.com>
2020-06-04 20:52:21 +0100

    test

--- scripts/raheem_script.js
+++ scripts/raheem_script.js
@@ -1,7 +1,7 @@
 const task = () => {
-	const fullName = "Raheem Inioluwa";
+const email    = "inioluwaraheem@gmail.com";
+	const fullName    = "Raheem Inioluwa";
 	const id       = "HNG-02235";
-	const email    = "inioluwaraheem@gmail.com";
 	const language = "javascript";
 	
 	return `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task`;
