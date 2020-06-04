82462f14334a06ad053ddd74f8c3592930f137c6
oluwaseun <jidielori77@gmail.com>
2020-06-04 11:24:33 +0100

    script for task2

---
+++ scripts/oluwaseun_script.py
@@ -0,0 +1,6 @@
+name = "Oluwaseun Ilori"
+Id ="HNG-04216"
+email="jideilori77@gmail.com"
+language="python"
+
+print(f"Hello World this is {name} with HNGi7 ID:{Id} and email: {email} using {language} for stage 2 task")
