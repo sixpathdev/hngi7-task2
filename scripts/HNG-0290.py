9c6076f95d4d15126bcebf6f2b9348ceb3c00c2d
trini7y <okekedesmond71@gmail.com>
2020-06-04 11:21:51 +0100

    trini7ys script

--- scripts/HNG-0290.py
+++ scripts/HNG-0290.py
@@ -1,6 +1,6 @@
-Full_Name ="Okeke Desmond"
+fullName ="Okeke Desmond"
 HNGboard_Id ="HNG-0290"
-my_EmailAddress="okekedesmond71@gmail.com"
-language_Used ="Python"
+email="okekedesmond71@gmail.com"
+language ="Python"
 
-print("Hello World, this is {0}, with HNGi7 ID {1} and email {2} using {3} for stage 2 task".format(Full_Name, HNGboard_Id, my_EmailAddress, language_Used))
+print("Hello World, this is {0}, with HNGi7 ID {1} and email {2} using {3} for stage 2 task".format(fullName, HNGboard_Id, email, language))
