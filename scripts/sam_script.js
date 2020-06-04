ff53602bb4c3e10db59a6cd652ca6d6e06883905
obaeko <66390406+obaeko@users.noreply.github.com>
2020-06-04 10:19:58 +0100

    Add files via upload

---
+++ scripts/sam_script.js
@@ -0,0 +1,10 @@
+const name = "Olumuyiwa Adeniyi";
+const hng_id = "HNG-06622";
+const language = "Javascript";
+const email = "oadeniyi45@gmail.com";
+
+function myHngDetails() {
+    return `Hello World, this is ${name} with HNGi7 ID ${hng_id} using ${language} for stage 2 task.`;
+}
+
+console.log(myHngDetails());
