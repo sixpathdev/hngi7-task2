ac344b89680e9917041282fbfa627a7583170a76
Onyinteldev <66255427+Onyinteldev@users.noreply.github.com>
2020-06-04 07:56:21 +0100

    Delete Onyekachukwu_OdenigboScript.py

--- scripts/Onyekachukwu_OdenigboScript.py
+++
@@ -1,6 +0,0 @@
-name = 'Onyekachukwu Odenigbo'
-id = 'HNG-02748'
-language = 'Python'
-email = 'onyekagodenigbo@gmail.com'
-
-print(f'Hello World, this is {name} with HNGi7 ID {id} using {language} for stage 2 task.')
