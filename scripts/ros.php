4400494b7ac24bed8c1b80655ce9f5138969d33f
Maryrose Okafor <okaformaryrose7@gmail.com>
2020-06-04 14:40:13 -0700

    upadated file

--- scripts/ros.php
+++ scripts/ros.php
@@ -1,3 +1,9 @@
 <?php
-echo "Hello World, this is Maryrose Okafor with  HNGi7 ID HNG-02777 using PHP for stage 2 task email okaformaryrose7@gmail.com";
+
+$email = "okaformaryrose7@gmail.com";
+$name = "Maryrose Okafor";
+$hngboard_id = "HNG-02777";
+
+echo "Hello World, this is $name with HNGi7 ID $hngboard_id using PHP for stage 2 task email $email";
+
 ?>
