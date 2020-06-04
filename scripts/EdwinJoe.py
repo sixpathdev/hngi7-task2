6173788d4f2bacb200d254bb00d0d219bdc324f7
Joeedwin073 <66366561+Joeedwin073@users.noreply.github.com>
2020-06-04 21:18:14 +0100

    Update EdwinJoe.py

--- scripts/EdwinJoe.py
+++ scripts/EdwinJoe.py
@@ -1,6 +1,7 @@
+email_address = "Joeedwin073@gmail.com"
 full_name = "Edwin Joe"
 hng_id = "HNG - 06375"
-email_address = "Joeedwin073@gmail.com"
+
 programming_language = "Python"
 
 print(f'Hello world, this is {full_name} with HNGi7 ID {hng_id} using {programming_language} for stage 2 task')
