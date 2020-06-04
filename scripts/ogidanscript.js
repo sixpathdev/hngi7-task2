7e0dfc81aba369c77418c2b203fe2a3c9a76c2fc
halimatsadiat <haleymaogidan@gmail.com>
2020-06-04 11:00:03 +0100

    added ogidanscript

---
+++ scripts/ogidanscript.js
@@ -0,0 +1,6 @@
+var info = [];
+info["myFullName"] = "Ogidan Sadiat";
+info["ID"] = "HNG-00834";
+var email = "haleymaogidan@gmail.com";
+
+console.log("Hello World, this is " + info["myFullName"] + " with HNGi7 ID " + info["ID"] + " using Javascript for stage 2 task");
