dc063f37d246b178838fe6ae69d97b73f09b71c5
AY-BAMZ <aybamz181@gmail.com>
2020-06-04 12:28:45 +0100

    this is my task commit

---
+++ scripts/adegbohungbe_ayobami.js
@@ -0,0 +1,9 @@
+const name = "Adegbohungbe Ayobami Paul";
+
+const id = "HNG-02572";
+
+const email = "aybamz181@gmail.com";
+
+const language = "Javascript";
+
+console.log(`Hello World, this is ${name}, with HNGi7 ID ${id}, and email address ${email} using ${language} for stage 2 task`);
