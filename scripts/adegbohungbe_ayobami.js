456433fe8fc82a630e2aabe7f82c442fffe5627b
AY-BAMZ <aybamz181@gmail.com>
2020-06-04 16:04:38 +0100

    updated

--- scripts/adegbohungbe_ayobami.js
+++ scripts/adegbohungbe_ayobami.js
@@ -1,9 +1,8 @@
-const name = "Adegbohungbe Ayobami Paul";
-
-const id = "HNG-02572";
-
 const email = "aybamz181@gmail.com";
 
+const name = "Adegbohungbe Ayobami";
+
+const id = "HNG-02572";
 const language = "Javascript";
 
-console.log(`Hello World, this is ${name}, with HNGi7 ID ${id}, and email address ${email} using ${language} for stage 2 task`);
+console.log(`Hello World, this is ${name}, with HNGi7 ID ${id} using ${language} for stage 2 task`);
