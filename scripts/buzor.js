6b753ab9336b02fcdb4468ff433738d10824e405
buhzor <coolbuhzor@gmail.com>
2020-06-04 19:01:38 +0100

    add script

--- scripts/buzor.js
+++ scripts/buzor.js
@@ -1,7 +1,7 @@
 const myEmail = "coolbuhzor@gmail.com";
-const myName = "Okafor chibuzor";
-const myHngId = "HNG-04117"
-const myLang = "javaScript"
+const myName = "[Okafor chibuzor]";
+const myHngId = "[HNG-04117]"
+const myLang = "[javaScript]"
 
 let output = ('Hello World , this is ' + myName + ' with HNGi7 ID ' + myHngId + ' using ' + myLang + ' for stage 2 task.')
 console.log(output);
