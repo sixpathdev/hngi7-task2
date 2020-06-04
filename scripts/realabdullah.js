1df0409d30ac79b933e64841e112a55f4d3d1dc7
realabdullah <abdulodesanmi@gmail.com>
2020-06-04 09:42:04 -0700

    added realabdullah.js

---
+++ scripts/realabdullah.js
@@ -0,0 +1,8 @@
+const fullName = 'Abdullahi Odesanmi';
+const id = 'HNG-01943';
+const language = 'JavaScript';
+const mailAddress = 'abdulodesanmi@gmail.com';
+
+const message = `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task`;
+
+console.log(message);
