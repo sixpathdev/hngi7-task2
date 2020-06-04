6162dde959da400b6304a48dd25747884dc2ac9b
JeremiahAkigbogun <61966753+JeremiahAkigbogun@users.noreply.github.com>
2020-06-04 10:35:04 +0000

    added JeremiahAkigbogun_script

---
+++ scripts/HNG-03139.py
@@ -0,0 +1,7 @@
+
+name = "Jeremiah Akigbogun"
+hngi7_id = "ID-03139"
+akigbogun70atgmaildotcom = "my_email"
+language = "python"
+
+print("Hello World, this is " + name + " with HNGi7 " + hngi7_id + " using " + language + " for stage 2 task.")
