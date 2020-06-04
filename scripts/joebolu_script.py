d3f0fc7bfd2916977ac21781584246b01578e54b
Joe-Bolu <66218895+Joe-Bolu@users.noreply.github.com>
2020-06-04 13:29:33 +0100

    Update joebolu_script.py

--- scripts/joebolu_script.py
+++ scripts/joebolu_script.py
@@ -1,9 +1,8 @@
-file = "joebolu_script.py"
 name = "Boluwatife Babajimi-Joseph"
 id = "HNG-00228"
 email = "josh@joebolu.com"
 language = "Python"
 
-print("Hello World, this is " + name + " with HNGi7 ID " + id + ", using " + language + " for stage 1 task 2")
+print("Hello World, this is " + name + " with HNGi7 ID " + id + " using " + language + " for stage 2 task")
 
 
