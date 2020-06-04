c7cf9e986ad33670a11f15a20e493403151fc138
Bislon Zulu <bislonzulu1@outlook.com>
2020-06-04 14:34:20 +0200

    Create bislon_script.js

---
+++ scripts/bislon_script.js
@@ -0,0 +1,5 @@
+
+
+const person = ['Bislon Zulu', 'HNG-03958', 'bislonzulu1@outlook.com', 'JavaScript'];
+
+console.log(`Hello World, this is ${person[0]} with HNGi7 ID ${person[1]} and email ${person[2]} using ${person[3]} for stage 2 task`);
