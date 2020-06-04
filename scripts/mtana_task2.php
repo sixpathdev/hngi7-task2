15d48cd0fe90654de705df098a52f409d58d96f6
Mveliso Mtana <36462899+Mveliso42@users.noreply.github.com>
2020-06-05 01:11:22 +0200

    From Python to PHP
    Changed my source code from Python to php...

---
+++ scripts/mtana_task2.php
@@ -0,0 +1,12 @@
+<?php
+
+$email = "mtanamveliso@gmail.com";
+$name = "Mveliso Mtana";
+$hng_id = "HNG-02711";
+$language = "PHP";
+
+$display = "Hello World, this is $name with HNGi7 ID $hng_id using $language_used for stage 2 task email $email";
+
+echo $display;
+
+?>
--- scripts/mtana_task2.py
+++
@@ -1,6 +0,0 @@
-email = 'mtanamveliso@gmail.com'
-name = 'Mveliso Mtana'
-hng_id = 'HNG - 02711'
-language = 'Python'
-
-print('Hello World, this is' + ' ' + name + ' ' + 'with HNGi7 ID' +' '+ hng_id +' '+ 'using' +' '+ language +' '+ 'for stage 2 task email' +' '+ email)
