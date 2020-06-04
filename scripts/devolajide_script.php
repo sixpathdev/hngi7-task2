88ac701cd4428b434aa67c9da5a2f56da406d7a1
Oladejo Olajide <programmerolajide@gmail.com>
2020-06-04 09:46:45 +0100

    added my script devolajide_script.php in scripts directory

---
+++ scripts/devolajide_script.php
@@ -0,0 +1,9 @@
+<?php
+$myName = 'Oladejo Olajide';
+$myID = 'HNG-00248';
+$email = 'programmerolajide@gmail.com';
+$progLanguage = 'PHP';
+
+echo "Hello World, this is " .$myName. " with HNGi7 ID " .$myID. " using " .$progLanguage. " for stage 2 task";
+
+?>
