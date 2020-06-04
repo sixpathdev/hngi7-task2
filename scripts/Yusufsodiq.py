7c4179ddbba08e1131b70631d9892ff105f04cde
surdman-coder <66185908+surdman-coder@users.noreply.github.com>
2020-06-05 00:09:43 +0100

    Delete Yusufsodiq.py

--- scripts/Yusufsodiq.py
+++
@@ -1,5 +0,0 @@
- full_name= 'Yusuf Sodiq'
- ID= 'HNG-02857'
- email= 'yusufsodiq22@gmail.com'
- lang= 'python'
- print('Hello world, This is '+full_name+' with HNG_ID '+ID+' using the language '+lang+' for task 2 and '+email+' is my email address')
