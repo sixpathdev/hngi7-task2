64a492a09eaa50e4ad51c3c890bfe1566fd28d41
johntaiwo005 <66323991+johntaiwo005@users.noreply.github.com>
2020-06-04 13:11:39 +0100

    Upadated myScript
    Done as required!

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -6,6 +6,6 @@ const email = 'johntaiwo005@gmail.com';
 
 const language = 'javascript';
 
-var details = "Hello Everyone, I am " + name + " with HNGi7 ID " + id + " using " + language  + " for stage 2 task";
+var details = "Hello World, this is " + name + " with HNGi7 ID " + id + " using " + language + " for stage 2 task";
 
 console.log(details);
