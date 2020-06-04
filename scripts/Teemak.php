623e683a9f1d6fddfc77c623a1778cb8cc4d8de4
prismathic <pjjsalam1999@gmail.com>
2020-06-04 14:23:48 +0100

    TeeMak file

---
+++ scripts/Teemak.php
@@ -0,0 +1,11 @@
+<?php
+    $fullname = "Makinde Tolu Michael";
+    $my_hng_id = "HNG-01995";
+    $myemail = "makindet74@gmail.com";
+    $language_used = "php";
+    $file = "TeeMak.php";
+    ;
+    $output = "Hello World, this is ".$fullname." with HNGi7 ID ".$my_hng_id." using language ".$language_used." for stage 2 task";
+    echo $output;
+   
+?>
