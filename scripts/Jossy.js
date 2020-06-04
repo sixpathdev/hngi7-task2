79af3518b4c8867f6957fb1ef8d4d77a90232f43
giwajossy <giwajosef@gmail.com>
2020-06-05 00:53:14 +0100

    Created Jossy.js

---
+++ scripts/Jossy.js
@@ -0,0 +1,14 @@
+// Full Name
+const name = 'Giwa Jossy'; 
+
+// Internship ID
+const HNG_id = 'HNG-01584'; 
+
+// Language Used
+const language = 'javascript'; 
+
+// Email
+const email = 'giwajossy@gmail.com';
+
+
+console.log("Hello World, this is " + name + " with HNGi7 ID " + HNG_id + " using " + language + " for stage 2 task email " + email);
