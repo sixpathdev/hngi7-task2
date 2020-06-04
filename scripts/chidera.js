fdbd026a84b0bc4a049f1c031e62f02d5ef93270
Otigba Chidera <61059259+YoungOT@users.noreply.github.com>
2020-06-04 15:52:57 +0100

    my hngi7-task2

---
+++ scripts/chidera.js
@@ -0,0 +1,6 @@
+const fullName = 'Otigba Chidera'
+const id = 'HNG-00525'
+const email = 'otigbachidera@gmail.com'
+const stack = 'Javascript'
+
+console.log (` "Hello World, this is ${fullName} with HNGi7 ID ${id} using ${stack} for stage 2"`)
