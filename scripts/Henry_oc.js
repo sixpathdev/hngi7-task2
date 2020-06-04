e3170d8e6252a19d02136624f3ea8e843389fdbf
okafohenrie <okafohenrie@gmail.com>
2020-06-04 14:14:25 +0100

    added Henry_oc.js to scripts

---
+++ scripts/Henry_oc.js
@@ -0,0 +1,9 @@
+const intro = (name, hng_id, lang) => {  
+	return console.log(`Hello World, this is ${name} with HNGi7 ID ${hng_id} using ${lang} for stage 2 task`
+	);
+}
+
+intro('Okafor Henry', 'HNG-06037', 'Javascript');
+
+
+
