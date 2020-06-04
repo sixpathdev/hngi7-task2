69f03a2e2385e29f6b88f8163ccbe74b4488d764
Arisekolat <64399351+Arisekolat@users.noreply.github.com>
2020-06-04 03:46:11 +0100

    Create HNG-01724.js

---
+++ scripts/HNG-01724.js
@@ -0,0 +1,6 @@
+const fullname = "Azeez Sunday";
+const hngid = "HNG-01724";
+const email = "azeezsunday.a@gmail.com";
+const language = "javascript";
+const my_output = `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`;
+console.log(my_output);
