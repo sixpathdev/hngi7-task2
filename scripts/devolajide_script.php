bc3573b947533d0f121a23c36b18c3089cf28f18
Oladejo Olajide <programmerolajide@gmail.com>
2020-06-04 19:43:40 +0100

    Update devolajide_script.php
    Changed email to the first line as instructed. If there is any other changes, kindly let me know. Thanks

--- scripts/devolajide_script.php
+++ scripts/devolajide_script.php
@@ -1,7 +1,7 @@
 <?php
+$email = 'programmerolajide@gmail.com';
 $myName = 'Oladejo Olajide';
 $myID = 'HNG-00248';
-$email = 'programmerolajide@gmail.com';
 $progLanguage = 'PHP';
 
 echo "Hello World, this is " .$myName. " with HNGi7 ID " .$myID. " using " .$progLanguage. " for stage 2 task";
