fae8f7600aa25d1174902df3f160eebf7e8ee2a7
eazytimzy <61963475+eazytimzy@users.noreply.github.com>
2020-06-04 20:40:52 +0100

    Update- eazytimzy.js file

--- scripts/eazytimzy.js
+++ scripts/eazytimzy.js
@@ -1,8 +1,6 @@
-
-
+let email = "adetunjitimilehin500@gmail.com"
 let MyName = "Adetunji Timilehin";
-let ID  = " HNG-02894";
-let email  = "adetunjitimilehin500@gmail.com"
+let ID  = "HNG-02894";
 let language = "Javascript";
 let MyDetails = ` Hello World, this is ${MyName} with HNGi7 ID ${ID} using ${language} for stage 2 task.`;
 console.log(MyDetails);
