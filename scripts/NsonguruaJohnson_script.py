c1f87842c0dc85afb8cf81bad6a552d202111dad
NsonguruaJohnson <61969839+NsonguruaJohnson@users.noreply.github.com>
2020-06-04 15:18:54 -0700

    Updated codes in file

--- scripts/NsonguruaJohnson_script.py
+++ scripts/NsonguruaJohnson_script.py
@@ -3,5 +3,5 @@ HNG_ID = "HNG-01068"
 myEmail = "nsonguruajohnson@gmail.com"
 Language = "Python"
 
-print ("Hello World, this is" , myName, "with HNG ID", HNG_ID, "using", Language, "for stage 2 task")
+print ("Hello World, this is" , myName, "with HNG ID", HNG_ID, "using", Language, "for stage 2 task email", myEmail)
 
