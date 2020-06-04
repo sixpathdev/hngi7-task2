950c9edf8f2b8f54a7af07afbe983de1f9c7dc73
nevis-adhiambo <44494861+nevis-adhiambo@users.noreply.github.com>
2020-06-04 14:19:29 +0300

    Delete nevisadhiambo_script.js

--- nevisadhiambo_script.js
+++
@@ -1,6 +0,0 @@
-var name= "Nevis Adhiambo";
-var id= "HNG-03656";
-var email= "nevisadhiambo71@gmail.com"
-var language = "javascript";
-var output= "Hello world, this is"+ name+ "with HNGi7 ID"+ id + email +"using"+language;
-console.log(output);
