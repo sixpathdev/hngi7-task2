dffc0e131e42f19c54d98a0cdca38b13d8a58605
addy360 <abbyboy360@gmail.com>
2020-06-05 00:56:09 +0300

    again,, added some required changes

---
+++ scripts/addy360_script.js
@@ -0,0 +1,15 @@
+let name = 'Adilu Khalid';
+let HNG_id = 'HNG-00673';
+let email = 'adiluabdul@gmail.com';
+let language = 'javascript';
+
+console.log(
+  'Hello World, this is ' +
+    name +
+    ' with HNGi7 ID ' +
+    HNG_id +
+    ' using ' +
+    language +
+    ' for stage 2 task email ' +
+    email
+);
