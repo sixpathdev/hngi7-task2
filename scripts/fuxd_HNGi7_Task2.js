f36942fa77008cad8d8fcdf6bf10014d7f47d70c
fuad-o <63184569+fuad-o@users.noreply.github.com>
2020-06-04 15:22:22 +0100

    Update fuxd_HNGi7_Task2.js

--- scripts/fuxd_HNGi7_Task2.js
+++ scripts/fuxd_HNGi7_Task2.js
@@ -1,5 +1,6 @@
 const name = "Oduola Fuad";
 const hng_id = "HNG-04189";
 const email = "oduolafuad@gmail.com";
+const lang = "Javascript";
 
-console.log("Hello World, this is ${name} with HNGi7 ID ${hng_id} for stage 2 task");
+console.log(`Hello World, this is ${name} with HNGi7 ID ${hng_id} for stage 2 task. I wrote this script using ${lang}.`);
