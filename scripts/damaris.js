ce2fd3771201f9e1dcaa925beb689a22533d2399
Damaris Muange <mdamarisnduku@gmail.com>
2020-06-04 19:07:57 +0300

    My task 2 assignment completed

---
+++ scripts/damaris.js
@@ -0,0 +1,5 @@
+const name = 'Damaris Muange';
+const hngId = 'HNG-04159';
+const email = 'mdamarisnduku@gmail.com';
+const language = 'javascript';
+console.log('Hello world, this is'+ name + 'with HNGi7 ID'+ hngId + 'using'+ language + 'for stage 2 task');
