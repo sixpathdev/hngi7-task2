e7c169a3e3223125b54511818c8de14ae3c4e9b5
Oluwatimilehin Makinde <temak@Oluwatimilehins-MacBook-Pro.local>
2020-06-04 16:08:57 +0100

    removed email from output

--- scripts/temak-HNG-06553.js
+++ scripts/temak-HNG-06553.js
@@ -3,6 +3,6 @@ var hng_id = "HNG-06553";
 var email = "makindeoluwatimilehin@gmail.com";
 var language = "JavaScript";
 
-let greeting = "Hello World, this is " + name + " with HNGi7 ID " + id + " and email " + email + " using " + language + " for stage 2 task";
+let greeting = "Hello World, this is " + name + " with HNGi7 ID " + hng_id + " and using " + language + " for stage 2 task";
 
 console.log(greeting);
