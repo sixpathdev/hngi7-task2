a82dcc247fda63d98cbec61bec212a676e505fbb
Hasbiy <58561237+Hasbiy@users.noreply.github.com>
2020-06-04 01:50:37 +0100

    Update hasbiy_script.js

--- scripts/hasbiy_script.js
+++ scripts/hasbiy_script.js
@@ -2,12 +2,12 @@ console.log()
 
 const name = 'hasbiy';
 
-const id = 'U014CPA93E2';
+const id = 'HNG-01932';
 
 const email = 'thasbiyallah@gmail.com';
 
 const language = 'javascript';
 
-console.log("Hello World, this is " + name + " with HNGi7 ID " + id + 
+console.log("Hello World, this is " + name + " with HNGi7 ID " + id + " and email " + email +
 
 " using " + language  + " for stage 2 task")
