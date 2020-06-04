e061d090ea466fdcdc72363f037b054d3436ad26
Ethical Ralph <rakinola90@gmail.com>
2020-06-04 21:55:24 +0100

    HNG-05150 script file

---
+++ scripts/HNG_05150.js
@@ -0,0 +1,10 @@
+const email = 'rakinola90@gmail.com';
+const name = 'Akinola Raphael';
+const hngId = 'HNG-05150';
+const language = 'javascript';
+
+const output = () => {
+	return `Hello World, this is ${name} with HNGi7 ID ${hngId} using ${language} for stage 2 task email ${email}`
+}
+
+console.log(output())
