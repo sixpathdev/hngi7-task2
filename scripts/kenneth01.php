cd401196b90d2d36c0181c49512401f73141601a
unknown <idokokennethnduka@gmail.com>
2020-06-04 15:01:12 -0700

    added kenneth01.php

---
+++ scripts/kenneth01.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+  <?php
+  $kName = "Kenneth Nduka";
+  $kId = "HNG-03220";
+  $Kgmail = "idokokennthnduka@gmail.com";
+  $KLanguage = "php";
+
+  echo "Hellow World, this is " . $kName . " with HNGi ID " . $kId. " using " . $KLanguage . " for stage 2 task";
+ ?>
+</body>
+</html>
