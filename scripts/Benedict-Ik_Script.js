d2df6edc213a0cde4067fdad16565431aba1fcb9
Benedict-Ik <benedict.ik98@gmail.com>
2020-06-04 15:25:27 +0100

    latest changes for stage 2 entry

---
+++ scripts/Benedict-Ik_Script.js
@@ -0,0 +1,9 @@
+let email = "benedict.ik98@gmail.com"
+let name = "Benedict Ikechukwuka Odoh"
+let hngboard_ID = "HNG-03261"
+let language = "JavaScript"
+let answer = "Hello World, this is " + name + " with HNGi7 ID " + hngboard_ID + " using " + language
+ + " for stage 2 task."
+
+console.log(answer)
+
