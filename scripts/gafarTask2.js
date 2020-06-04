7d6cf9ac401d39144e4ed4ae579b56f36c13a330
gafargits <rajigafaronline@gmail.com>
2020-06-04 07:59:27 +0100

    code refactored to use console.log

--- scripts/gafarTask2.js
+++ scripts/gafarTask2.js
@@ -1,4 +1,4 @@
-const myInfo = (fName, id, lang) =>( 
-  alert(`Hello World, this is ${fName} with HNGi7 ID ${id} using ${lang} for stage 2 task.`)
+const myInfo = (fName, id, email, lang) =>( 
+  console.log(`Hello World, this is ${fName} with HNGi7 ID ${id} using ${lang} for stage 2 task.`)
 )
 myInfo('Gafar Raji', '05863', 'Javascript');
