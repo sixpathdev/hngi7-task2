9a634a0cf2770ffb7dad7f7095e34ba366a8e60d
Seweje Damilola John <sewejed@gmail.com>
2020-06-04 21:00:52 +0100

    Update HNG-01034.js

--- scripts/HNG-01034.js
+++ scripts/HNG-01034.js
@@ -3,6 +3,6 @@ const
   name = 'Seweje Damilola',
   hngId = 'HNG-01034',
   language = 'JavaScript',
-  output = `Hello World, this is ${name} with HNGi7 ID ${hngId} using ${language} for stage 2 task`;
+  output = `Hello World, this is ${name} with HNGi7 ID ${hngId} using ${language} for stage 2 task email ${email}`;
 
 console.log(output);
