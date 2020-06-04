5ea854253c2df1466bad2070ce0fa4825772b048
Abdulazeez Olalere <olalereazeez11@gmail.com>
2020-06-04 21:34:37 +0100

    My task 2

---
+++ scripts/HNG-02121.js
@@ -0,0 +1,10 @@
+var fullName = "Olalere Abdulazeez";
+
+var ID = "HNG-02121";
+
+var language = "Javascript";
+
+var Email= "olalereazeez11@gmail.com";
+
+var myString = `Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${language} for stage 2 task. ${Email}`;
+console.log(myString);
