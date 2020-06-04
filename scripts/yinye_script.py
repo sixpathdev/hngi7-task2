161c984fb77aa8299d6c6d29041155722428326e
chudyn <chudyn@yahoo.com>
2020-06-04 06:04:47 +0100

    feat: added grace.py

---
+++ scripts/yinye_script.py
@@ -0,0 +1,7 @@
+name = "Chukwuonye Grace Onyinyechi"
+hng_id ="HNG-00439"
+email = "chudyn@yahoo.com"
+lang = "python"
+
+
+print ("Hello World" + "," + "this is" + name + "with HNDi7 ID" + hng_id + "using" + lang + "for stage 2 task")
