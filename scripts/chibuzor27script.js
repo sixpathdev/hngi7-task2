22bf8dfee538e4c3645387a378183043073593b6
Chibuzor Nwankwo <mackingnwanks27@gmail.com>
2020-06-04 23:58:02 +0100

    Added my script file: chibuzor27script.js

---
+++ scripts/chibuzor27script.js
@@ -0,0 +1,10 @@
+const fullName = 'Nwankwo Chibuzor Stanley';
+const hngId = 'HNG-03233';
+const email = 'mackingnwanks27@gmail.com';
+const language = 'Javascript';
+
+const result = () => {
+    return `Hello World, this is ${fullName} with HNGi7 ID ${hngId} using ${language} for stage 2 task email ${email}`;
+}
+
+console.log(result());
