5db607f883efc29da7badc0eecb325e8faf6cba9
Adeleke Isaiah <iadeleke01@gmail.com>
2020-06-04 21:53:54 +0100

    Edited Adeleke Isaiah.js

--- scripts/Adeleke Isaiah.js
+++ scripts/Adeleke Isaiah.js
@@ -7,4 +7,4 @@ const language = "Javascript";
 
 console.log("Hello World, this is " + name + " with HNGi7 ID " + id + 
 
-" using " + language  + " for stage 2 task")
+" using " + language  + " for stage 2 task" + " email " + email)
