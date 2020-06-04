025d61fa1480f40973a4d9a426a7fa38b51d6772
was-coder <wasleadwears4u2c@gmail.com>
2020-06-04 21:21:54 +0400

    put email in console.log

--- scripts/index.js
+++ scripts/index.js
@@ -3,4 +3,4 @@ let name = "Adelakun Wasiu";
 let id = "HNG-00057";
 let language = "javascript";
 
-console.log ("Hello World, this is " + name + " with HNGi7 ID " + id + " using " + language + " for stage 2 task");
+console.log ("Hello World, this is " + name + " with HNGi7 ID " + id + " using " + language + " for stage 2 task email " + email);
