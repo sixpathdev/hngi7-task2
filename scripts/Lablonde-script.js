76478fd0621e3ea606ac3b2cfee4cbade70ef8ff
Lablonde-Developper <[lkalalizi@gmail.com]>
2020-06-04 11:05:06 -0700

    My HNGI7-task2

---
+++ scripts/Lablonde-script.js
@@ -0,0 +1,6 @@
+let FullName = "Lablonde Juliette Kalalizi";
+let Hngi7_id ="HNG-03269";
+let myEmail = "lkalalizi@gmail.com";
+let language = "JavaScript";
+
+console.log(`Hello world! this is ${FullName} with ID ${Hngi7_id} using ${language} for task 2 task `);
