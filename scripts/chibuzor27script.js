de24dabfc5620d9e6e0dd6d1ebfad12bc0acf541
Chibuzor Nwankwo <mackingnwanks27@gmail.com>
2020-06-05 00:23:42 +0100

    Added my script file: chibuzor27script.js

--- scripts/chibuzor27script.js
+++ scripts/chibuzor27script.js
@@ -4,7 +4,7 @@ const email = 'mackingnwanks27@gmail.com';
 const language = 'Javascript';
 
 const result = () => {
-    return `Hello World, this is ${fullName} with HNGi7 ID ${hngId} using ${language} for stage 2 task email ${email}`;
+    return `Hello World, this is ${fullName} with HNGi7 ID ${hngId} using ${language} for stage 2 task`;
 }
 
 console.log(result());
