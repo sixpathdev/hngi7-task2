bf7af98c83e077233e3763214f3216519c910871
ChibuFirst <chibuzoonuoha57@gmail.com>
2020-06-04 21:05:20 +0100

    chore: updated chibufirst_script.php

--- scripts/chibufirst_script.php
+++ scripts/chibufirst_script.php
@@ -1,7 +1,7 @@
 <?php
-    $name = "Onuoha Godfirst Chibuzo";
-    $id = "HNG-00498";
     $email = "chibuzoonuoha57@gmail.com";
+    $name = "Onuoha Godfirst";
+    $id = "HNG-00498";
     $language = "PHP";
     
     echo "Hello World, this is " . $name . " with HNGi7 ID " . $id ." using " . $language . " for stage 2 task";
