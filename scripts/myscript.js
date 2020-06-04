4153012c9b32e593b17d275ddc3b93b4d55417d4
olazshile <olashilequadry@gmail.com>
2020-06-04 02:38:22 +0100

    added myscript.js file to scripts folder

---
+++ scripts/myscript.js
@@ -0,0 +1,9 @@
+// first script using js
+const id = "HNG-03317";
+const name = "Quadri Olashile Ogundare";
+const language = "javascript";
+const myemail = "olashilequadry@gmail.com";
+
+console.log(
+    `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`
+);
