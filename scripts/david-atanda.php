252fcdc3a2917b55f26248730b841030ef5f598b
David Atanda <atandadave@gmail.com>
2020-06-04 05:34:22 +0100

    Update david-atanda.php

--- scripts/david-atanda.php
+++ scripts/david-atanda.php
@@ -1,6 +1,6 @@
 <?php
-$internname = "Atanda David";
-$internID = "HNG-04072";
-$internmail ="atandadave@gmail.com";
-echo "Hello World, this is " .$internname . " with HNGi7 ID " .$internID . "using PHP for stage 2 task";
+$internnname = "Atanda David";
+$internnID = "HNG-04072";
+$internnmail ="atandadave@gmail.com";
+echo "Hello World, this is " .$internnname . "and email" .$internnmail . "with HNGi7 ID " .$internnID . "using PHP for stage 2 task";
 ?>
