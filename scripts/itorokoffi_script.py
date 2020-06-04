b6a5213e297f798556fb095c042760a53fcb7864
ItoroKoffi <itorokoffi@gmail.com>
2020-06-04 17:48:38 +0100

    email location update

--- scripts/itorokoffi_script.py
+++ scripts/itorokoffi_script.py
@@ -1,5 +1,5 @@
+my_email='itorokoffi@gmail.com'
 my_name='Itoro Koffi'
 my_HNG_id='HNG-06595'
 language='Python'
-my_email='itorokoffi@gmail.com'
 print(f"Hello World, this is {my_name} with HNGi7 ID {my_HNG_id} using {language} for stage 2 task")
