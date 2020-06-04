788a681127a27b4e3efb56589343e7a9aa39fe5a
tundeLayan <tunderoxyafolayan@gmail.com>
2020-06-04 20:38:25 +0100

    Re-arranged the variable declarations

--- scripts/tundeLayan_script.js
+++ scripts/tundeLayan_script.js
@@ -1,6 +1,6 @@
+const email = "tunderoxyafolayan@gmail.com";
 const name = "Afolayan Olatunde";
 const id = "HNG-04770";
-const email = "tunderoxyafolayan@gmail.com";
 const language = "Javascript";
 
 const myHngDets = () => `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`;
