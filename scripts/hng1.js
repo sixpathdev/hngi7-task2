509b6325081935116a9fd988450ee6104b175d71
favourchiazokam <52789965+favourchiazokam@users.noreply.github.com>
2020-06-04 17:53:52 +0100

    Add files via upload
    my javascript code

---
+++ scripts/hng1.js
@@ -0,0 +1,19 @@
+const output = () => {
+  const info = {
+    fullName: "Favour Igwe",
+    hngId: "HNG-01067",
+    email: "favchiazokam@gmail.com",
+    language: "javascript",
+  };
+  return (
+    " Hello world, this is " +
+    info.fullName +
+    " with HNG ID " +
+    info.hngid +
+    " using " +
+    info.language +
+    " for stage 2 task. "
+  );
+};
+
+console.log(output());
