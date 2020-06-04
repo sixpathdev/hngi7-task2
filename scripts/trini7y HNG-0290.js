eed4a7fef2ed13ae1f72e84b198a526116f821e7
trini7y <okekedesmond71@gmail.com>
2020-06-05 00:38:12 +0100

    Changed the file format

---
+++ scripts/trini7y HNG-0290.js
@@ -0,0 +1,6 @@
+const name ="Okeke Desmond"
+const ID ="HNG-0290"
+const email="okekedesmond71@gmail.com"
+const language ="JavaScript"
+
+console.log("Hello World, this is " + name + ", with HNGi7 ID " + ID + " using " +  language + " for stage 2 task")
