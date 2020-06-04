53c5752d9d5723aa2eccb71bcacef5879f8f354c
Tuasegun <59211245+Tuasegun@users.noreply.github.com>
2020-06-04 17:36:21 +0100

    Update HNG-02208.php

--- scripts/HNG-02208.php
+++ scripts/HNG-02208.php
@@ -3,6 +3,6 @@ $full_name = "Oguntuase Oluwasegun";
 $hng_id = "HNG-02208";
 $lang = "PHP";
 $email = "tuase360@gmail.com";
-echo "Hello World, this is ". $full_name .  " with HNGi7 ID "  .$hng_id . " using "  .$lang ." for stage 2 task";
+echo "Hello World, this is ". $full_name .  " with HNGi7 ID "  .$hng_id . " and email " . $email. " using "  .$lang ." for stage 2 task";
 
 ?>
