a802d2c5702e405c8d73fbb3515cfc5456fbcae1
Muhideen Mujeeb Adeoye <41507005+Mujhtech@users.noreply.github.com>
2020-06-04 15:02:14 +0100

    Create mujhtech_script.php

---
+++ scripts/mujhtech_script.php
@@ -0,0 +1,9 @@
+<?php
+
+$email = "mujeeb.muhideen@gmail.com";
+$name = "Muhideen Mujeeb Adeoye";
+$language = "PHP";
+$id = "HNG-05967";
+
+ echo "Hello World, this is Muhideen Mujeeb Adeoye with HNGi7 ID HNG-05967 using PHP for stage 2 task";
+?>
