75e3c29eb6ca9f0fedeb068ddafd2032ddcac902
Joe-Bolu <66218895+Joe-Bolu@users.noreply.github.com>
2020-06-04 13:15:09 +0100

    Update joebolu_script.py

--- scripts/joebolu_script.py
+++ scripts/joebolu_script.py
@@ -1,9 +1,9 @@
-{
-file: "joebolu_script.py",
-output: "Hello World, this is Babajimi-Joseph Boluwatife (JoeBolu) with HNGi7 ID HNG-00228 using Python for stage 2 task",
-name: "Babajimi-Joseph Boluwatife",
-id: "HNG-00228",
-email: "josh@joebolu.com",
-language: "Python",
-status: "Pass"
-},
+file = "joebolu_script.py"
+name = "Boluwatife Babajimi-Joseph"
+id = "HNG-00228"
+email = "josh@joebolu.com"
+language = "Python"
+
+print("Hello World, this is " + name + " with HNGi7 ID " + id + ", using " + language + " for stage 1 task 2")
+
+
