95d64e3ea390870b117df1a0f021b0c70e257823
ryann254 <keddelyronjoz@gmail.com>
2020-06-04 15:05:26 +0300

    Ryan_Njoroge-HNG-00306 script

---
+++ scripts/Ryan_Njoroge_HNG-00306_script.js
@@ -0,0 +1,6 @@
+const fullName = "Ryan Njoroge";
+const id = "HNG-00306";
+const language = "Javascript";
+const email = "keddelyronjoz@gmail.com";
+console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for
+stage 2 task`);
