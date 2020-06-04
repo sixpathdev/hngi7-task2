f899bb7446ee00e2dd9980c303ea43d509e217c5
Abdullah-ohida <ohida2001@gmail.com>
2020-06-04 22:12:06 +0100

    fea: added abdullah_script.js

---
+++ scripts/abdullah_script.js
@@ -0,0 +1,9 @@
+let userName = 'Ismail Abdullah';
+let hngId = 'HNG-02845';
+let  language = 'javascript';
+
+function myDetail() {
+  return `Hello World, this is ${userName} with HNGi7 ID ${hngId} using ${language} for stage 2 task`
+}
+
+console.log(myDetail())
