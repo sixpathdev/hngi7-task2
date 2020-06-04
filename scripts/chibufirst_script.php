cd1755985c82b2ea0b017ae72558ff08c43c376e
Onuoha G. Chibuzo <52785343+ChibuFirst@users.noreply.github.com>
2020-06-04 21:17:28 +0100

    chore: added email at the end of the output

--- scripts/chibufirst_script.php
+++ scripts/chibufirst_script.php
@@ -4,5 +4,5 @@
     $id = "HNG-00498";
     $language = "PHP";
     
-    echo "Hello World, this is " . $name . " with HNGi7 ID " . $id ." using " . $language . " for stage 2 task";
+    echo "Hello World, this is " . $name . " with HNGi7 ID " . $id ." using " . $language . " for stage 2 task email " . $email;
 ?>
