4d550479a51668acfcd2bdbea92ba492290afbbc
deola-dev <abdulwahabadeola@gmail.com>
2020-06-04 21:18:39 +0000

    adeola2058-HNG-20915 script

--- scripts/HNG-02915-script.js
+++ scripts/HNG-02915-script.js
@@ -1,3 +1,6 @@
-let name = "Abdulwahab Adeola", id = "HNG-02915", email = "abdulwahabadeola@gmail.com", language = "javascript";
-let output = `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`;
-document.write(output);
+const email = "abdulwahabadeola@gmail.com";
+const name = "Abdulwahab Adeola";
+const id = "HNG-02915";
+const language = "javascript";
+const output = `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task email ${email}`;
+console.log(output);
