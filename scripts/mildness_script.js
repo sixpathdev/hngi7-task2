35de029714f24ecf3976d649860f77624106e4c4
Mildness10 <66227187+Mildness10@users.noreply.github.com>
2020-06-04 23:28:39 +0200

    Update mildness_script.js

--- scripts/mildness_script.js
+++ scripts/mildness_script.js
@@ -1,5 +1,5 @@
+const email ='m.akomoize@gmail.com';
 const name ='Mildness Akomoize';
 const id ='HNG-03014';
-const email ='m.akomoize@gmail.com';
 const language ='javascript';
 console.log("Hello World, this is "+name+" with HNGi7_id "+id+" using "+language+" for stage 2 task")
