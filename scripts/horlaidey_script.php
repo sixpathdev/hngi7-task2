a432f8107d8a0cdb767d6987c092bf0abf70f5f0
horlaidey <salaudeenolaboy@gmail.com>
2020-06-04 10:12:36 +0100

    Desc: Decided to make some adjustment to my variable naming for uniqueness and made some correction to my output

--- scripts/horlaidey_script.php
+++ scripts/horlaidey_script.php
@@ -1,8 +1,8 @@
 <?php
-$name = "Salaudeen Taofeek Olaidey";
+$_name = "Salaudeen Taofeek Olaidey";
 $id = "HNG-01019";
-$email = "salaudeenolaboy@gmail.com";
+$_email = "salaudeenolaboy@gmail.com";
 $language = "PHP";
-echo "Hello world, this is $name with HNGi7 ID: $id using $language for stage 2 task"
+echo "Hello World, this is $_name with HNGi7 ID $id using $language for stage 2 task"
 
 ?>
