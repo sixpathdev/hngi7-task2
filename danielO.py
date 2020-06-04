b753290b171063dd2f838c6c343b9b1a48c3f53b
danieloladunjoye <66263004+danieloladunjoye@users.noreply.github.com>
2020-06-04 18:47:14 +0100

    Add files via upload

---
+++ danielO.py
@@ -0,0 +1,7 @@
+name = "Oladunjoye Daniel"
+hng_id = "HNG-03754"
+email = "Dannywizzy14@gmail.com"
+language = "Python"
+
+#output
+print("Hello World, this is " + name + " with HNGi7 ID " + hng_id + " using " + language + " for stage 2 task. ")
