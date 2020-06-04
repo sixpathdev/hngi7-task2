b52521f78113037046e69dab53ec2d8d5ddb0570
Isiko saidiali <isikosaidiali@gmail.com>
2020-06-04 10:02:58 +0300

    refactor

--- scripts/isiko_saidiali.py
+++ scripts/isiko_saidiali.py
@@ -1,11 +1,8 @@
 import sys
-
-# email variable
+name = 'Isiko Saidiali'
+hng_id = 'HNG-01856'
 email = 'isikosaidiali@gmail.com'
+language = 'Python'
 
-def my_info():
-    '''function returns my information'''
-    print('Hello world, this is Isiko Saidiali with HNGi7 ID HNG-01856 using Python for stage 2 task')
-
-my_info()
+print('Hello world, this is {} with HNGi7 ID {} using {} for stage 2 task'.format(name, hng_id, language))
 sys.stdout.flush()
