84b2c1d8f17f8c5cdf8796750365c564c766d686
Tmakinde <57964881+Tmakinde@users.noreply.github.com>
2020-06-04 21:55:16 +0100

    Update Teemak.php

--- scripts/Teemak.php
+++ scripts/Teemak.php
@@ -1,10 +1,9 @@
 <?php
+    $myemail = "makindet74@gmail.com";
     $fullname = "Makinde Tolu Michael";
     $my_hng_id = "HNG-01995";
-    $myemail = "makindet74@gmail.com";
     $language_used = "php";
     $file = "Teemak.php";
-    ;
     $output = "Hello World, this is ".$fullname." with HNGi7 ID ".$my_hng_id." using language ".$language_used." for stage 2 task";
     echo $output;
    
