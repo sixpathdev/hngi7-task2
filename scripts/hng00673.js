0a01f6dd10f8874dee443c48a75fc35d3d6a7755
addy360 <abbyboy360@gmail.com>
2020-06-04 12:16:21 +0300

    added a script based on the format given

---
+++ scripts/hng00673.js
@@ -0,0 +1,6 @@
+const full_name = 'Adilu Khalid';
+const hng_id = 'HNG-00673';
+
+console.log(
+  `Hello World, this is ${full_name} with HNGi7 ID ${hng_id} and email adiluabdul@gmail.com using javascript for stage 2 task`
+);
