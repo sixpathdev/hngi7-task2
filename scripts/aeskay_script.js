107027e286f4411824b2b17ac4416424c403ebe4
aeskay <56758277+aeskay@users.noreply.github.com>
2020-06-04 21:26:50 +0100

    Update aeskay_script.js

--- scripts/aeskay_script.js
+++ scripts/aeskay_script.js
@@ -7,5 +7,5 @@ const info = {
 }
 
 const { name, id, email, language } = info;
-const displayText = `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task`;
+const displayText = `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task email ${email}`;
 console.log(displayText);
