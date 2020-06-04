10102c942832375223c9cfbb145043bbdeeb1dc4
kynas <45441764+kynas@users.noreply.github.com>
2020-06-04 18:52:58 +0100

    Rearrange variables

---
+++ Gabriel.py
@@ -0,0 +1,7 @@
+
+my_email = "gabrieltosin77@gmail.com"
+my_name = "Kenneth Tosin"
+my_ID = "HNG-05186"
+my_lang ="python"
+
+print ("hello world this is "+my_name+" with ID number "+my_ID+" using "+my_lang+" for this stage 2 task ")
