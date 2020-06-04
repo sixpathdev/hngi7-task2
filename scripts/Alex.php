3896ebfa33c70e8561925b9e77f567f67f799697
alexhongo <38734290+alexhongo@users.noreply.github.com>
2020-06-05 01:09:00 +0300

    Add files via upload

---
+++ scripts/Alex.php
@@ -0,0 +1,9 @@
+<?php
+
+$name = "Alex Okwach Hongo";
+$id = "HNG-00787";
+$mail = "alexokwach@gmail.com";
+$lang = "php";
+
+echo "Hello World, this is $name :HNGi7 ID $myid using $lang for stage 2 task, my email $myemail";
+?>
