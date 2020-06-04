d99f2ccabaebb88d9688f1084f4a82127bf78615
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2020-06-04 21:05:29 +0100

    Add files via upload

---
+++ scripts/zero.php
@@ -0,0 +1,12 @@
+<?php
+$email = "abdullahij951@gmail.com";
+$name = "Abdullahi Jimoh";
+$id = "HNG-00160";
+$language = "PHP";
+
+function greet($name, $id, $language){
+    echo "Hello World, this is " . $name . " with HNGi7 ID " . $id . " using " . $language . " for stage 2 task.";
+}
+
+greet($name, $id, $language);
+
