2182a37128f0772b024d0e5c113b1da4bd132b90
duflendon7 <65234009+duflendon7@users.noreply.github.com>
2020-06-04 21:19:15 +0100

    Update duflendon7_script.py

--- scripts/duflendon7_script.py
+++ scripts/duflendon7_script.py
@@ -3,4 +3,4 @@ name = "Adikemere Fortune"
 hngi7_ID = "HND-05046"
 language = "Python"
 
-print ("Hello World, this is " + name + " with HNGi7 ID " + hngi7_ID + " using " + Python + " for stage 2 task")
+print ("Hello World, this is " + name + " with HNGi7 ID " + hngi7_ID + " using " + language + " for stage 2 task")
