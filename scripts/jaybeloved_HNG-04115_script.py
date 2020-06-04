080e3737fcff953c20ffb1cfd0aa9d5159a06b88
JayBeloved <63831361+JayBeloved@users.noreply.github.com>
2020-06-05 00:55:56 +0100

    Commited Jaybeloved script with email edit

--- scripts/jaybeloved_HNG-04115_script.py
+++ scripts/jaybeloved_HNG-04115_script.py
@@ -5,7 +5,7 @@ email = 'info.jaybeloved@gmail.com'
 language = 'python'
 
 #Define Output
-output ='Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task.' %(name,hng_id,language)
+output ='Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task email %s.' %(name,hng_id,language,email)
 
 #print
 print(output)
