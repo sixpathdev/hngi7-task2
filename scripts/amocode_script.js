71155113d6999169d1baa4cb7203b45d909cc53b
Daniel Amoko <mokowebtech@gmail.com>
2020-06-04 23:11:35 +0100

    Update amocode_script.js

---
+++ scripts/amocode_script.js
@@ -0,0 +1,6 @@
+const email = 'mokowebtech@gmail.com';
+const name = 'Daniel Amoko';
+const hng_id = 'HNG-05214';
+const language_used = 'javascript';
+
+console.log(`Hello World, this is ${name} with HNGi7 ID ${hng_id} using ${language_used} for stage 2 task email ${email}`);
