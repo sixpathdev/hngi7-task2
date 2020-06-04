2924d4d77c6c222dea0c0201bc94230e7163ddbf
The-indigo <adepojuadeyemi11@gmail.com>
2020-06-04 10:09:25 +0100

    adepoju adeyemi script

---
+++ scripts/adeyemijoshua.php
@@ -0,0 +1,10 @@
+<?php
+$name= "Adepoju Adeyemi";
+$intern_id = "HNG-01540";
+$email ="adepojuadeyemi11@gmail.com";
+$language = "php";
+
+if(isset($email) && isset($intern_id)){
+    echo "Hello World, this is ".$name." with HNGi7 ID ".$intern_id." using ".$language." for stage 2 task";
+}
+?>
