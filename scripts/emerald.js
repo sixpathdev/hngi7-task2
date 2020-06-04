86408d9cb90e021b4ee77e5b12e46caeb52e68f9
engremerald <58258512+engremerald@users.noreply.github.com>
2020-06-04 17:23:09 +0100

    emeraldscript

---
+++ scripts/emerald.js
@@ -0,0 +1,5 @@
+console.log('hello world');
+var fullName='Aitomun Omonigho Faith';
+var num="02783";
+var email="faithaitomun@gmail.com";
+console.log('hello world,' + '' +'this is' + ' ' + fullName + ' ' + 'with HNGi7 ID'+' '+ num +' '+ 'and' + ' ' +'email'+' '+ email +' '+  'using js for stage 2 task')
