efb4f6e6b0b2df12aa9f0ff816d2d014050a0326
Glory tefe <glorytefe@gmail.com>
2020-06-03 16:19:20 -1200

    first commit

--- scripts/glowree_script.js
+++ scripts/glowree_script.js
@@ -2,6 +2,6 @@ let fullName = 'Mebuiefene Tefe Glory';
 let id = 'HNG-02840';
 let email = 'tefeglowree@yahoo.com'
 let language = 'JavaScript';
-let output = `Hello World, this is ${fullName} with ${id} using ${language} to solve stage 2 task.`;
+let output = `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task.`;
 
 console.log(output);
