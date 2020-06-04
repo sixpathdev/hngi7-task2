d0dd543725d04935ef149b16b223a9743bcfbfcc
inioluwa25-hub <inioluwaraheem@gmail.com>
2020-06-04 14:13:23 +0100

    raheem.js

---
+++ scripts/raheem_script.js
@@ -0,0 +1,11 @@
+const task = () => {
+	const fullName = "Raheem Inioluwa";
+	const id       = "HNG-02235";
+	const email    = "inioluwaraheem@gmail.com";
+	const language = "javascript";
+	
+	return `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task`;
+	
+}
+
+console.log(task());
