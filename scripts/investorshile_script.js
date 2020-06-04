a8c4b0b4222c8d81edf582cbf95ebd861a5091ec
olazshile <olashilequadry@gmail.com>
2020-06-04 11:27:26 +0100

    added new changes in myscript.js

---
+++ scripts/investorshile_script.js
@@ -0,0 +1,10 @@
+// script for task 2
+const hng_id = "HNG-03317";
+const myname = "Quadri Olashile Ogundare";
+const script_language = "javascript";
+const myemail = "olashilequadry@gmail.com";
+
+console.log(
+    `Hello World, this is ${myname} with HNGi7 ID ${hng_id} using ${script_language} for stage 2 task \n`
+);
+// End of script using js
--- scripts/myscript.js
+++
@@ -1,9 +0,0 @@
-const id = "HNG-03317";
-const name = "Quadri Olashile Ogundare";
-const language = "javascript";
-const myemail = "olashilequadry@gmail.com";
-
-console.log(
-    `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task\n`
-);
-// End of first script using js
