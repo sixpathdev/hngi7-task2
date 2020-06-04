e7795eaa6b26b4de6d0b4d8841771a900370375f
teelda <0812874+teelda@users.noreply.github.com>
2020-06-04 19:40:49 +1200

    Create teeldah2.js

---
+++ scripts/teeldah2.js
@@ -0,0 +1,6 @@
+const name = "ANASHIE MATILDA IPEH",
+const Hng_Id = "HNG-06306",
+const email = "matildaipeh@gmail.com",
+const language = "javascript",
+
+console.log('Hello world, this is ${name} with HNGi7 ID ${hng_Id} using ${language} for stage 2 task');
