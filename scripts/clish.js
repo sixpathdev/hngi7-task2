ec7d55949eb796d9e6d5b403ef8925e8d6bc847e
clish254 <clishilla@gmail.com>
2020-06-04 14:32:01 +0300

    added whitespace

--- scripts/clish.js
+++ scripts/clish.js
@@ -5,7 +5,7 @@ const myEmail = "clishilla@gmail.com";
 const language = "Javascript";
 //function to concatenate strings and return result
 let task2 = () => {
-    return `Hello World,this is ${fullName} with HNGi7 ID ${hngId} using ${language} for stage 2 task`
+    return `Hello World, this is ${fullName} with HNGi7 ID ${hngId} using ${language} for stage 2 task`
 };
 //print result
 console.log(task2());
