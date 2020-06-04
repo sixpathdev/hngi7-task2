8c18dc4a86d2d0a971e2e5bde05e307f88fc5eab
johnmaxnew <forevermeticulous@gmail.com>
2020-06-04 15:15:05 +0300

    feature: added johnmax_script.py

---
+++ scripts/johnmax_script.py
@@ -0,0 +1,7 @@
+name = 'John Max'
+id_ = 'HNG-06291'
+email = 'forevermeticulous@gmail.com'
+language = 'Python'
+output = 'Hello World, this is '+ name + ' undertaking the HNGi7 Program. My ID is ' + id_ + ' and I am using ' + language +' for stage 2 task. My email address is ' + email +''
+
+print(output)
