91d42f0fb7f87d28d9fce2983700ebfded76c0d9
Tanyah8 <ntmungofa@gmail.com>
2020-06-04 15:50:43 +0200

    email removed from output

--- scripts/tanyah8_script.js
+++ scripts/tanyah8_script.js
@@ -1,8 +1,8 @@
+const email = "ntmungofa@gmail.com";
 const full_name = "Nyasha Mawarire";
 const hng_id = "HNG-02775";
 const language = "Javascript";
-const email = "ntmungofa@gmail.com";
-const message = `Hello World, this is ${full_name} with HNGi7 ID ${hng_id} and email ${email} using ${language} for stage 2 task.`;
+const message = `Hello World, this is ${full_name} with HNGi7 ID ${hng_id} using ${language} for stage 2 task.`;
 
 function log(val) {
     console.log(val);
