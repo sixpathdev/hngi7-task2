b939a86ec817076ae637fc82a8bd8267ee3ee285
devvyky <okanemechibuikem@gmail.com>
2020-06-04 20:23:37 +0100

    update email on my script

--- scripts/devvyky_script.js
+++ scripts/devvyky_script.js
@@ -1,9 +1,10 @@
 // Hello World, this is fullname with HNGi7 ID [ID]
 const fullName = "Okaneme Victor";
 const hngi7ID = 4215;
+const email = "okanemechibuikem@gmail.com";
 
 introFunction = () => {
-  const intro = `Hello World, this is ${fullName} with HNGi7 ID 0${hngi7ID}`;
+  const intro = `Hello World, this is ${fullName} with HNGi7 ID 0${hngi7ID} and my email is ${email}`;
   return console.log(intro);
 };
 
