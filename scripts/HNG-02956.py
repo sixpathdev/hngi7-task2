c0eb955e798c2ede5cdeb9a3eaf4cf247ec6970f
Chucks Leo Ogwo <59630115+chucksleo@users.noreply.github.com>
2020-06-04 16:55:02 +0100

    Create HNG-02956.py

---
+++ scripts/HNG-02956.py
@@ -0,0 +1,6 @@
+name = "Chucks Ogwo"
+hng_id = "HNG-02956"
+email = "chucksogwo7@gmail.com"
+language = "Python"
+
+print(f"Hello World, this is {name} with HNGi7 ID {hng_id} using {language} for stage 2 task")
