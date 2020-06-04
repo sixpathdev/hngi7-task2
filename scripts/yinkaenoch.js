6b5de520aa0f7561d176342c88d90375d8f35253
yinkaenoch <adedokunyinkaenoch@gmail.com>
2020-06-04 09:48:02 +0100

    [feat] Add yinkaenoch stage 1 task

---
+++ scripts/yinkaenoch.js
@@ -0,0 +1,10 @@
+const output = () => {
+    const fullName = "Yinka Enoch Adedokun";
+    const id       = "HNG-05028";
+    const email    = "adedokunyinkaenoch@gmail.com";
+    const language = "JavaScript";
+
+    return `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task.`;
+}
+
+console.log(output());
