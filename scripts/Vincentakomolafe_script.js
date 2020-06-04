8fa42408c36c972cbb2422d61595e82267300454
Vincent Akomolafe <66220157+VincentAKO@users.noreply.github.com>
2020-06-04 11:55:43 +0100

    Update Vincentakomolafe_script.js

--- scripts/Vincentakomolafe_script.js
+++ scripts/Vincentakomolafe_script.js
@@ -2,4 +2,4 @@ const name = 'vincentakomolafe';
 const hng_id, = '00579';
 const email = 'vincentakomolafe@gmail.com';
 const language = 'javascript;
-console.log("Hello World, this is "+ name +" with HNGi7 ID "+ hng_id +" and email "+ email +" using "+ language +" for stage 2 task")
+console.log("Hello World, this is "+ name +" with HNGi7 ID "+ hng_id +" using "+ language +" for stage 2 task")
