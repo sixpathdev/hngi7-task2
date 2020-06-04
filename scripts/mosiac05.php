3f8b05d05352df7e1e07f47ac3cde17c4e6353cc
obute-agbo <obutemoses5@gmail.com>
2020-06-04 20:22:01 +0100

    'mosiac05_PR'

---
+++ scripts/mosiac05.php
@@ -0,0 +1,10 @@
+<?php
+
+$name='Obute Moses';
+$id='HNG-05246';
+$language='PHP';
+$email='obutemoses05@gmail.com';
+
+echo "Hello World, this is " . $name . " with HNGi7 ID ". $id ." using ".$language." for stage 2 task";
+
+?>
