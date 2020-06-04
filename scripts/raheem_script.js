f696006b2b3fc2dba46f0da141afc1449c55bf3f
inioluwa25-hub <inioluwaraheem@gmail.com>
2020-06-04 21:21:47 +0100

    email change

--- scripts/raheem_script.js
+++ scripts/raheem_script.js
@@ -4,8 +4,8 @@ const task = () => {
 	const id = "HNG-02235";
 	const language = "javascript";
 	
-	return `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task`;
+	return `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task email ${email}`;
 	
 }
 
-console.log(task());
+console.log(task()); 
