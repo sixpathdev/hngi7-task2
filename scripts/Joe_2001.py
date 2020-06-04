cbc877d3a69dedc46324030a7853858e17d31a8d
eriteria <51262474+eriteria@users.noreply.github.com>
2020-06-05 00:48:26 +0100

    Updated the format

--- scripts/Joe_2001.py
+++ scripts/Joe_2001.py
@@ -1,10 +1,5 @@
-# hello-world-project
-#Hello World, this is [fullname] with HNGi7 ID [HNG-ID] using [language] for stage 2 task email [email]
-#defining attributes
 full_name="Joseph John"
 hng_id="HNG-04194"
 language="python"
 email="joseph.john.ali.2001@gmail.com"
-#Inputing into the sentence
-#print("Hello World, this is",full_name,"with","HNGi7 ID",hng_id,"using",language,"for the stage 2 task email",email)
-print("Hello World, this is {full_name} with HNGi7 ID {hng_id} using {language} for stage 2 task email {email}")
+print("Hello World, this is",full_name,"with","HNGi7 ID",hng_id,"using",language,"for the stage 2 task email",email)
