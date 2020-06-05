af1efbe75203de9244ad78106bd7541754c891ce
Wale Timothy <awtim01@gmail.com>
2020-06-05 07:04:13 +0100

    corrected to match the new format

--- scripts/HNG-00124.php
+++ scripts/HNG-00124.php
@@ -3,5 +3,5 @@
     $my_name = 'Wale Timothy';
     $my_hng_id = 'HNG-00124';
     $my_language = 'Php';
-    echo "Hello World, this is {$my_name} with HNGi7 ID {$my_hng_id} using {$my_language} for stage 2 task";
+    echo "Hello World, this is {$my_name} with HNGi7 ID {$my_hng_id} using {$my_language} for stage 2 task email {$my_email}.";
 ?>
