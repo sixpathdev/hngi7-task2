a6aa9a7426c8535d43cafeaca8922590a2dde175
sbpythonista <asaajulekan@gmail.com>
2020-06-05 05:31:49 +0100

    smartbrain-HNH-05813

---
+++ scripts/smartbrain_HNG_05813.py
@@ -0,0 +1,14 @@
+
+#Stage 2 task
+def python_script():
+    full_name = "Asaaju Olalekan"
+    Id = "05813"
+    lng = "python"
+    stage = "stage 2 task"
+    statement = "Hello World,"
+    e_mail = "asaajulekan@gmail.com"
+    return statement + " This is " + full_name +  " with HNGi7 ID HNG-" + Id + " and email " + e_mail+ " using " + lng + " for " + stage
+
+result = python_script()
+print(result)
+    
