ef81bacb87ee4871660f37e4bd42ae86c22d3093
Richard-InOm <48685789+Richard-InOm@users.noreply.github.com>
2020-06-04 22:32:45 -0400

    Add files via upload

---
+++ scripts/RichardOmotosho.js
@@ -0,0 +1,6 @@
+const NAME = "Richard Omotosho"
+const EMAIL = "ricardiomotosho@gmail.com"
+const ID = "HNG-03176"
+const LANG = "JavaScript"
+
+console.log("Hello World, this is " + NAME + " with HNGi7 ID " + ID + " using " + LANG + " for the stage 2 task.")
