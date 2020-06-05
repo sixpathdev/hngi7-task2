8b7cd9111e086f5e1961824063793ac9af1f0c36
Okeke Desmond <okekedesmond71@gmail.com>
2020-06-05 10:09:21 +0100

    Update HNG-0290.js

--- scripts/HNG-0290.js
+++ scripts/HNG-0290.js
@@ -3,4 +3,4 @@ const id ="HNG-0290";
 const email="okekedesmond71@gmail.com";
 const language ="JavaScript";
 
-console.log("Hello World, this is " + name + ", with HNGi7 ID " + id + " using " +  language + " for stage 2 task email" + email);
+console.log("Hello World, this is " + name + " with HNGi7 ID " + id + " using " +  language + " for stage 2 task email " + email);
