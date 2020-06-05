a89fd1879ae38debee2420a3d7c5524eddf16ab6
ryann254 <keddelyronjoz@gmail.com>
2020-06-05 08:18:46 +0300

    RyanNjoroge-HNG-00306 Fixed format

--- scripts/Ryan_Njoroge_HNG-00306_script.js
+++ scripts/Ryan_Njoroge_HNG-00306_script.js
@@ -3,4 +3,4 @@ const fullName = "Ryan Njoroge";
 const id = "HNG-00306";
 const language = "Javascript";
 console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for
-stage 2 task`);
+stage 2 task email ${email}`);
