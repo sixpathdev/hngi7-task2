17064dc3ce03d25a1dc31ce6184df88dc29416b4
Emrys Qeens <emrysqeens@gmail.com>
2020-06-05 15:03:07 +0100

    Update qeen.js

--- scripts/qeen.js
+++ scripts/qeen.js
@@ -1,1 +1,13 @@
+_=()=>
+{
+  const candName='Awogbami Victor';
+  const id='HNG-01842';
+  const mail='Emrysqeens@gmail.com';
+  const lang='Javascript';
+
+  console.log(`Hello World, this is ${candName} with HNGi7 ID ${id} using ${lang} for stage 2 task email ${mail}.`);
+}
+
+
+_();
 
