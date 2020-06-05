bf499ec20a403f75a328183593392a7cafaf8617
linda <lindatochukwu30@gmail.com>
2020-06-05 20:35:10 +0100

    i have added my script

---
+++ scripts/uchendu.js
@@ -0,0 +1,6 @@
+const email = "lindatochukwu30@gmail.com";
+const name = "Uchendu Tochukwu Linda";
+const hng_id = "HNG-02664";
+const language = "Javascript";
+
+console.log(`Hello World, this is ${name} with HNGi7 ID ${hng_id} using ${language} for stage 2 task email ${email}`)
