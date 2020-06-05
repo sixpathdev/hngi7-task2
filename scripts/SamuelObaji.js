12f66b33fa50b194478a7fffb642c76ebfc1db95
SamuelPhases <52789789+SamuelPhases@users.noreply.github.com>
2020-06-05 11:14:32 +0100

    Add files via upload

---
+++ scripts/SamuelObaji.js
@@ -0,0 +1,15 @@
+let email = "obajisamuelekene@gmail.com";
+let name = "Samuel Obaji";
+let id = "HNG-05528";
+let language = "javascript";
+
+console.log(
+  "Hello World, this is " +
+    name +
+    " with HNGi7 ID " +
+    id +
+    " using " +
+    language +
+    " for stage 2 task email " +
+    email
+);
