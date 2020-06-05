974b728f84e9c20a2b7e822bdcdf9efbc09955a7
Ajike Chimaobi <ajikechima@gmail.com>
2020-06-05 06:15:43 +0100

    feat: added AjikeChimaobi.js

---
+++ scripts/AjikeChimaobi.js
@@ -0,0 +1,6 @@
+const email = "ajikechimaobi@gmail.com";
+const name = "Ajike Chimaobi";
+const hng_id = "HNG-06593";
+const language = "javascript";
+
+console.log(`Hello World, this is ${name} with HNGi7 ID ${hng_id} using ${language} for stage 2 task email ${email}`
