f7578daac43772b8328650e7904c3714456e52dd
Mozeloper <ojeimoses@gmail.com>
2020-06-05 19:37:06 +0100

    added moses.js file with email variable

--- scripts/moses.js
+++ scripts/moses.js
@@ -6,4 +6,4 @@ const Email = 'ojeimoses@gmail.com';
 
 const Language = 'javascript';
 
-console.log(`Hello world, this is ${fullName} with HNGi7 ID ${ID} using ${Language} for stage 2 task Email ${Email}`);
+console.log(`Hello World, this is ${fullName} with HNGi7 ID ${ID} using ${Language} for stage 2 task Email ${Email}`);
