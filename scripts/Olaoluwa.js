80403d561aaab5c5aa000cefef594bd978a42f1b
Darey <horla@Dareys-MacBook-Pro.local>
2020-06-05 01:22:39 +0100

    correction

--- scripts/Olaoluwa.js
+++ scripts/Olaoluwa.js
@@ -4,5 +4,5 @@ const emailAddress = "darehorla@gmail.com";
 const language = "JavaScript";
 
 console.log(
-  `Hello Everyone, my name is ${fullName} my HNGi7 ID ${internshipId} using ${language} for stage 2 task ${emailAddress}`
+  `Hello World, this is ${fullName} with HNGi7 ID ${internshipId} using ${language} for stage 2 task ${emailAddress}`
 );
