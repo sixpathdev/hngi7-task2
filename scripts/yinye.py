adb691aab1ef43ce0a75864cc092c8608900a499
chudyn <chudyn@yahoo.com>
2020-06-05 12:37:55 +0100

    UPDATED SCRIPT

--- scripts/yinye.py
+++ scripts/yinye.py
@@ -3,5 +3,6 @@ name = "Chukwuonye Grace Onyinyechi"
 hng_id = "HNG-00439"
 LANG = "python"
 
-print ("Hello World" + "," + "this is" + name + "with HNGi7 ID" + hng_id + "using" + LANG + "for stage 2 task email" + email)
+
+print("Hello World" + ", " + "this is " + name + " with HNGi7 ID " + hng_id + " using " + LANG + " for stage 2 task email " + email)
 
