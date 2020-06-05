831b96f526b5a8e6106e1701c40d724182f642fa
mosnyik <mosnyik@gmail.com>
2020-06-05 11:38:19 -0700

    RESUBMISSION

--- scripts/moses.js
+++
@@ -1,9 +0,0 @@
-const fullName = 'Ojei Moses';
-
-const ID = 'HNG-06670';
-
-const Email = 'ojeimoses@gmail.com';
-
-const Language = 'javascript';
-
-console.log(`Hello world, this is ${fullName} with HNGi7 ID ${ID} using ${Language} for stage 2 task Email ${Email}`);
