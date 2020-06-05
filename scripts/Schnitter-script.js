44623e300247a95951f81008192ecb346dc302a7
Stein-Hakase <62748317+Stein-Hakase@users.noreply.github.com>
2020-06-05 11:18:28 +0700

    Updating format to support mail

--- scripts/Schnitter-script.js
+++ scripts/Schnitter-script.js
@@ -1,7 +1,6 @@
-// Variables contaigning my Details
-const fullName='Fakhreddine Hariz'
-const hngId='HNG-03410'
-const language='JavaScript'
-const eMail='Fakhriddine.hariz@gmail.com'
-// outputing my details with console.log()
-console.log(`Hello World, this is ${fullName} with HNGi7 ID ${hngId} using ${language} for stage 2 task`)
+const email="Fakhriddine.hariz@gmail.com"
+const name="Fakhreddine Hariz"
+const id="HNG-03410"
+const language="javascript"
+const output = `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task email ${email}`
+console.log(output)
