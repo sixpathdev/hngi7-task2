e676b35c02df5707577d43a415f142f4439c2e03
Noble-Genius <56192742+Noble-Genius@users.noreply.github.com>
2020-06-05 12:08:36 +0100

    Add files via upload

---
+++ NobleGenius.js
@@ -0,0 +1,9 @@
+var email = "jafar.aolayiwola@gmail.com";
+var name = "Jafar Abdulwaheed";
+var hng_id = "HNG-05645";
+var language = "JavaScript";
+
+var myText = "Hello World, this is " + name + " with HNGi7 ID " + hng_id +
+                " using " + language + " for stage 2 task email " + email ;
+
+console.log(myText);
