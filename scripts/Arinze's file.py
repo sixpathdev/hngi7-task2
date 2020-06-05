c0e20a48145d6c63b14a5a8e64239a8153597a98
Caephas <arinzeobidiegwu@gmail.com>
2020-06-05 19:56:34 +0100

    Arinze Obidiegwu scripts

--- scripts/Arinze's file.py
+++ scripts/Arinze's file.py
@@ -4,4 +4,4 @@ email_address = 'arinzeobidiegwu@gmail.com'
 
 Programming_language = "Python"
 
-print("Hello world" + ','  + "this is: " + name + " " + ' ' + 'with HNGi7 ID: '+ ' '+ hNGID +' '+ 'using' + ' ' + programming_language + ' ' + 'for stage 2 task' )
+print("Hello world" + ','  + "this is: " + name + " " + ' ' + 'with HNGi7 ID: '+ ' '+ hNGID +' '+ 'using' + ' ' + Programming_language + ' ' + 'for stage 2 task' )
