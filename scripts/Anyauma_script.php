59486e99689fd873beb3f052eae082b93ade3a75
AnyaDaniel <30692059+AnyaDaniel@users.noreply.github.com>
2020-06-05 01:33:21 +0100

    Update Anyauma_script.php

--- scripts/Anyauma_script.php
+++ scripts/Anyauma_script.php
@@ -1,4 +1,24 @@
+﻿<?php 
+//My PHP code contribution to team-titan
 
-<?php
-echo "Hello World, this is Daniel Anya with HNGi7 ID HNG-01846 using PHP for stage 2 task";
+/** This is where my variables are defined **/
+
+$titan_name = "Daniel Anya";
+
+ $titan_ID = "HNG-01846"; 
+
+$email = "anyadaniel1@gmail.com"; 
+
+$titan_special_language = "PHP"; 
+
+function intro($titan_name, $titan_ID, $titan_special_language) 
+{ 
+//This is the string that would be displayed on the screen 
+return ("Hello World, this is $titan_name with HNGi7 ID $titan_ID using $titan_special_language for stage 2 task."); 
+}
+ //Output for my function
+ echo intro("Daniel  Anya", "HNG-01846", "PHP");
+ 
+// end of my php task 2 project for team_titan
+
 ?>
