d12f09254d6bf60eb0c1ecb9c90e5b6b81bb316f
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2020-06-05 16:59:00 +0100

    Update adeoluwa20_script.py

--- scripts/adeoluwa20_script.py
+++ scripts/adeoluwa20_script.py
@@ -2,4 +2,4 @@ email = "aogbogboade@gmail.com"
 name = "Gbogboade Adeoluwa"
 id = "HNG-01991"
 language = "python"
-print = ('Hello World, this is ' + name + ' with HNGi7 ID ' + id + ' using ' + language + ' for stage 2 task email ' + email)
+print('Hello World, this is ' + name + ' with HNGi7 ID ' + id + ' using ' + language + ' for stage 2 task email ' + email)
