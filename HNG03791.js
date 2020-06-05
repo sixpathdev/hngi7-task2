2994d3f01f47f17ff87817ed8d1376952fab9cd7
Chimdi Jane Samuel <61986766+JaneSamuel@users.noreply.github.com>
2020-06-05 10:28:38 +0100

    Update HNG03791.js

--- HNG03791.js
+++ HNG03791.js
@@ -1,4 +1,4 @@
 const name = 'Chimdi Jane Samuel'
 const id = 'HNG-03791'
 const email = 'janesamuel308@gmail.com'
-console.log("Hello World, this is " + name +" with HNGi7 ID " + id + " using JavaScript for stage 2 task ." + email);
+console.log("Hello World, this is " + name +" with HNGi7 ID " + id + " using JavaScript for stage 2 task email" + email);
