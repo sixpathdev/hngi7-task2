5f40b19cde965ac64aebcbe60f3fcabdd25a9258
Alinko0 <favouralex084@gmail.com>
2020-06-04 23:57:30 -0700

    added my script

---
+++ scripts/Alex script.js
@@ -0,0 +1,9 @@
+let name= "Alex Favour";
+let email="favouralex084@gmail.com"
+let hngid= "HNG_02158";
+let language="javascript"
+
+function greet(){
+return`Hello world this is ${name} with Hng id ${hngid} using ${language}  for the stage 2 task ${email}`;  
+}
+console.log(greet());
