11efa070071eaf04c7a842b1f69bdeeee88900fa
kynas <45441764+kynas@users.noreply.github.com>
2020-06-05 02:40:31 +0100

    Adjusted.py

---
+++ scripts/Gabriel.py
@@ -0,0 +1,7 @@
+
+my_email = "gabrieltosin77@gmail.com"
+my_name = "Kenneth Tosin"
+my_id = "HNG-05186"
+my_language = "python"
+
+print("Hello World, this is " +my_name+ " with HNGi7 ID "+my_id+ " using " +my_language+" for stage 2 task email "+my_email)
