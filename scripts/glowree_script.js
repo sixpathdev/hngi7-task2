b58cd0e8a8e0a29b1287f0f4694e744721f2a482
Glory tefe <glorytefe@gmail.com>
2020-06-04 21:34:45 -1200

    few changes

--- scripts/glowree_script.js
+++ scripts/glowree_script.js
@@ -1,7 +1,7 @@
-let fullName = 'Mebuiefene Tefe Glory';
-let id = 'HNG-02840';
 let email = 'tefeglowree@yahoo.com'
+let fullName = 'Tefe Mebuiefene';
+let id = 'HNG-02840';
 let language = 'JavaScript';
-let output = `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task.`;
+let output = `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task email ${email}`;
 
 console.log(output);
