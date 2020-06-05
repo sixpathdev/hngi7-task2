3750f47f201ca7297f670937de12860e0bca9f1f
Nwokoye Chigozie <gregoflash05@users.noreply.github.com>
2020-06-05 06:56:08 +0100

    Updated greg.php file
    Please add my script to the server, I can't see it there.

--- scripts/greg.php
+++ scripts/greg.php
@@ -1,10 +1,11 @@
 <?php
-$name='Nwokoye Chigozie';
-$id='02811';
-$programming_language= 'PHP';
 $email='gregoflash05@gmail.com';
+$name='Nwokoye Chigozie';
+$id='HNG-02811';
+$programming_language= 'PHP';
 
-echo "Hello World, this is $name with HNGi7 ID HNG-$id using $programming_language for stage 2 task";
+
+echo "Hello World, this is $name with HNGi7 ID $id using $programming_language for stage 2 task";
 
 
 ?>
