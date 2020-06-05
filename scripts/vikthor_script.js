fadf70f408aecc3a3f431fe7fa91c3bb01ae74dd
Aienobe Victor <viktornobe@gmail.com>
2020-06-05 15:41:51 +0100

    Delete vikthor_script.js

--- scripts/vikthor_script.js
+++
@@ -1,6 +0,0 @@
-const FULL_NAME = 'AIENOBE VICTOR';
-const HNG_ID = 'HNG-01482';
-const EMAIL = 'viktornobe@gmail.com';
-const LANGUAGE_USED = 'Javascript';
-
-Console.log('Hello World, this is ' + FULL_NAME + ' with HNGi7 ID ' + HNG_ID + ' using ' + LANGUAGE_USED + ' for stage 2 task email ' + EMAIL);
