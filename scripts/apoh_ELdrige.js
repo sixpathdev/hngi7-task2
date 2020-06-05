f07f21f62365a80e1832641fea5e31cd065bcb2f
Eldrige <52784114+eldrige@users.noreply.github.com>
2020-06-05 08:17:44 +0100

    Modified the script folder to include my name
    My name now follows the new rules

--- scripts/apoh_ELdrige.js
+++ scripts/apoh_ELdrige.js
@@ -4,7 +4,7 @@ const info = ()=>{
     let emailAddress = 'peldrige8@gmail.com'
     let language = 'Javascript'
 
-    return `Hello World, this is ${userName} with HNGi7 ID ${internId} using ${language} for stae 2 task`
+    return `Hello World, this is ${userName} with HNGi7 ID ${internId} using ${language} for stae 2 task ${emailAddress}`
 }
 
 console.log(info())
