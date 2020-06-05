51350b88b5e6246dbf0a20844ac6db225efac4d5
Harith Onigemo <45686269+Harithmetic1@users.noreply.github.com>
2020-06-05 10:13:04 +0100

    Update Harith_HNG-01227script.js

--- scripts/Harith_HNG-01227script.js
+++ scripts/Harith_HNG-01227script.js
@@ -3,4 +3,4 @@ let name = "Harith Onigemo";
 let hng_idd = "HNG-01227";
 let lang = "Javascript";
 
-console.log(`Hello world, this is ${name} with HNG ID ${hng_idd} using ${lang} for stage 2 task email ${emailAdd}`)
+console.log(`Hello World, this is ${name} with HNGi7 ID ${hng_idd} using ${lang} for stage 2 task email ${emailAdd}`)
