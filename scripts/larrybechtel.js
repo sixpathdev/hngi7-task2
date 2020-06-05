67309ea190d831a80933a9fd01f938a9a0d1fa15
larrybechtel <54470955+larrybechtel@users.noreply.github.com>
2020-06-05 03:23:02 +0100

    Add files via upload

--- scripts/larrybechtel.js
+++ scripts/larrybechtel.js
@@ -2,9 +2,9 @@
     var name = "Ibrahim Ajala";
     var ID = "HNG-05288";
     var email = "larrybechtel396@gmail.com"
-    var lang = "JavaScript";
+    var lang = "Javascript";
     
 
-document.write("  Hello World, This is  " +    name    + "   with HNGi7 ID   " +   ID    + "  using   " +    lang    + "   for stage 2 task   ");
-console.log("  Hello World, This is  " +    name    + "   with HNGi7 ID   " +   ID    + "  using   " +    lang    + "   for stage 2 task   email   " +    email);
+document.write("Hello World, this is " +  name + " with HNGi7 ID " + ID    + " using " + lang    + " for stage 2 task email "+email);
+console.log("Hello World, this is " +  name + " with HNGi7 ID " + ID    + " using " + lang    + " for stage 2 task email "+email);
 </script>
