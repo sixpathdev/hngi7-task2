bae1cb4d53007b7ee4664578ed42816c2866ee46
Augustine Alex <49088317+xpoter@users.noreply.github.com>
2020-06-05 12:00:49 +0100

    Me format

--- scripts/AlexScript.js
+++ scripts/AlexScript.js
@@ -3,5 +3,5 @@ const fullname  = 'Alex Augustine';
 const hng_id    = 'HNG-00955';
 const language  = 'JavaScript';
 
-let output = 'Hello world, this is ' +fullname+ ' with HNGi7 ID '+hng_id+ ' using ' +language+ ' for the stage 2 task.';
-console.log(output);
+let output = 'Hello world, this is ' +fullname+ ' with HNGi7 ID '+hng_id+ ' using ' +language+ ' for the stage 2 task email '+email+ '.';
+console.log(output)
