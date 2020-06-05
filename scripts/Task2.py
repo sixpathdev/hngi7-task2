ab9e5ed22fa9102c0902a90f5304da3c997c5e8c
Akpan Godswill <55089434+GeeAkpan@users.noreply.github.com>
2020-06-05 07:17:12 +0100

    Update Task2.py

--- scripts/Task2.py
+++ scripts/Task2.py
@@ -3,4 +3,4 @@ Name = 'Akpan Godswill'
 HNGID = 'HNG-01956'
 language = 'Python'
 
-print('Hello World, this is' + ' '+ Name + ' ' + 'with HNGi7 ID' + ' '+ HNGID +' '+ 'using'+' '+ language +' '+ 'for stage 2 task email '+ email)
+print('Hello World, this is' + ' '+ Name + ' ' + 'with HNGi7 ID' + ' '+ HNGID +' '+ 'using'+' '+ language +' '+ 'for stage 2 task' +' email '+ email)
