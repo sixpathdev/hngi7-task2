44b5b10be7570c62b8e42634defeed4f35282812
Alex Favour <52333077+Alinko0@users.noreply.github.com>
2020-06-05 00:33:49 -0700

    added email

--- scripts/Alex script.js
+++ scripts/Alex script.js
@@ -4,6 +4,6 @@ let hngid= "HNG_02158";
 let language="javascript"
 
 function greet(){
-return`Hello world this is ${name} with Hng id ${hngid} using ${language}  for the stage 2 task ${email}`;  
+return`Hello world this is ${name} with Hng id ${hngid} using ${language}  for the stage 2 task email ${email}`;  
 }
 console.log(greet());
