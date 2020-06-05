acffda4a707563236ac01c4f04a33b251afb123b
fuad-o <63184569+fuad-o@users.noreply.github.com>
2020-06-05 07:07:07 +0100

    Create OduolaFuad_script.js

---
+++ OduolaFuad_script.js
@@ -0,0 +1,6 @@
+let name = "Oduola Fuad";
+let email = "oduolafuad@gmail.com"
+let HNG_id = "HNG-04189";
+let lang = "javascript";
+console.log(`Hello World, this is ${name} with HNGi7 ID ${HNG_id} using the ${lang} for stage 2 task email ${email}.`
+
