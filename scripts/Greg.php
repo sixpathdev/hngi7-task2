73498cbd963a1147c3346cb38b419fee6537c3db
Nwokoye Chigozie <gregoflash05@users.noreply.github.com>
2020-06-05 18:53:07 +0100

    Update Greg.php

--- scripts/Greg.php
+++ scripts/Greg.php
@@ -3,6 +3,6 @@ $email= "gregoflash05@gmail.com";
 $name = "Nwokoye Chigozie";
 $id = "HNG-02811";
 $programming_language = "PHP";
-echo "Hello World, this is $name with HNGi7 ID $id using $programming_language for stage 2 task email $email.";
+echo "Hello World, this is $name with HNGi7 ID $id using $programming_language for stage 2 task email $email";
 
 ?>
