94e2cc1ff685c2ed3828351bbefce8c49b718a24
Victor Peter <ukokjnr@gmail.com>
2020-06-05 21:47:08 +0100

    Edited my work

--- scripts/mytest.js
+++ scripts/mytest.js
@@ -1,7 +1,4 @@
-var name = "Victor Ukok";
-var hngID = "HNG-03805";
 var email = "ukokjnr@gmail.com"
-var language = "Javascript";
 
 console.log(`Hello World, this is Victor Ukok with HNGi7 ID HNG-03805 using Javascript for stage 2 task`)
 
