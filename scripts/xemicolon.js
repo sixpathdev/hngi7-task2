31f193fd3355a095d2ab33b6ac62913ca6fd60de
Xemicolon <1vibenation@gmail.com>
2020-06-05 05:25:09 +0100

    Xemicolon_script

---
+++ scripts/xemicolon.js
@@ -0,0 +1,13 @@
+const details = {
+  email: "jgetitdonefast@gmail.com",
+  name: "Agunloye Oluwatobiloba Light",
+  id: "02614",
+  language: "Javascript",
+};
+
+function helloWorld() {
+  let output = `Hello world, this is ${details.name} with HNGi7 ID ${details.id} using ${details.language} for stage 2 task email ${email}`;
+  console.log(output);
+}
+
+helloWorld();
