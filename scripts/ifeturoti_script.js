731ef7d22d37e196d5cf7e279f09e66b17d2877d
ifeturoti <ifeturotioluwakemi@gmail.com>
2020-06-05 22:22:10 +0100

    added script for task 2

---
+++ scripts/ifeturoti_script.js
@@ -0,0 +1,5 @@
+var name = "Oluwakemi Ifeturoti"
+var email = "ifeturotioluwakemi@gmail.com"
+var hng_id = "HNG-05081"
+var language = "javascript"
+console.log(`Hello World, this is ${name} with HNGi7 ID ${hng_id} using ${language} for stage 2 task email ${email}`)
