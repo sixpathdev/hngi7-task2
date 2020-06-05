31c44424c617ebc2f3d8e9c3b5740bf7a9e897a0
voxboy2 <ebieroma323@gmail.com>
2020-06-05 01:42:19 +0100

    hopefull final commit

--- scripts/donefe.php
+++ scripts/donefe.php
@@ -8,7 +8,7 @@ $lang = "PHP";
 
 
 
-ECHO "Hello World, this is $full_name with HNGi7 ID $hngid  using $lang for stage 2 task email $emai";
+ECHO "Hello World, this is $full_name with HNGi7 ID $hngid  using $lang for stage 2 task email $email";
 
 
 ?>
