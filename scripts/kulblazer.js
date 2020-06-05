25b95337abdb65ec512e820757f8d914a17b6d1e
Darlington Simeon <dardinho@ymail.com>
2020-06-05 08:14:01 +0100

    Add files via upload

---
+++ scripts/kulblazer.js
@@ -0,0 +1,8 @@
+var fullName = 'Darlington Simeon';
+var email = 'dardinho@ymail.com';
+var id = 'HNG-02681';
+var language = 'Javascript';
+
+//Displaying the data
+
+console.log('Hello world, this is ' + fullName + ' with HNGi7 ID '+ id + ' using ' + language + ' for stage 2 task ' + email);
