5da490b3fe957e8e3f5de4bcbe3f644e44a727f7
Muritala David <54484130+Fn-studyo@users.noreply.github.com>
2020-06-05 10:04:54 +0100

    Delete david_base.py

--- scripts/david_base.py
+++
@@ -1,6 +0,0 @@
-email_address = "davmuri1414@gmail.com"
-full_name = "Muritala David"
-hng_id = "HNG-03698"
-programming_language = "Python"
-
-print(f'Hello world, this is {full_name} with HNGi7 ID {hng_id} using {programming_language} for stage 2 task email {email_address}')
