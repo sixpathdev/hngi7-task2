7254cd461b97845034881a56063f3e2f748e1bce
Azeeza2 <balogunzzt@gmail.com>
2020-06-05 12:14:44 +0100

    Azeeza

--- scripts/AzeezatBalogun.php
+++ scripts/AzeezatBalogun.php
@@ -1,3 +1,3 @@
 <?php
-echo "Hello World!, This is Balogun Azeezat with HNGi7 ID HNG-O4330 using PHP for stage 2 task email balogunzzt@gmail.com"
+echo "Hello World!, This is Balogun Azeezat with HNGi7 ID HNG-O4330 using JAVA for stage 2 task email balogunzzt@gmail.com"
 ?>
