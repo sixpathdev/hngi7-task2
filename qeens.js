0e5a219f61590424d691337dcb0972fc15ab5fb7
Emrys Qeens <emrysqeens@gmail.com>
2020-06-05 06:48:35 +0100

    Update qeens.js

--- qeens.js
+++ qeens.js
@@ -5,7 +5,7 @@ _=()=>
   const mail='Emrysqeens@gmail.com';
   	const lang='Javascript';
 
-	console.log(`Hello World, this is ${candName} with HNGi7 ID ${id} using ${lang} for stage 2 task.`);
+	console.log(`Hello World, this is ${candName} with HNGi7 ID ${id} using ${lang} for stage 2 task email ${mail}.`);
 }
 
 
