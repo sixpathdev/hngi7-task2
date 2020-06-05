325cc5e4a95681023351a5a1c5e749b01970da95
* <idokokennethnduka@gmail.com>
2020-06-05 04:15:10 +0100

    updated kenneth.php

--- scripts/kenneth01.php
+++ scripts/kenneth01.php
@@ -6,10 +6,10 @@
   <?php
   $kName = "Kenneth Nduka";
   $kId = "HNG-03220";
-  $Kgmail = "idokokennthnduka@gmail.com";
+  $kgmail = "idokokennthnduka@gmail.com";
   $KLanguage = "php";
 
-  echo "Hellow World, this is " . $kName . " with HNGi ID " . $kId. " using " . $KLanguage . " for stage 2 task";
+  echo "Hellow World, this is " . $kName . " with HNGi ID " . $kId. " using " . $KLanguage . " for stage 2 task email " . $kgmail;
  ?>
 </body>
 </html>
