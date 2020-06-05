bf9a233b7cc0f150d1bb01f2221832d9b406bf3d
Damaris Muange <mdamarisnduku@gmail.com>
2020-06-05 21:12:03 +0300

    my edited commit

--- scripts/damaris.js
+++ scripts/damaris.js
@@ -2,4 +2,4 @@ const name = 'Damaris Muange';
 const hngId = 'HNG-04159';
 const email = 'mdamarisnduku@gmail.com';
 const language = 'javascript';
-console.log('Hello world, this is'+ name + 'with HNGi7 ID'+ hngId + 'using'+ language + 'for stage 2 task');
+console.log('Hello world, this is'+ name + 'with HNGi7 ID'+ hngId + 'using'+ language + 'for stage 2 task email' + email +'.');
