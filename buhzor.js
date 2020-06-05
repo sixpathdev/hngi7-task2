d476490e3eb19769decb9cc73db652b65e56e5a7
buhzor <coolbuhzor@gmail.com>
2020-06-05 06:52:33 +0100

    js included.

---
+++ buhzor.js
@@ -0,0 +1,8 @@
+const myEmail = "coolbuhzor@gmail.com"
+const myName = "Okafor Chibuzor"
+const myHngId = 'HNG-04117'
+const myLang = "javaScript"
+
+let output = ("hello World, this is " + myName + " with HNGi7 ID " + myHngId + " using " + myLang + " for stage 2 task " + myEmail + ".")
+
+console.log(output);
