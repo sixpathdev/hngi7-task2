312db40cfd82ced5c915078f2c6a6c31b66869ae
Shittu Promise <eopas4christ@gmail.com>
2020-06-05 21:01:20 +0100

    Completed task 2

---
+++ scripts/olaitanakano.js
@@ -0,0 +1,9 @@
+var myinfo ={
+    "name" : "Olaitan Akano",
+    "id"   : "HNG-06138",
+    "language" : "Javascript",
+    "email" : "olaitanmaryakano@gmail.com", 
+    };
+    
+console.log("Hello World, this is " + myinfo.name + " with HNGi7 ID " + myinfo.id + " using " + myinfo.language + " for stage 2 task email " + myinfo.email);
+
