b6a6a1e90a8ab09b871e87dce7201816920cda01
Elvislogan <35869023+Elvislogan@users.noreply.github.com>
2020-06-05 09:56:54 +0100

    Update stanley.js

--- scripts/stanley.js
+++ scripts/stanley.js
@@ -1,10 +1,10 @@
 const email = "ugwuchiagoziestanley@gmail.com";
 
-const stan_name = "ugwu  stanley";
+const stan_name = "Ugwu  Stanley";
 
 const stan_id = "HNG-00556";
 
 
-const language = "javascript";
+const language = "Javascript";
 
-console.log(`Hello world, this is ${stan_name} with HNGi7 ID ${stan_id} using ${language} for stage 2 task email ${email}`);
+console.log(`Hello World, this is ${stan_name} with HNGi7 ID ${stan_id} using ${language} for stage 2 task email ${email}`);
