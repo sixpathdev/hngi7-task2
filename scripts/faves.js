c7cd7a81310990d6fac7a406e13c1fe5dfd1bf9d
david odejobi <talk2engineerdavid@gmail.com>
2020-06-05 01:01:08 +0100

    initial commit

---
+++ scripts/faves.js
@@ -0,0 +1,9 @@
+
+
+const name = "DAVID ODEJOBI";
+const id = "HNG-04193";
+const email = "talk2engineerdavid@gmail.com";
+const language = "Javascript";
+
+const result = "Hello World, this is " + name + " with HNGi7 ID "+ id +" and email "+ email + " using "+ language + " for stage 2 task";
+console.log(result);
