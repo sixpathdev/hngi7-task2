bf07a7ad5fa746d280cccb7d81f6d52fcd8b6abd
Kennedykp <46505884+Kennedykp@users.noreply.github.com>
2020-06-05 19:51:03 +0100

    Create kennedy.js
    Js Kennedy

---
+++ kennedy.js
@@ -0,0 +1,11 @@
+let name = 'Kennedy Nwokeocha';
+
+let id = 'HNG-05212';
+
+let email = 'kennedykc55@gmail.com';
+
+let language = 'javascript';
+
+const details = "Hello world this is " + name + " with HNGi7 ID " + id + " using " + language + " for stage 2 task email " + email ;
+
+console.log(details);
