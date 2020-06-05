1d0409fdae0e82a992e1ac11369e14cee5e3065d
Clashkid155 <frankdiss9@gmail.com>
2020-06-05 02:26:42 +0100

    include steam.py

---
+++ scripts/steam.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+name="Njike Francis"
+HNG_id="HNG-05023"
+email="beluchukwunjike@gmail.com"
+language="Python"
+
+output = "Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task email beluchukwunjike@gmail.com".format(name,HNG_id,language)
+print(output)
