6ad8f489be4b9f6c47611fa7b9c404b30a36a660
larrybechtel <54470955+larrybechtel@users.noreply.github.com>
2020-06-05 02:16:05 +0100

    Add files via upload

---
+++ scripts/larrybechtel.js
@@ -0,0 +1,10 @@
+ <script>
+    var name = "Ibrahim Ajala";
+    var ID = "HNG-05288";
+    var mail = "larrybechtel396@gmail.com"
+    var lang = "JavaScript";
+    
+
+document.write("  Hello World, This is  " +    name    + "   with HNGi7 ID   " +   ID    + "  using   " +    lang    + "   for stage 2 task   ");
+console.log("  Hello World, This is  " +    name    + "   with HNGi7 ID   " +   ID    + "  using   " +    lang    + "   for stage 2 task   ");
+</script>
