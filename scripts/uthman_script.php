8e778b9233f0e200ea07830c84f42d9ba029d7bf
huthmanov <ojoduuthman2013@gmail.com>
2020-06-05 12:25:57 +0100

    updated script

---
+++ scripts/uthman_script.php
@@ -0,0 +1,13 @@
+<?php
+
+$email = 'ojoduuthman2013@gmail.com';
+$name = 'Ojodu Uthman';
+$id = "HNG-00145";
+$language = "PHP";
+
+echo "Hello World, this is ".$name." with HNGi7 ID ".$id." using ".$language." for stage 2 task email ".$email;
+
+
+
+
+?>
