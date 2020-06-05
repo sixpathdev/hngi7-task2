c669ac051ff43498ef33daf97115dba23d5583ca
Kennedykp <46505884+Kennedykp@users.noreply.github.com>
2020-06-05 09:18:00 +0100

    Create kennedy.js
    Ken file

---
+++ scripts/kennedy.js
@@ -0,0 +1,11 @@
+let name = 'Kennedy Nwokeocha';
+
+let id = 'HNG-05212';
+
+let email = 'kennedykc55@gmail.com';
+
+let language = 'javascript';
+
+const details = "Hello World, this is " + name + " with HNGi7 ID " + id + " using " + language + " for stage 2 task. Email: " + email ;
+
+console.log(details);
