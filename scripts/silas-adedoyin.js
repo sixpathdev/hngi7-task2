8378b9a6c3961d26ac038b7139cda9d8a85319df
Silas Adedoyin <silasfaarda@gmail.com>
2020-06-05 15:10:42 +0100

    Create silas-adedoyin.js

---
+++ scripts/silas-adedoyin.js
@@ -0,0 +1,8 @@
+const emailAddress = "silasadedoyin@gmail.com";
+const myName = "Adedoyin Silas";
+const myId = "HNG-03809";
+const languageUsed = "JavaScript";
+
+const outputLine = `Hello World, this is ${myName} with HNGi7 ID ${myId} using ${languageUsed} for stage 2 task email ${emailAddress}`;
+
+console.log(outputLine);
