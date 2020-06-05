c941274e48c83ba3f4d49f31c94dcab54277ce96
Johnexzy <obajohn75@gmail.com>
2020-06-05 08:21:15 +0100

    Update johnoba.php

--- scripts/johnoba.php
+++ scripts/johnoba.php
@@ -4,4 +4,6 @@ $name = 'Oba John';
 $id = 'HNG-04363';
 $email = 'obajohn75@gmail.com';
 $language = 'PHP';
-echo sprintf('Hello world, this is %s with HNGi7 ID %s using %s for stage 2 task', $name, $id, $language);;
+echo sprintf('Hello world, this is %s with HNGi7 ID %s using %s for stage 2 task email %s', $name, $id, $language, $email);
+
+?>
