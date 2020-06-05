86c9f26c5e0e8c7fd6f986ba5319e8f3e1c062bf
udemni <61274858+udemni@users.noreply.github.com>
2020-06-05 18:58:13 +0100

    Update Udemni_ HNG-05777.php

--- Udemni_ HNG-05777.php
+++ Udemni_ HNG-05777.php
@@ -7,7 +7,7 @@ $_language_used = "Php";
 $_email ="justinobarogie@gmail.com";
 
 
-echo $_name = ' Hello World, this is '. $_full_name .' with HNGi7 ID '.$_id. ' using '.$_language_used. ' for stage 2 task. Email'.$_email. '.' ;
+echo $_name = ' Hello World, this is '. $_full_name .' with HNGi7 ID '.$_id. ' using '.$_language_used. ' for stage 2 task. email'.$_email. '.' ;
 
 
 
