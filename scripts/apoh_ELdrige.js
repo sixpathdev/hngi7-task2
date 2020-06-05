8ee2efe6fa3b4984d5725dab186299f7c6c14e7a
Eldrige <52784114+eldrige@users.noreply.github.com>
2020-06-05 09:53:38 +0100

    final commit
    Hope it is ok, Sorry for not following the rules

--- scripts/apoh_ELdrige.js
+++ scripts/apoh_ELdrige.js
@@ -1,10 +1,10 @@
 const info = ()=>{
+    let emailAddress = 'peldrige8@gmail.com'
     let userName = 'Apoh Eldrige'
     let internId = 'HNG-02427'
-    let emailAddress = 'peldrige8@gmail.com'
     let language = 'Javascript'
 
-    return `Hello World, this is ${userName} with HNGi7 ID ${internId} using ${language} for stae 2 task ${emailAddress}`
+    return `Hello World, this is ${userName} with HNGi7 ID ${internId} using ${language} for stage 2 task ${emailAddress}`
 }
 
 console.log(info())
