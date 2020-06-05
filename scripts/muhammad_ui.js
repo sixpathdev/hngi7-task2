f44e888cf33c9b72f1d67ed1d5c2cbb24cf62ad9
Muhammad UI <muhammadbmw69@gmail.com>
2020-06-05 11:56:43 +0100

    added mi script with email

--- scripts/muhammad_ui.js
+++ scripts/muhammad_ui.js
@@ -2,5 +2,6 @@ const email = "muhammadbmw69@gmail.com";
 const name = "Muhammad Ibrahim Umar";
 const hngId = "HNG-01904";
 const language = "Javascript";
-let output = `Hello World, this is ${name} with HNGi7 ID ${hngId} using ${language} for stage 2 task`;
-console.log(output);
+console.log(
+  `Hello World, this is ${name} with HNGi7 ID ${hngId} using ${language} for stage 2 task ${email}`
+);
