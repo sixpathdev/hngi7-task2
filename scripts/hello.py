628a969e763ad8ac566b207b1a7caca959add673
Gerald-Izuchukwu <geraldlouisugwunna@gmail.com>
2020-06-05 17:26:24 +0100

    noticed a comma was missing

--- scripts/hello.py
+++ scripts/hello.py
@@ -6,4 +6,4 @@ language = "Python"
 
 email = "geraldlouisugwunna@gmail.com"
 
-print(f'Hello World this is {full_name} with HNGi7 ID {hngi7_ID} using {language} language for stage 2 task email {email}')
+print(f'Hello World, this is {full_name} with HNGi7 ID {hngi7_ID} using {language} language for stage 2 task email {email}')
