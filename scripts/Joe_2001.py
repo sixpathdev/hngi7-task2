49916fdec69c652d463e352164e4b4270cfe1e2d
eriteria <51262474+eriteria@users.noreply.github.com>
2020-06-05 01:23:19 +0100

    Update Joe_2001.py

--- scripts/Joe_2001.py
+++ scripts/Joe_2001.py
@@ -1,5 +1,6 @@
-full_name="Joseph John"
-hng_id="HNG-04194"
-language="python"
-email="joseph.john.ali.2001@gmail.com"
-print("Hello World, this is",full_name,"with","HNGi7 ID",hng_id,"using",language,"for the stage 2 task email",email)
+email_address = "joseph.john.ali.2001@gmail.com"
+full_name = "Joseph John"
+hng_id = "HNG - 04194"
+programming_language = "Python"
+
+print(f'Hello world, this is {full_name} with HNGi7 ID {hng_id} using {programming_language} for stage 2 task email {email_address}')
