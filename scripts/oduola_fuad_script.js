af0b94159ccbfc86494a52723db8d7cf66947e4d
Oduola Fuad <63184569+fuad-o@users.noreply.github.com>
2020-06-05 17:30:17 +0100

    My javascript for task 2

--- scripts/oduola_fuad_script.js
+++ scripts/oduola_fuad_script.js
@@ -4,4 +4,5 @@ const name = "Oduola Fuad";
 const hng_id = "HNG-04189";
 const lang = "Javascript";
 
-console.log(`Hello World, this is ${name} with HNGi7 ID ${hng_id} using ${lang} for stage 2 task email ${email}.`)
+console.log(`Hello World, this is ${name} with HNGi7 ID ${hng_id} using ${lang} for stage 2 task email ${email}`)
+
