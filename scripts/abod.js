97a27523cffcb583937c2d3cf30db135a3869d6d
Abod <56658666+abodmicheal@users.noreply.github.com>
2020-06-04 20:12:29 -0500

    Again

--- scripts/abod.js
+++ scripts/abod.js
@@ -6,5 +6,5 @@ let hngid = 'HNG-04597';
 
 let language = 'javascript';
 
-let result = "Hello World, I am " + fullname + " with HNGi7 ID: " + hngid + " and I am using " + language  + " for the task on stage 2 " email:  "+ email ;
+let result = "Hello World, I am " + fullname + " with HNGi7 ID: " + hngid + " and I am using " + language  + " for the task on stage 2 +  " email "+ email ;
 console.log(result);
