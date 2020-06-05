2db75b4c91c5d717e4aa18064e791121f4414f7f
fayrok <66219909+fayrok@users.noreply.github.com>
2020-06-05 08:23:07 +0200

    Update hng00258.js

--- scripts/hng00258.js
+++ scripts/hng00258.js
@@ -9,5 +9,5 @@ const output = ()=>
 {
     return `Hello World, this is ${PERSONAL.name} with HNGi7 ID ${PERSONAL.id} and using ${PERSONAL.language} for stage 2 task email ${PERSONAL.email} .`;
 
-
+}
 console.log(output());
