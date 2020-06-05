cb31f50d26ac3bc98f34a1b5f9a7e330f61cd603
Elie237 <66333199+Elie237@users.noreply.github.com>
2020-06-06 02:06:01 +0300

    Revert "Update HNG-Task2.js"
    This reverts commit 564845e924a6f5da826407488ce9f6a5312f63e6.

--- scripts/HNG-Task2.js
+++ scripts/HNG-Task2.js
@@ -1,6 +1,8 @@
-const email = 'otigbachidera@gmail.com'
-const fullName = 'Otigba Chidera'
-const id = 'HNG-00525'
-const stack = 'Javascript'
-
-console.log (` "Hello World, this is ${fullName} with HNGi7 ID ${id} using ${stack} for stage 2 task email ${email}"`)
+var myInfo = () => {
+    var name = 'Silas Abiodun';
+    var id = 'HNG-04406';  
+    var lang = 'Javascript';
+    var email = 'silasabiodun25@gmail.com';
+return "Hello World, this is " + name + " with HNGi7 ID " + id + " using " + lang + ' for stage 2 task email ' + email; 
+}
+console.log(myInfo());
