555e079a61afb676949d57b31d040f67b3098ef1
Chidi Okpala <63919087+ChidiOkpala@users.noreply.github.com>
2020-06-05 01:32:03 +0100

    Chidi,s Script

--- scripts/Chidi,s_Script.js
+++ scripts/Chidi,s_Script.js
@@ -5,4 +5,3 @@ let language = 'javascript';
 
 let output = 'Hello World, this is ' + name + ' with HNGi7 ID ' + hng_id +  ' using ' + language + ' for stage 2 task email ' + email;
 console.log(output);
-
