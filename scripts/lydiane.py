c81228f4910e00ec3172d09ecf289934239aee4e
lydiane <kengnelydiane684@gmail.com>
2020-06-05 18:00:01 +0100

    update lydiane.py
    changes on lydiane's file

--- scripts/lydiane.py
+++ scripts/lydiane.py
@@ -4,4 +4,4 @@ Id = "HNG-00371"
 email = "kengnelydiane684@gmail.com"
 language = "python"
 
-print ("Hello World, this is " + name +" with HNGi7 ID "+Id + " using language " +language +" for stage 2 task")
+print ("Hello World, this is " + name +" with HNGi7 ID "+Id + " using " +language +" for stage 2 task")
