8596c8fb3482fab4004520630d8faf26eecbf0ab
Joshua Odejide <48139470+joshuaodejide@users.noreply.github.com>
2020-06-05 09:44:18 +0100

    Revert "Onigemo HNG-01227 "

--- scripts/Harith_HNG-01227script.js
+++
@@ -1,6 +0,0 @@
-let emailAdd = "onigemotosin@gmail.com";
-let name = "Harith Onigemo";
-let hng_idd = "HNG-01227";
-let lang = "Javascript";
-
-console.log(`Hello world, this is ${name} with HNG ID ${hng_idd} using ${lang} for stage 2 task email ${emailAdd}`)
