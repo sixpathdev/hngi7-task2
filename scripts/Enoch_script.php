aaa275f93d5bd568617476640f6f70749954b1f0
Enoch01 <66218372+Enoch01@users.noreply.github.com>
2020-06-05 23:04:32 +0100

    Delete Enoch_script.php

--- scripts/Enoch_script.php
+++
@@ -1,5 +0,0 @@
-+name = "Enoch Owoeye";
-+id = "00991";
-+email = "enochowoeye001@gmail.com";
-+Language = "PHP";
-+print("Hello World, this is "+ name +" with HNGi7 ID "+id +" using "+ Language +" for stage 2 task email ")
