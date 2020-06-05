5af51971bb79d14388564511d5daadfecd8a5452
Xemicolon <1vibenation@gmail.com>
2020-06-05 05:30:23 +0100

    Xemicolon_script

--- scripts/xemicolon.js
+++ scripts/xemicolon.js
@@ -6,7 +6,7 @@ const details = {
 };
 
 function helloWorld() {
-  let output = `Hello world, this is ${details.name} with HNGi7 ID ${details.id} using ${details.language} for stage 2 task email ${email}`;
+  let output = `Hello World, this is ${details.name} with HNGi7 ID ${details.id} using ${details.language} for stage 2 task email ${email}`;
   console.log(output);
 }
 
