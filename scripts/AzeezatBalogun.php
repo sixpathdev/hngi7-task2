31d37ce05392344e24cb4c08ad15ee0930de03ff
Azeeza2 <balogunzzt@gmail.com>
2020-06-05 13:10:35 +0100

    Edite

--- scripts/AzeezatBalogun.php
+++ scripts/AzeezatBalogun.php
@@ -1,3 +1,9 @@
 <?php
-echo "Hello World!, This is Balogun Azeezat with HNGi7 ID HNG-O4330 using JAVA for stage 2 task email balogunzzt@gmail.com"
+
+$name = "Azeezat Balogun";
+$id = "HNG-04330";
+$lang = "PHP";
+$email = "balogunzzt@gmail.com";
+
+echo "Hello World!, This is $name with HNGi7 ID $id using $lang for stage 2 task email $email";
 ?>
