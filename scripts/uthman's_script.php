f03709bce15bfc54a5de564f24738eac467fe94c
huthmanov <ojoduuthman2013@gmail.com>
2020-06-05 06:13:56 +0100

    new uthman's_script.php

--- scripts/uthman's_script.php
+++ scripts/uthman's_script.php
@@ -4,6 +4,6 @@ $name = "Ojodu Uthman";
 $id = "HNG-00145";
 $language = "PHP";
 
-echo "Hello world, this is $name with HNGi7 id $id using $language for Stage 2 Task";
+echo "Hello world, this is $name with HNGi7 id $id using $language for Stage 2 Task and email $email";
 
  ?>
