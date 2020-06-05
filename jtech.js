4851f14520664ab1c5ed183a4f243c2524fc7406
johntaiwo005 <66323991+johntaiwo005@users.noreply.github.com>
2020-06-05 15:40:52 +0100

    JTech
    Done as required! I used javascript.

---
+++ jtech.js
@@ -0,0 +1,5 @@
+const name = "Taiwo John";
+const email = "johntaiwo909@gmail.com";
+const id = "HNG-05848";
+const language = "Javascript";
+console.log("Hello World, this is " + name + "with HNGi7 ID " + id + "using " + language + "for stage 2 task email " + email +".");
