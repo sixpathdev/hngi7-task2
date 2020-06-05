575dbc202dc2915a3323734ba41c1e14d69101bd
Darey <horla@Dareys-MacBook-Pro.local>
2020-06-05 01:16:34 +0100

    olaoluwa

---
+++ scripts/Olaluwa.js
@@ -0,0 +1,8 @@
+const fullName = "Dare Sodiq";
+const internshipId = "HNG-00451";
+const emailAddress = "darehorla@gmail.com";
+const language = "JavaScript";
+
+console.log(
+  `Hello Everyone, my name is ${fullName} my HNGi7 ID ${internshipId} using ${language} for stage 2 task ${emailAddress}`
+);
