7d574e079e67e4d4a5e451b57311ed25c3d4409b
Joe-Bolu <66218895+Joe-Bolu@users.noreply.github.com>
2020-06-05 01:11:39 +0100

    Delete joebolu_script.py

--- scripts/joebolu_script.py
+++
@@ -1,8 +0,0 @@
-name = "Boluwatife Babajimi-Joseph"
-id = "HNG-00228"
-email = "josh@joebolu.com"
-language = "Python"
-
-print("Hello World, this is " + name + " with HNGi7 ID " + id + " using " + language + " for stage 2 task")
-
-
