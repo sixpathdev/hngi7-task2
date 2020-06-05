7f0f7943083ecd7bceb2f5eb75e5d450fb4c6c4c
timolsoft <timolcode@gmail.com>
2020-06-05 18:50:31 +0100

    timolsoft task

--- scripts/tim6711.php
+++ scripts/tim6711.php
@@ -1,10 +1,6 @@
 <?php
 
 $name = "Timothy Olaniyi";
-$id = "HNG-06711"; 
-$email = "timolcode@gmail.com";
-$lang = "PHP";
+$id = "HNG-06711"; $lang = "PHP";
 
-echo "Hello World,This is   " . $name . " with HNGi7 ID  " . $id . "  using ". $lang . "  for stage 2 task  email:   " . $email  
-
-?>
+echo "Hello World,This is   " . $name . " with HNGi7 ID  " . $id . "  using ". $lang . "  for stage 2 task
--- scripts/timolsoft.php
+++ scripts/timolsoft.php
@@ -2,9 +2,8 @@
 
 $name = "Timothy Olaniyi";
 $id = "HNG-06711"; 
-$email = "timolcode@gmail.com";
 $lang = "PHP";
 
-echo "Hello World,This is   " . $name . " with HNGi7 ID  " . $id . "  using ". $lang . "  for stage 2 task  email:   " . $email  
+echo "Hello World,This is   " . $name . " with HNGi7 ID  " . $id . "  using ". $lang . "  for stage 2 task 
 
 ?>
