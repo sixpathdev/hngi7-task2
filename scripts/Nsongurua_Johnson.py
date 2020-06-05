1e2e0f743efe32f2318d719d7552b737cf900840
Nsongurua Johnson <nsonguruajohnson@gmail.com>
2020-06-05 00:47:16 -0700

    Corrected scripts

---
+++ scripts/Nsongurua_Johnson.py
@@ -0,0 +1,8 @@
+myName = "Nsongurua Johnson"
+HNG_ID = "HNG-01068"
+myEmail = "nsonguruajohnson@gmail.com"
+Language = "Python"
+
+print ("Hello World, this is" , myName, "with HNGi7 ID", HNG_ID, "using", Language, "for stage 2 task email", myEmail)
+
+5
