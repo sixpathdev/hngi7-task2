1ab31e3f28e74aef133b60cf4b901a2ed275ca93
Noble-Genius <56192742+Noble-Genius@users.noreply.github.com>
2020-06-05 12:55:33 +0100

    Add files via upload
    Jafar Abdulwaheed Task 2 edited

--- NobleGenius.js
+++ NobleGenius.js
@@ -1,9 +1,6 @@
-var email = "jafar.aolayiwola@gmail.com";
 var name = "Jafar Abdulwaheed";
+var email = "jafar.abdulwaheed@gmail.com";
 var hng_id = "HNG-05645";
 var language = "JavaScript";
-
-var myText = "Hello World, this is " + name + " with HNGi7 ID " + hng_id +
-                " using " + language + " for stage 2 task email " + email ;
-
+var myText = "Hello World, this is " + name + " " + " with HNGi7 ID " + hng_id + " " + " using " + language + " " + " for stage 2 task email " + email ;
 console.log(myText);
