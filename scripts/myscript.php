55bd0ad9b2592a1cb8f9ea97e6d0c819bb0990db
misi <ayoadefunmi59@gmail.com>
2020-06-05 18:34:58 +0100

    feat added myscript.php

--- scripts/myscript.php
+++ scripts/myscript.php
@@ -3,5 +3,5 @@ $name = "Ayoade"." "."Oluwafunmisi";
 $hngid = "HNG-01173";
 $email = "ayoadefunmi59@gmail.com";
 $language = "PHP";
-echo "Hello World, this is ".$name."  with HNGi7 ID ".$hngid." using ".$language." for stage 2 task";
+echo "Hello World, this is ".$name."  with HNGi7 ID ".$hngid." using ".$language." for stage 2 task email $email ";
 ?>
