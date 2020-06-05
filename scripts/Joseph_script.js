5f11843e9d0f4f5037db7863baf1e181cf1299a6
Joseph Atada <jayatada@gmail.com>
2020-06-05 13:44:15 +0100

    feat: added joseph_script.js

---
+++ scripts/Joseph_script.js
@@ -0,0 +1,8 @@
+const name = "Joseph Atada";
+const hng_id = "HNG-00606";
+const email = "jayatada@gmail.com";
+const lang = "JavaScript";
+Task = () => {
+ console.log(`Hello World, this is ${name} with HNGi7 ID ${hng_id} using ${lang} for stage 2 task email ${email}`);
+}
+Task();
