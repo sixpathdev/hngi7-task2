75b036e4015866ecadf30747b5e14982a41f52a3
fitzgerald kachi <57835051+geraldkachi@users.noreply.github.com>
2020-06-05 19:03:25 +0100

    Create kachiman.py

---
+++ scripts/kachiman.py
@@ -0,0 +1,7 @@
+name = 'Gerald Kachi'
+hng_id = 'HNG-03447'
+email = 'Fitzgeraldkachi@gmail.com'
+language = 'Python'
+output = 'Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task email %s' %(name,hng_id,language,email)
+
+print(output)
