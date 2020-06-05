a6b3ef6649105c19185e95f8966400ed2c5ff549
JohnLightLuc <47230594+JohnLightLuc@users.noreply.github.com>
2020-06-05 02:33:10 +0000

    Make script hello

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-
+<?php
+    include('scripts/hng06157.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        echo "<h1>Hello World, this is  with HNGi7 ID ".$data['id']. "and email ".$data['email']. " using ".$data['language']. " for stage 2 task</h1>";
+    ?>
+</body>
+</html>
---
+++ scripts/hng06157.php
@@ -0,0 +1,11 @@
+<?php
+
+$data = [
+    'id'=> "HNG-06157",
+    'fullname'=> "TOURE Lamagnigui",
+    'email'=> "lamagniguitoure@gmail.com",
+    'language'=> "PHP"
+];
+
+
+
