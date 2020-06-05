07d4512931a0f2d7631bf15752b2354370de417a
Adeyemi Timilehin <adeyemitimmy@gmail.com>
2020-06-05 19:29:58 +0100

    Adeyemi Timilehin Mike

---
+++ scripts/AdeyemiTimilehin-HNG-02251.js
@@ -0,0 +1,7 @@
+const email     = 'adeyemitimmy@gmail.com';
+const fullname  = 'Adeyemi Timilehin Mike;
+const hng_id    = 'HNG-02251';
+const language  = 'JavaScript';
+
+let output = 'Hello world, this is ' +fullname+ ' with HNGi7 ID '+hng_id+ ' using ' +language+ ' for the stage 2 task email '+email+;
+console.log(output)
