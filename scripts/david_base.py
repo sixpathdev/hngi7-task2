ca7f7ad7c7a5aeb1143b6ca860e43be32b262a8c
Muritala David <54484130+Fn-studyo@users.noreply.github.com>
2020-06-05 10:42:09 +0100

    Update david_base.py

--- scripts/david_base.py
+++ scripts/david_base.py
@@ -1,6 +1,7 @@
-email_address = "davmuri1414@gmail.com"
-full_name = "Muritala David"
-hng_id = "HNG-03698"
-programming_language = "Python"
+name = 'Muritala David'
+hng_id = 'HNG-03698'
+email = 'davmuri1414@gmail.com'
+language = 'Python'
+output = 'Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task email %s' %(name,hng_id,language,email)
 
-print(f'Hello world, this is {full_name} with HNGi7 ID {hng_id} using {programming_language} for stage 2 task email {email_address}')
+print(output)
