2e1c93abdf102ecacef9f1c9c60081198347c4d3
Lablonde-Developper <[lkalalizi@gmail.com]>
2020-06-05 00:33:02 -0700

    This is my updated HNGi7-task2

--- scripts/Lablonde-script.js
+++ scripts/Lablonde-script.js
@@ -1,6 +1,6 @@
-let FullName = "Lablonde Juliette Kalalizi";
-let Hngi7_id ="HNG-03269";
 let myEmail = "lkalalizi@gmail.com";
+let myName = "Lablonde Kalalizi";
+let hngi7_id ="HNG-03269";
 let language = "JavaScript";
 
-console.log(`Hello world! this is ${FullName} with ID ${Hngi7_id} using ${language} for task 2 task `);
+console.log(`Hello world, this is ${myName} with HNGi7 ID ${hngi7_id} using ${language} for task 2 task email ${myEmail}`);
