2fd5367f8180358bf8b11fc601876513f1ed5850
Enoch01 <66218372+Enoch01@users.noreply.github.com>
2020-06-05 15:54:28 +0100

    Update Enoch_script.py

--- scripts/Enoch_script.py
+++ scripts/Enoch_script.py
@@ -1,5 +1,5 @@
-+name = "Enoch Owoeye";
-+id = "00991";
-+email = "enochowoeye001@gmail.com";
-+Language = "PHP";
-+print("Hello World, this is "+ name +" with HNGi7 ID "+ id +" using "+ Language +" for stage 2 task email "+ email +)
+name = "Enoch Owoeye";
+id = "HNG-00991";
+email = "enochowoeye001@gmail.com";
+Language = "Phyton";
+print("Hello World, this is "+ name +" with HNGi7 ID "+ id +" using "+ Language +" for stage 2 task email "+ email)
