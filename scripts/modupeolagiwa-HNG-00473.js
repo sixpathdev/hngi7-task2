b8daf061de8b203d953362d2127a92e39657e8ba
modupe-giwa <57496243+modupe-giwa@users.noreply.github.com>
2020-06-05 14:48:11 +0100

    Add files via upload

--- scripts/modupeolagiwa-HNG-00473.js
+++ scripts/modupeolagiwa-HNG-00473.js
@@ -1,7 +1,6 @@
-const Email = "gmodupe11@gmail.com";
+const email = "gmodupe11@gmail.com";
 const name = "Modupeola Giwa";
 const id = "HNG-00473";
-const Language = "javaScript";
+const language = "Javascript";
 
- 
-console.log("Hello World, this is "+ name +" with HNGi7 ID "+ id +" using "+ Language +" for stage 2 task email,"+ Email);
+console.log(`Hello world, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task email ${email}`)
