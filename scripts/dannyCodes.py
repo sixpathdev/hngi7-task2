731805a8b4a2f9e4c19d5c1b5300e78e8db5dd30
taylormed <taylormed2@gmail.com>
2020-06-05 03:26:12 +0100

    Update dannyCodes.py

--- scripts/dannyCodes.py
+++ scripts/dannyCodes.py
@@ -3,4 +3,4 @@ name = "Akpan Daniel"
 hngId = "HNG-06656"
 language = "Python"
 
-print("Hello World, this is {}, with HNGi7 ID {} using {} for stage 2 task".format(name, hngId, language))
+print("Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task email {}".format(name, hngId, language, email))
