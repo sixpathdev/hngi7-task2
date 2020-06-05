1580e959b67ab51240d134b8c948a2e5e32bca3e
stanley <ugwuchiagoziestanley@gmail.com>
2020-06-05 06:56:43 +0100

    used the new format

---
+++ scripts/stanley.js
@@ -0,0 +1,10 @@
+const EMAIL = "ugwuchiagoziestanley@gmail.com";
+
+const STAN_NAME = "ugwu  stanley";
+
+const STAN_ID = "HNG-00556";
+
+
+const LANGUAGE = "javascript";
+
+console.log(`Hello world, this is ${STAN_NAME} with HNGi7 ID ${STAN_ID} using ${LANGUAGE} for stage 2 task email ${EMAIL}`);
