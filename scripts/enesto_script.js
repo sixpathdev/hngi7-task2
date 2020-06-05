23a6387acdfb04d9b58a5e00045accf8e1477484
Ernest Nnamdi <ernestnamdi6@gmail.com>
2020-06-05 01:42:47 -0700

    enesto_script.js

--- scripts/enesto_script.js
+++ scripts/enesto_script.js
@@ -4,5 +4,5 @@ let id = "HNG-03858";
 
 let language = "JavaScript"
 
-const output = `Hello World!, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task email ${email}`;
+const output = `Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task email ${email}`;
 console.log(output);
