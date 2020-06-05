27ede7778e0037acabba7251ae7926a118b00b46
Hamza <hamzaelessawy@gmail.com>
2020-06-05 16:06:59 +0800

    added my script Hamza

---
+++ scripts/hamza_script.js
@@ -0,0 +1,8 @@
+// My task for Hng internship to go forward //
+
+let fullName = "Hamza Elessawy";
+let hngId = "03392" ;
+let email = "hamzaelessawy@gmail.com";
+let language = "JavaScript";
+let welcomeMessage = "Hello World, this " + fullName + "and My Internship ID is" + hngId + "to contact my email " + email + "the language i used " + language ;
+console.log(welcomeMessage);
