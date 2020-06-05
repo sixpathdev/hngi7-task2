a78445aabe29c0e6b4e5cca566505439b7a58f4b
Okafor Henry <okafohenrie@gmail.com>
2020-06-05 17:29:57 +0100

    Update Henry_oc.js
    Removed punctuation and added email string

--- scripts/Henry_oc.js
+++ scripts/Henry_oc.js
@@ -4,7 +4,7 @@ name = 'Okafor Henry';
 hng_id = 'HNG-06037';
 language = 'Javascript';
 
- console.log("Hello World, this is " + name +  " with HNGi7 ID " + hng_id + " using " + language + " for stage 2 task.");
+ console.log("Hello World, this is " + name +  " with HNGi7 ID " + hng_id + " using " + language + " for stage 2 task email " + email);
 	
 
 
