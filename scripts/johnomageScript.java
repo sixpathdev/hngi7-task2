17fff2995b73d73adac07fe1feb300c88457db29
johnomage <31286651+johnomage@users.noreply.github.com>
2020-06-05 06:50:37 +0100

    Create johnomageScript.java

---
+++ scripts/johnomageScript.java
@@ -0,0 +1,12 @@
+public class johnomageScript
+{
+           public static void main(String[] edus){
+		String fullName = "John Omage";
+		String ID = "HNG-01373";
+		String language = "Java";
+		String email = "john.e.omage@gmail.com";
+		System.out.print("Hello, World ");
+		System.out.println("I'm " + fullName + ", with HNGi7 ID " + ID + " using " + language + " for stage 2 task." + " Email is " + email);
+	}
+
+}
