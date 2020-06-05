ec6514863e24eb44c7610c67acb2e7b8cc5b798d
fayrok <66219909+fayrok@users.noreply.github.com>
2020-06-05 08:22:12 +0200

    Update hng00258.js

--- scripts/hng00258.js
+++ scripts/hng00258.js
@@ -7,7 +7,7 @@ const  PERSONAL= {
 
 const output = ()=>
 {
-    return `Hello World, this is ${PERSONAL.name} with HNGi7 ID ${PERSONAL.id} and email ${PERSONAL.email} using ${PERSONAL.language} for stage 2 task.`;
-}
+    return `Hello World, this is ${PERSONAL.name} with HNGi7 ID ${PERSONAL.id} and using ${PERSONAL.language} for stage 2 task email ${PERSONAL.email} .`;
+
 
 console.log(output());
