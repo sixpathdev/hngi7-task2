9993602256a52de41aaca89c99c72ab831f8beb1
Enoch01 <66218372+Enoch01@users.noreply.github.com>
2020-06-05 02:12:36 +0100

    Update Enoch_script.py

--- scripts/Enoch_script.py
+++ scripts/Enoch_script.py
@@ -1,6 +1,6 @@
 +const name = "Enoch Owoeye";
 +const id =  "HNG-00991";
 +const email = "enochowoeye001@gmail.com";
-+const language = "Python";
++const language = "PHP";
 +const status = "Pass"
 +console.log("Hello World, this is +name with HNGi7 ID HNG- "id" using + Language for Stage 2 task"
