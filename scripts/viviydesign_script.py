3fc988f89a8c334d0e018ad3681354a719b63438
viviydesign <66270014+viviydesign@users.noreply.github.com>
2020-06-05 02:27:10 +0100

    Add files via upload

---
+++ scripts/viviydesign_script.py
@@ -0,0 +1,10 @@
+Email = "Ofodum101@gmail.com"
+
+Name = "Ofodum Ogechukwu"
+
+id = "HNG-04571"
+
+language= "Python"
+
+print("Hello World, this is  "+ Name + " with HNGi7 ID " + id + " using " +language + " for stage 2 task. Email "+ Email)
+
