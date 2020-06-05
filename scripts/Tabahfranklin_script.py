a46b070e49129049f8731660d26ac59f6431d5bf
Bill Kif <tabah.franklin@gmail.com>
2020-06-05 17:51:11 +0100

    Update Tabahfranklin_script.py

--- scripts/Tabahfranklin_script.py
+++ scripts/Tabahfranklin_script.py
@@ -1,5 +1,7 @@
 
+my_Name = "Tabah Franklin Kifem "
+hngi7_id = "HNG-02657 "
+my_Email = "tabah.franklin@gmail.com "
+language = "Python "
 
-string_word = "Hello World, this is Tabah Franklin Kifem with HNGi7 ID HNG-02657 using Python for stage 2 task email tabah.franklin@gmail.com"
-
-print(string_word)
+print("Hello World, this is " + my_Name + "with HNGi7 ID " + hngi7_id + "using " + language + "for stage 2 task email " + my_Email)
