9067dfbfadf92e0872cf6f0eed6a90c39334cb80
Anowi Reginald Kosisochukwu <anowi_5@yahoo.com>
2020-06-05 08:32:37 +0100

    Update kosis_script.py

--- scripts/kosis_script.py
+++ scripts/kosis_script.py
@@ -1,5 +1,5 @@
+email='anowi_5@yahoo.com'
 namaewa= 'ANOWI KOSI'
 ID= 'HNG-02205'
 language='PYTHON'
-email='anowi_5@yahoo.com'
 print('Hello World this is',namaewa,'with HNGi7 ID',ID,'using',language,'for stage 2 task')
