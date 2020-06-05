14ae1730f54ee116f53409a789e7e4dfcd6f9fbe
charles <charles.onuorah@yahoo.com>
2020-06-05 07:02:47 +0100

    Create charles_script.js
    Removed Email from output

---
+++ scripts/HNG-03043.js
@@ -0,0 +1,7 @@
+const email = "charles.onuorah@yahoo.com"
+const fullName = "Charles Onuorah"
+const ID = "HNG-03043"
+const language = "Javascript"
+
+
+console.log(`Hello World, this is ${fullName} with HNGi7 ID ${ID}  using ${language}  for stage 2 task`)
