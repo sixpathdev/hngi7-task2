7e7cd4a799e8d761e9ede084586f37472dc61b39
viviydesign <66270014+viviydesign@users.noreply.github.com>
2020-06-05 03:22:15 +0100

    Update viviydesign_script.py

--- scripts/viviydesign_script.py
+++ scripts/viviydesign_script.py
@@ -6,5 +6,5 @@ id= "HNG-04571"
 
 language= "Python"
 
-print("Hello World, this is  "+ name + " with HNGi7 ID " + id + " using " +language + " for stage 2 task. Email "+ email)
+print("Hello World, this is  "+ name + " with HNGi7 ID " + id + " using " +language + " for stage 2 task email "+ email)
 
