b01ccd62049e3d1bce9879f693874cb0db73e36e
rajilateef <rajilateef.rl@gmail.com>
2020-06-05 07:28:36 +0100

    edited script

--- scripts/latech.php
+++ scripts/latech.php
@@ -4,12 +4,12 @@ $name = "RAJI AJIBOLA LATEEF";
 $id = "HNG-01859";
 $email = "rajilateef.rl@gmail.com";
 $language = "php";
-function bio($fullname, $id, $language){
-    return ("Hello World, this is $fullname with HNGi7 ID $id using $language for stage 2 task");
+function bio($fullname, $id, $language, $email){
+    return ("Hello World, this is $fullname with HNGi7 ID $id using $language for stage 2 task email $email");
 }
 
 //for output
-$output = bio($name, $id, $language);
+$output = bio($name, $id, $language, $email);
 echo($output);
 
 ?>
