bc5c7d8630c4fa9f42d2851dc635d537f0bc6b64
ClementPeter <ClementPeter@users.noreply.github.com>
2020-06-05 08:35:19 +0100

    Updated clementpeter.py

--- scripts/clementpeter.py
+++ scripts/clementpeter.py
@@ -1,6 +1,6 @@
 name = "Clement Peter "
-hngi7_ID = "HNG-06643"
+hngi7_ID = "HNG-06643 "
 email = "clementpeter2000@gmail.com"
 language = "Python"
 print("Hello World,My name is " + name + " with HNGi7 ID " +
-      hngi7_ID + " using " + language + " for stage 2 task")
+      hngi7_ID + email+" using " + language + " for stage 2 task")
