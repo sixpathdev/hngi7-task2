3dbb59b9a57e2e626f683024a816414b735fd6a1
Davidson <Davidbrain162@gmail.com>
2020-06-05 17:27:16 +0100

    feat :added Davidtask1.js

--- scripts/task1.js
+++ scripts/task1.js
@@ -3,4 +3,4 @@ const id = 'HNG-06359';
 const email = 'Davidbrain162@gmail.com';
 const language = 'Javascript';
 
-console.log('Hello world, this is ' + names + ' with HNGi7 ID ' + id + ' with email: ' + email + ' using ' + language + ' for stage 2 task.')
+console.log('Hello world, this is ' + names + ' with HNGi7 ID ' + id  + ' using ' + language + ' for stage 2 task email' + email)
