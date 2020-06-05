fb13f18815bd8f3704eb77460e0db15981a2a0fc
Muhammad Faruk <mahammadfumar5@gmail.com>
2020-06-05 07:40:41 +0100

    Edited The Format

--- scripts/Faruk_Ruwa.js
+++ scripts/Faruk_Ruwa.js
@@ -3,4 +3,4 @@ const Name = 'Faruk Umar Muhammad';
 const HNG_id = 'HNG-02152';
 const language = 'JavaScript';
 
-console.log(`Hello World, this is ${Name} with HNGi7 ID ${HNG_id} using ${language} for stage 2 task ${email}`);
+console.log(`Hello World, this is ${Name} with HNGi7 ID ${HNG_id} using ${language} for stage 2 task email ${email}`);
