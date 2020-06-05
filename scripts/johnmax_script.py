46c24f3d58f1bef9bc46e879ea8cc5d513a31bde
johnmaxnew <forevermeticulous@gmail.com>
2020-06-05 07:37:36 +0300

    feat: added a script

--- scripts/johnmax_script.py
+++
@@ -1,7 +0,0 @@
-name = 'John Max'
-id_ = 'HNG-06291'
-email = 'forevermeticulous@gmail.com'
-language = 'Python'
-output = 'Hello World, this is '+ name + ' with HNGi7 Program ID ' + id_ + ' using ' + language +' for stage 2 task'
-
-print(output)
