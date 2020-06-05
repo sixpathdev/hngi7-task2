d382c142511c80027e7eb838ccc3deeceb89f7a9
Herbert <herbert@debmower_adj>
2020-06-05 15:43:55 +0300

    updating my file

--- scripts/herbert_script.py
+++ scripts/herbert_script.py
@@ -2,4 +2,4 @@ name = 'Herbert Tsabotosambinony'
 id_ = 'HNG-01636'
 email = 'herbert.tsabotosambinony@aims.ac.rw'
 language = 'Python'
-print('Hello World, this is '+ name + ' with HNGi7 ID ' + id_ + ' using ' + language +' for stage 2 task')
+print('Hello World, this is '+ name + ' with HNGi7 ID ' + id_ + ' using ' + language +' for stage 2 task email '+ email)
