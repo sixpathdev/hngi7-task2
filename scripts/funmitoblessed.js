a0af5c723b2b10bbacacf25ed21fed247834baa6
Oluwafunmito Blessed <contact@funmitoblessed.com>
2020-06-05 01:46:29 +0100

    commit script file for funmitoblessed

---
+++ scripts/funmitoblessed.js
@@ -0,0 +1,11 @@
+// HNGi7 Stage 1 Task - Team flash
+
+let intern = {
+    name: 'Olúwafúnmitọ́ Blessed Ọdẹ́fẹ́mi',
+    email: 'funmito.odefemi@outlook.com',
+    id: 'HNG-04314',
+    lang: 'JavaScript'
+}
+
+// Output 
+console.log(`Hello World, this is ${intern.name} with HNGi7 ID ${intern.id} using ${intern.lang} for stage 2 task email ${intern.email}`)
