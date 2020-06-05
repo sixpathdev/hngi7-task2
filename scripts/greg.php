291c3a386d445418528b76845f708232a641d685
Nwokoye Chigozie <gregoflash05@users.noreply.github.com>
2020-06-05 08:51:13 +0100

    Updated greg.php
    Please, I just updated my code to the readme format

--- scripts/greg.php
+++ scripts/greg.php
@@ -5,7 +5,7 @@ $id='HNG-02811';
 $programming_language= 'PHP';
 
 
-echo "Hello World, this is $name with HNGi7 ID $id using $programming_language for stage 2 task";
+echo "Hello World, this is $name with HNGi7 ID $id using $programming_language for stage 2 task email $email.";
 
 
 ?>
