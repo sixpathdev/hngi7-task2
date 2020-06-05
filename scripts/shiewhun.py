48cbcd799202ef96cb600c080b228d26b4155261
shiewhun2 <66360435+shiewhun2@users.noreply.github.com>
2020-06-05 08:15:15 +0100

    used new format

--- scripts/shiewhun.py
+++ scripts/shiewhun.py
@@ -2,4 +2,4 @@ email = "shiewhunnues@gmail.com"
 name = "Seun Arowojobe"
 hng_id = "HNG-04754"
 language = "python"
-print("Hello World, this is " + name + " with HNGi7 ID " + hng_id + " using " + language + " for stage 2 task.")
+print("Hello World, this is " + name + " with HNGi7 ID " + hng_id + " using " + language + " for stage 2 task" + " email " + email)
