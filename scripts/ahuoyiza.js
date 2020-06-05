e4fbe87d1aee22acca7f933507240aebaec30e2d
Ahuoyiza <60071500+Ahuoyiza@users.noreply.github.com>
2020-06-05 10:38:26 +0100

    script upload

---
+++ scripts/ahuoyiza.js
@@ -0,0 +1,7 @@
+const eMail = "sophieoyiza@gmail.com";
+const myFullName = "Sophia Ahuoyiza Abubakar";
+const myHngId = "HNG-00354";
+const language = "JavaScript";
+
+console.log (`Hello World, this is ${myFullName} with HNGi7 ID ${myHngId}
+ using ${language} for stage 2 task email ${eMail}`);
