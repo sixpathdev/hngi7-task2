42230009a1eb4208062ed46775e9893dcbcc2216
= <=>
2020-06-05 22:24:28 +0100

    Lord gerald file

---
+++ scripts/lordgerald.js
@@ -0,0 +1,8 @@
+const name = "Gerald Kachi";
+const hng_id = "HNG-03447";
+const email = "Fitzgeraldkachi@gmail.com";
+const lang = "JavaScript";
+Task = () => {
+ console.log(`Hello World, this is ${name} with HNGi7 ID ${hng_id} using ${lang} for stage 2 task email ${email}`);
+}
+Task();
