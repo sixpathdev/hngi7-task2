18198dd947756664a72cc53ccc3e056c92f8601f
Bongi <66295440+Bongi-buzzyb@users.noreply.github.com>
2020-06-05 13:48:27 +0200

    Create hng01263.js

---
+++ scripts/hng01263.js
@@ -0,0 +1,12 @@
+const ME = {
+	name: 'Bongi Mkhonza',
+	ID: 'HNG-01263',
+	email: 'bongi.buzzyb@outlook.com',
+	language: 'Javascript'
+};
+
+const Text = ()=>
+{
+	return `Hello World, this is ${ME.name} with HNGi7 ID ${ME.id} using ${ME.language} for stage 2 task ${ME.email}`;
+}
+console.log(Text());
