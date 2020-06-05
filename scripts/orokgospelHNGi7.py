529aa794a3434899763bb34939ddba2076a3f6cf
Gospel Theodore Orok <51316483+orokgospel@users.noreply.github.com>
2020-06-05 01:20:25 +0100

    Added my task 2 script with a changed format

--- scripts/orokgospelHNGi7.py
+++ scripts/orokgospelHNGi7.py
@@ -1,5 +1,6 @@
+#Hello World, this is [full name] with HNGi7 ID [ID] using [language] for stage 2 task"
 my_name= "Gospel Orok"
-language_used= "python"
-HNG_ID="HND-02122"
+language_used= "python 3"
+HNG_id="HND-02122"
 
-print("Hello world this is", my_name ,"with HNGi7 ID:", HNG_ID, "using ", language_used, "for Stage2 Task",flush=True)
+print("Hello World this is", my_name ,"with HNGi7 ID", HNG_id, "using", language_used, "for Stage 2 task",flush=True)
