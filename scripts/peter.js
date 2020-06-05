7e7795ec6183a8d3d15b953df20414bc70d8acb2
petrepan <peterawotola@gmail.com>
2020-06-05 16:56:25 +0100

    profile added

--- scripts/peter.js
+++ scripts/peter.js
@@ -4,7 +4,7 @@ const hngID = "HNG-04950";
 const email = "peterawotola@gmail.com";
 
 const myProfile = () => {
-    return `Hello World, this is ${name} with HNGi7 ${hngID} using ${language} for stage 2 task email ${email}.`;
+    return `Hello World, this is ${name} with HNGi7 ${hngID} using ${language} for stage 2 task email ${email}`;
 }
 
 console.log(myProfile());
