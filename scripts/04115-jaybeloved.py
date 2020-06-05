5fec5ee3abfd01371df3c25319a850699f207280
JayBeloved <63831361+JayBeloved@users.noreply.github.com>
2020-06-05 20:28:42 +0100

    last commit for jaybeloved team flash

---
+++ scripts/04115-jaybeloved.py
@@ -0,0 +1,9 @@
+
+name = 'Lawal John'
+hng_id = 'HNG-04115'
+email = 'info.jaybeloved@gmail.com'
+language = 'python'
+
+output ="Hello World, this is " + name + " with HNGi7 ID " + hng_id + " using " + language + " for stage 2 task " + email + ""
+
+print(output)
