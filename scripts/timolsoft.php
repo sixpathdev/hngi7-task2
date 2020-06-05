d20c4f460388e8beccd7e6aced7677ebcb17fe64
timolsoft <timolcode@gmail.com>
2020-06-05 16:21:48 +0100

    my script

--- scripts/timolsoft.php
+++ scripts/timolsoft.php
@@ -5,7 +5,6 @@ $id = "HNG-06711";
 $email = "timolcode@gmail.com";
 $lang = "PHP";
 
-
 echo "Hello World,This is   " . $name . " with HNGi7 ID  " . $id . "  using ". $lang . "  for stage 2 task  email:   " . $email  
 
 ?>
