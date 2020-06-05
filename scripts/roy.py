75b389b8dd14b02e4074479e86da184bf91abf26
Amolo Chukwudumebi  Joseph <65375943+Roymaniac@users.noreply.github.com>
2020-06-05 14:08:43 +0100

    Update roy.py

--- scripts/roy.py
+++ scripts/roy.py
@@ -3,4 +3,4 @@ ID = 'HNG-04793'
 Language = 'Python'
 Email = 'amolojoseph2020@gmail.com'
 
-return('Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task email {}'.format(Name,ID,Language,Email))
+return("Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task email {}".format(Name,ID,Language,Email))
