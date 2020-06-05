1b15d17ae3f11fbd580a6e61f14264e5b957a9ef
Alex Favour <52333077+Alinko0@users.noreply.github.com>
2020-06-05 02:01:37 -0700

    updated

--- scripts/Alex script.js
+++ scripts/Alex script.js
@@ -4,6 +4,6 @@ let hngid= "HNG-02158";
 let language="javascript"
 
 function greet(){
-return`Hello world this is ${name} with Hng id ${hngid} using ${language}  for the stage 2 task email ${email}`;  
+return`Hello world,this is ${name} with HNGi7 ID ${hngid} using ${language} for stage 2 task email ${email}`;  
 }
 console.log(greet());
