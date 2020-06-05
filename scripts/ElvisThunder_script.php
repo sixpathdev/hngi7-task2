4e963f353984df0d894ebf77fd9142b731b5348a
ElvisThunder <elvislogan1@gmail.com>
2020-06-04 21:48:30 -0700

    Followed the format of the group by making email to be on the first line

--- scripts/ElvisThunder_script.php
+++ scripts/ElvisThunder_script.php
@@ -1,8 +1,8 @@
 <?php
     //Declare Variables first
+    $email = "elvislogan1@gmail.com";
     $name = "Elvis Udaigwe";
     $hng_id = "HNG-04955";
-    $email = "elvislogan1@gmail.com";
     $language = "PHP";
     //Then output the result
     echo "Hello World, this is $name with HNGi7 ID $hng_id using $language for stage 2 task";
