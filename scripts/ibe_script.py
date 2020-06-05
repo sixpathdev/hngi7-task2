2e686ad23fc8d5497449b624fef39d1cc20d0409
daniel egeonu <degeonu23@gmail.com>
2020-06-05 07:35:45 +0100

    the real one

--- scripts/ibe_script.py
+++
@@ -1,7 +0,0 @@
-user_name = "egeonu daniel"
-user_id = "HNG-00274"
-user_email = "degeonu23@gmail.com"
-user_language = "python"
-
-output = f"Hello World, this is {user_name} with HNGi7 ID {user_id} and {user_email} using {user_language} for stage 2 task"
-print(output)
