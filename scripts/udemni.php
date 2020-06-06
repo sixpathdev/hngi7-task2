31f5adfc02c6a801e04f7844790abda3f73b6f6e
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2020-06-06 19:35:38 +0100

    Update udemni.php

--- scripts/udemni.php
+++ scripts/udemni.php
@@ -3,7 +3,8 @@ $_email ="justinobarogie@gmail.com";
 $_full_name = "Obarogie Osas";
 $_id = 'HNG-05777';
 $_language_used = "PHP";
-echo $_output =" Hello World, this is  $_full_name  with HNGi7 ID $_id using $_language_used for stage 2 task email $_email ";
+$_output = "Hello World, this is " . $_full_name . " with HNGi7 ID " . $_id . " using " . $_language_used . " for stage 2 task email " . $_email;
+echo $_output
 ?>
 
 
