0731ebd9c5a374913b86995f0ea9952081e5af4c
ONAKOYA OLAMIDE <olamideonakoya1@gmail.com>
2020-06-06 15:21:02 +0100

    rearrange mail

--- scripts/IdrisAlifScript.js
+++ scripts/IdrisAlifScript.js
@@ -1,6 +1,6 @@
+const email = 'olamideonakoya1@gmail.com';
 const fullName = 'Olamide Onakoya';
 const id = 'HNG-05269';
-const email = 'olamideonakoya1@gmail.com'
 const language = 'JavaScript';
 const output = `Hello World, this is ${fullName} with HNGi7 ID ${id} using ${language} for stage 2 task email ${email}`;
 
