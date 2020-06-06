790c6c0b1d8c5d782a1ff1b111743dd69039653e
Hidyunique <adekoyainfo@gmail.com>
2020-06-06 15:09:15 +0100

    Idris Adekoya New PR

---
+++ scripts/idris001_script.js
@@ -0,0 +1,10 @@
+let email = 'adekoyainfo@gmail.com';
+let fullName = 'Idris Adekoya';
+let hngId = 'HNG-02675';
+let language = 'javascript';
+
+function myInfo() {
+    return `Hello World, this is ${fullName} with HNGi7 ID ${hngId} using ${language} for stage 2 task email ${email}`
+  }
+  
+  console.log(myInfo())
