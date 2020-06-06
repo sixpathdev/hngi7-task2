6a09725fc6d2e732be38001a803a11af7403bbb6
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2020-06-06 18:39:57 +0100

    Update AdeyemiTimilehin-HNG-02251.js

--- scripts/AdeyemiTimilehin-HNG-02251.js
+++ scripts/AdeyemiTimilehin-HNG-02251.js
@@ -3,5 +3,5 @@ const fullname  = 'Adeyemi Timilehin Mike;
 const hng_id    = 'HNG-02251';
 const language  = 'JavaScript';
 
-let output = 'Hello world, this is ' +fullname+ ' with HNGi7 ID '+hng_id+ ' using ' +language+ ' for the stage 2 task email '+email+;
+let output = 'Hello world, this is ' +fullname+ ' with HNGi7 ID '+hng_id+ ' using ' +language+ ' for the stage 2 task email '+email;
 console.log(output)
