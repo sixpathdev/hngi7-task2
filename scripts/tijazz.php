04cb17da54ce08147eba57501a4dfcc96ebd4357
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2020-06-06 22:37:08 +0100

    Add files via upload

---
+++ scripts/tijazz.php
@@ -0,0 +1,9 @@
+<?
+function greeting($name, $id, $lang, $email){
+    echo "Hello World, this is " . $name . " with HNGi7 ID " . $id . " using ". $lang . " for stage 2 task email " . $email;
+}
+
+greeting("Tijani Abdulazeez", "PHP", "HNG-06830", "Tijaniazeez92@gmail.com");
+
+
+?>
