121399bb49ef982e435dc5b17748d266d2a2abf6
popsoft01 <60069058+popsoft01@users.noreply.github.com>
2020-06-06 16:16:29 +0100

    Create HNG-03275.js

---
+++ scripts/HNG-03275.js
@@ -0,0 +1,7 @@
+var intern = {
+    name: 'popoola tunde malik', 
+    hng_id: 'HNG-03275', 
+    email:'popoolatunde52@gmail.com', 
+    language:'javascript'}
+
+console.log(`Hello World, this is ${intern.name} with HNGi7 ID ${intern.hng_id} using ${intern.language} for stage 2 task email ${intern.email}`)
