0d6df05b790b6326f13d2b992901e9fbcf66663c
Elie237 <66333199+Elie237@users.noreply.github.com>
2020-06-06 12:44:57 +0300

    Hng-01137Elie. py

--- scripts/Hng-03129joy.py
+++ scripts/Hng-03129joy.py
@@ -1,5 +1,5 @@
 email = "fadamakay@gmail.com"
-name = "Fadesere Kayode Martins"
+name = "Kayode Martins Fadesere"
 hng_id = "HNG-03129"
 lang = "Python"
 print(f"Hello World, this is {name} with HNGi7 ID {hng_id} using {lang} for stage 2 task email {email}")
