90ed00057c3a4220348a0de9bcd1fc37b25625d9
Balogun Adebisi <balosure@gmail.com>
2020-06-06 16:58:55 +0100

    Create Balogun_Adebisi.php

---
+++ scripts/Balogun_Adebisi.php
@@ -0,0 +1,9 @@
+<?php
+$email = "balosure@gmail.com";
+$name = "Balogun Adebisi";
+$id = "01438";
+$language = "PHP";
+
+
+echo "Hello World, this is $name with HNGi7 ID $id using $language for stage 2 task email $email.";
+?> 
