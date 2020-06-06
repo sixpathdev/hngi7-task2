dadde28bdcbb8e29cdfd8987f5dbe8c1c2df13ef
fitzgerald kachi <57835051+geraldkachi@users.noreply.github.com>
2020-06-06 09:06:39 +0100

    Update lordgerald.js

--- scripts/lordgerald.js
+++ scripts/lordgerald.js
@@ -1,6 +1,6 @@
 const name = "Gerald Kachi";
 const hng_id = "HNG-03447";
-const email = "Fitzgeraldkachi@gmail.com";
+const email = "Fitzgeraldkachi@yahoo.com";
 const lang = "JavaScript";
 Task = () => {
  console.log(`Hello World, this is ${name} with HNGi7 ID ${hng_id} using ${lang} for stage 2 task email ${email}`);
