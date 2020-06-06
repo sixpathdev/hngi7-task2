69ac3da787ea730504743cf8c26688bcc44c4ecb
William Mwendwa <50356076+mwendwa-will@users.noreply.github.com>
2020-06-06 12:31:08 +0300

    Add files via upload

---
+++ scripts/mwendwa_script.php
@@ -0,0 +1,15 @@
+<?php
+$name = "William Mwendwa";
+
+$id = " HNG-06573";
+
+$email = "williammwendwa@gmail.com";
+
+$language = "php";
+
+echo "Hello World, this is ".$name." with HNGi7 ID ".$id." using ".$language." for stage 2 task email ".$email."";
+
+
+
+
+?>
