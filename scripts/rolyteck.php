8159226b47dff3cd3ca1f88d3abc132c0b3fad97
Blessing Roland <30534732+Rolyteck@users.noreply.github.com>
2020-06-06 17:01:17 +0100

    Delete rolyteck.php

--- scripts/rolyteck.php
+++
@@ -1,8 +0,0 @@
-
- <?php
-$internmail ="Rolyconsult@gmail.com";
-$internname = "Roland Blessing";
-$internID = "HNG-02628";
-$language ="PHP";
-echo "Hello World, this is " .$internname . " with HNGi7 ID " .$internID . " using ".$language." for stage 2 task email ".$internmail;
-?>
