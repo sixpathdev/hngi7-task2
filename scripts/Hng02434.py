4744bde053268423c84335a3903214987b2e8c66
Sammylex01 <samueloyekoya12@gmail.com>
2020-06-06 09:13:31 +0100

    HNG-01920
    My script

--- scripts/Hng02434.py
+++ scripts/Hng02434.py
@@ -3,4 +3,4 @@ hng_id = "HNG-02434"
 email = "jessie.98ugowe@gmail.com"
 language = "python"
 
-print("Hello World, this is " + name + " with HNG ID " + hng_id + " using " + language + " for stage 2 task ")
+print("Hello World, this is " + name + " with HNG ID " + hng_id + " and " + email + " using " + language + " for stage 2 task ")
