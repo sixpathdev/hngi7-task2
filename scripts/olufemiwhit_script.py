be39a5053ad95cc45b1280a7e996d19f0604096d
Adeyinka M. Adefolurin <folurinyinka@gmail.com>
2020-06-06 14:24:07 +0100

    feat: added olufemiwhit.py

--- scripts/olufemiwhit_script.py
+++ scripts/olufemiwhit_script.py
@@ -3,4 +3,4 @@ ID = 'HNG-06258'
 language = 'Python'
 email = 'oluwafemiwhit@gmail.com'
 
-print('Hello World, this is',fullname,'with HNGi7 ID',ID,'using',language,'for stage 2 task',email)
+print('Hello World, this is',fullname,'with HNGi7 ID',ID,'using',language,'for stage 2 task email',email)
