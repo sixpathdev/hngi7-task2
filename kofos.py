a837dd82a7447f1f7980b0a8576d50c348abb5cc
kofoworola08 <66370394+kofoworola08@users.noreply.github.com>
2020-06-06 20:02:45 +0100

    Create kofos.py

---
+++ kofos.py
@@ -0,0 +1,5 @@
+email = "kofoworolashadare@gmail.com"
+full_name = "kofoworola shadare"
+hng_id = "HNG-06440"
+language = "python"
+print("Hello World, this is " + full_name + ", with HNGi7 ID " + hng_id + " using " + language + " for stage 2 task email " + email)
