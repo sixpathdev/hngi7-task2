d644593e37154dd4775bbb4029f77927b5527785
Haybee96 <abeebayinla@gmail.com>
2020-06-06 19:18:34 +0100

    update abeeb_script.py

--- scripts/abeeb_script.py
+++ scripts/abeeb_script.py
@@ -1,8 +1,8 @@
 
-my_name = 'Abeeb Ayinla'
+my_name = 'ABEEB AYINLA'
 my_id = 'HNG-05437'
-language = 'Python'
-email = 'abeebayinla@gmail.com'
+language = 'PYTHON'
+email = 'ABEEBAYINLA@GMAIL.COM'
 
 print('Hello World', ',', 'this is', my_name, 'with HNGi7 ID', my_id, 'using', language, 'for stage 2 task email', email)
 
