b745449208ed0198a659ad96c4126de1b0443ee4
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2020-06-06 18:38:36 +0100

    Rename Adeleke Isaiah.js to AdelekeIsaiah.js

---
+++ scripts/AdelekeIsaiah.js
@@ -0,0 +1,6 @@
+const email = "iadeleke01@gmail.com";
+const name = "Adeleke Isaiah";
+const id = "HNG-00931";
+const language = "Javascript";
+
+console.log("Hello World, this is " + name + " with HNGi7 ID " + id + " using " + language  + " for stage 2 task" + " email " + email)
