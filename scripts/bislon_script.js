ef754852ffe60bc0a8163e17e037a91c2cbc214c
Bislon Zulu <bislonzulu1@outlook.com>
2020-06-06 18:59:38 +0200

    Update bislon_script.js

--- scripts/bislon_script.js
+++ scripts/bislon_script.js
@@ -1,6 +1,9 @@
 const func = () => {
-  const [name, ID, email, language] = ['Bislon Zulu', 'HNG-03958', 'bislonzulu1@outlook.com', 'JavaScript'];
-  return `Hello World, this is ${name} with HNGi7 ID ${ID} using ${language} for stage 2 task`;
+  const email = "bislonzulu1@outlook.com";
+  const name = "Bislon Zulu";
+  const ID = "HNG-03958";
+  const language = "javaScript";
+  return `Hello World, this is ${name} with HNGi7 ID ${ID} using ${language} for stage 2 task email ${email}`;
 }
 
 console.log(func());
