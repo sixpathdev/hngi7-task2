0b07a53c872214f1bd5f4eba6e5435120ef0bf4f
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2020-06-06 23:43:32 +0100

    Add files via upload

---
+++ scripts/fissy.js
@@ -0,0 +1,15 @@
+const email = "samsonfisayo05@gmail.com";
+const name = "Fisayo Samson";
+const HNG_ID = "HNG-04020";
+const language = "JavaScript";
+
+console.log('Hello World, this is ' + name +' with HNGi7 ID ' + HNG_ID + ' using ' + language +' for Stage 2 task email ' + email);
+
+
+
+let email = "samsonfisayo05@gmail.com";
+let name = "Fisayo Samson";
+let id = "HNG-04020";
+let lang = "JavaScript";
+
+console.log("Hello World, this is " + name +" with HNGi7 ID " + id + " using " + lang +" for stage 2 task email " + email)
