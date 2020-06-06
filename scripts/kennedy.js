f34a50f7c0daba157165b91435d788cf4284761b
Kennedykp <46505884+Kennedykp@users.noreply.github.com>
2020-06-06 12:40:38 +0100

    Update kennedy.js
    Edited Js

--- scripts/kennedy.js
+++ scripts/kennedy.js
@@ -4,8 +4,8 @@ let id = 'HNG-05212';
 
 let email = 'kennedykc55@gmail.com';
 
-let language = 'javascript';
+let language = 'Javascript';
 
-const details = "Hello World this is " + name + " with HNGi7 ID " + id + " using " + language + " for stage 2 task email " + email ;
+const details = "Hello World, this is " + name + " with HNGi7 ID " + id + " using " + language + " for stage 2 task email " + email ;
 
 console.log(details);
