a0da76758cd4f200d0b72fa1c830420b4dda8a69
Bongi <66295440+Bongi-buzzyb@users.noreply.github.com>
2020-06-06 06:34:59 +0200

    hng-01263

---
+++ scripts/Bongi.js
@@ -0,0 +1,12 @@
+const Me = {
+    name: 'Bongi Mkhonza',
+    id: 'HNG-01263',
+    language: 'Javascript',
+    email: 'bongi.buzzyb@outlook.com'
+};
+
+const Text = ()=> 
+{
+    console.log(`Hello World, this is ${Me.name} with HNGi7 ID ${Me.id} using ${Me.language} for stage 2 task email ${Me.email}`)
+}
+Text();
