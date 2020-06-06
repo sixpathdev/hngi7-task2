2abc993c34ac21728bc5bb4f7aae5f5a5e35d3de
Dule Martins <dulemartins07@gmail.com>
2020-06-06 09:04:21 +0100

    recent update

--- dule_script.js
+++
@@ -1,6 +0,0 @@
-const name = "Tabah Baridule Martins"
-const id = "HNG-02033"
-const language = "Javascript"
-const email = "dulemartins07@gmail.com"
-
-console.log(`Hello world, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task email ${email}`)
