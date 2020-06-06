e9f7a7a833ac40847f37ae944d329cd9327738d5
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2020-06-06 17:19:57 +0100

    Update nadheefGit_script.js

--- scripts/nadheefGit_script.js
+++ scripts/nadheefGit_script.js
@@ -6,10 +6,10 @@ const intern={
 	id:"HNG-03415",
 	language: "javascript"
 	}
-console.log(intern)
+
 	const fullname=intern.firstName+" "+intern.lastName;
 	const id=intern.id;
 	const language=intern.language;
 	const email=intern.email; 
 
-console.log("Hello world!"+" "+ "This is"+" "+fullname+","+" "+"HNGi7 ID"+" "+id+" "+"using"+" "+language+" "+"for stage 2 task email:"+email+);
+console.log("Hello world,"+" "+ "This is,"+" "+fullname+","+" "+"HNGi7 ID"+" "+id+" "+"using"+" "+language+" "+"for stage 2 task email "+email);
