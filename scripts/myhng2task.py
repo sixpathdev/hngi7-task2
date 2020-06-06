fe96e3cdf8c9658cf830e107117f28de3372ecab
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2020-06-06 18:06:11 +0100

    Update myhng2task.py

--- scripts/myhng2task.py
+++ scripts/myhng2task.py
@@ -1,9 +1,6 @@
-Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> # hello-World-project
->>> # Hello World, this is (fullname with HNGi7ID [HNG-ID])
->>> full_name = "Ayeni Racheal"
->>> hng_id = "HNG-05163"
->>> language = "python"
->>> email = "rachealayenipelumi@gmail.com"
->>>  print ("Hello World, this is ", full_name, "with", "HNGi7 ID",hng_id, "using", language, "for the stage 2 task email ", email)
+
+full_name = "Ayeni Racheal"
+hng_id = "HNG-05163"
+language = "python"
+email = "rachealayenipelumi@gmail.com"
+print("Hello World, this is " + full_name + " with" + " HNGi7 ID " + hng_id + " using " + language + " for the stage 2 task email " + email)
