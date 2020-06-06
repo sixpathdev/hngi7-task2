de8637a47a16a83fc71be49a4764477b522fcd28
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2020-06-06 17:42:55 +0100

    Update larrybechtel.js

--- scripts/larrybechtel.js
+++ scripts/larrybechtel.js
@@ -1,8 +1,6 @@
-    var name = "Ibrahim Ajala";
-    var ID = "HNG-05288";
-    var email = "larrybechtel396@gmail.com"
-    var lang = "Javascript";
+var name = "Ibrahim Ajala";
+var id = "HNG-05288";
+var email = "larrybechtel396@gmail.com";
+var lang = "Javascript";
     
-
-document.write("Hello World, this is " +  name + " with HNGi7 ID " + ID    + " using " + lang    + " for stage 2 task email "+email);
-console.log("Hello World, this is " +  name + " with HNGi7 ID " + ID    + " using " + lang    + " for stage 2 task email "+email);
+console.log("Hello World, this is " +  name + " with HNGi7 ID " + id    + " using " + lang + " for stage 2 task email "+email);
