784ac5c668fddedcf7c8d6c3b6ed0f2f905c2fa1
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2020-06-06 15:50:05 +0100

    Update victoryNwokocha.js

--- scripts/victoryNwokocha.js
+++ scripts/victoryNwokocha.js
@@ -1,12 +1,12 @@
 let userInfo = () =>{
     const details ={
         Name : 'Victory Nwokocha',
-        hng_Id: 'HNG-05224',
+        hng_id: 'HNG-05224',
         email_Address: 'nwokochavictory47@gmail.com',
         language: 'Javascript',
     }
 
-    return 'Hello World, this is '  + details.Name  + ' with HNGi7 ID ' + details.hng_ID  + ' using ' + details.language + ' for stage 2 task email ' + details.email_Address;
+    return 'Hello World, this is '  + details.Name  + ' with HNGi7 ID ' + details.hng_id  + ' using ' + details.language + ' for stage 2 task email ' + details.email_Address;
 }
 
 console.log(userInfo());
