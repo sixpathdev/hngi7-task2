9e3db462585c61d03c098f0a9969fda3d34b95e4
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2020-06-06 22:49:51 +0100

    Delete sixtus.js

--- scripts/sixtus.js
+++
@@ -1,5 +0,0 @@
-var email = "chinonyesixtus@gmail.com";
-var name = " Chinonye Sixtus";
-var aidee = " HNG-05069";
-var skript = " Javascript";
-console.log("Hello World, this is " + name + " with HNGi7 ID " + aidee + " using " + skript + " for stage 2 task email " + email);
