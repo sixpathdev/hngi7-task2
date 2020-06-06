23320d96eaea2dc1b5ffac70ab736a3286973692
izik4004 <49079739+izik4004@users.noreply.github.com>
2020-06-06 08:31:58 +0100

    Update Isaac.js

--- scripts/Isaac.js
+++ scripts/Isaac.js
@@ -1,7 +1,7 @@
 let name= "Isaac John";
 let email="izik4004@gmail.com";
 let hngid= "HNG-04381";
-let language="javascript"
+let language="javascript";
 
 function greet(){
 return`Hello World, this is ${name} with HNGi7 ID ${hngid} using ${language} for stage 2 task email ${email}`;  
