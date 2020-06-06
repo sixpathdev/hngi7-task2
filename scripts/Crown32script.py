4bcaff7bb814ad29583e3a0b752a42e1f56559a8
David Asem <40046427+dyasem@users.noreply.github.com>
2020-06-06 23:05:23 +0000

    just helping a brother

--- scripts/Crown32script.py
+++ scripts/Crown32script.py
@@ -2,4 +2,4 @@ email = "oladunni32@gmail.com"
 name = "Oladunni Sunday Chris"
 hngi7_num = "HNG-01152"
 lan ="Python"
-print ("Hello World, this is " +  name + " with HNGi7 ID"  + hngi7_num +" using " +lan+ " for stage 2 task Email " +email)
+print ("Hello World, this is " +  name + " with HNGi7 ID"  + hngi7_num +" using " +lan+ " for stage 2 task")
