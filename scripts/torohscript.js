cce6e6bcb99508f399ac925bf9003a1455ad7d0d
etojama <torohkiss@gmail.com>
2020-06-06 09:38:07 +0100

    feat: new script

---
+++ scripts/torohscript.js
@@ -0,0 +1,4 @@
+const email = "torohkiss@gmail.com"
+const name = "Etoro Umoren"
+const id = "HNG-01884"
+console.log("Hello World, this is" + name + "with HNGi7 ID" + id + "using javascript for stage 2 task")
