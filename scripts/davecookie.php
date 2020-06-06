d7f8cd27645470e245109cd96afc689d7d14466d
olasode-david <60571573+olasode-david@users.noreply.github.com>
2020-06-06 18:01:07 +0100

    Create davecookie.php

---
+++ scripts/davecookie.php
@@ -0,0 +1,11 @@
+<?php
+
+function greetings_hng($name, $email, $hnd_id, $language)
+   {
+      data = "Hello World, this is " .$name." with HNGi7 ID ".$hnd_id." using ".$language." for stage 2 task email ". $email ;
+          return data;
+   }
+   
+   echo greetings_hng("Olasode David","PHP","HNG-03287","olasodedavid@gmail.com");
+   
+?>
