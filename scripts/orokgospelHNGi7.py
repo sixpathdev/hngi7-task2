1ba8ab3c86fa1e40e64f20ff2f46dd2a9d1c82ad
Gospel Theodore Orok <51316483+orokgospel@users.noreply.github.com>
2020-06-06 16:23:02 +0100

    Update orokgospelHNGi7.py

--- scripts/orokgospelHNGi7.py
+++ scripts/orokgospelHNGi7.py
@@ -2,5 +2,5 @@
 my_email="orokgospel@gmail.com"
 my_name="Gospel Orok"
 language_used="Python"
-HNG_id="HNG-02122"
-print("Hello World, this is "+my_name+" with HNGi7 ID "+HNG_id+" using "+language_used+" for Stage 2 task email "+my_email)
+hng_id="HNG-02122"
+print("Hello World, this is "+my_name+" with HNGi7 ID "+hng_id+" using "+language_used+" for stage 2 task email "+my_email)
