26b22421f0ce0b229443ba1dc26231eb258b655d
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2020-06-07 00:27:14 +0100

    Add files via upload

---
+++ hasbiy_script.js
@@ -0,0 +1,13 @@
+console.log()
+
+const name = 'hasbiy';
+
+const id = 'HNG-01932';
+
+const email = 'thasbiyallah@gmail.com';
+
+const language = 'JavaScript';
+
+console.log("Hello World, this is " + name + " with HNGi7 ID " + id +
+
+" using " + language  + " for stage 2 task email "  + email)
