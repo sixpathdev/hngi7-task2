7092f96db616d3f68a0a99aa24326f8039dcfff9
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2020-06-06 23:48:08 +0100

    Update fissy.js

--- scripts/fissy.js
+++ scripts/fissy.js
@@ -1,12 +1,3 @@
-const email = "samsonfisayo05@gmail.com";
-const name = "Fisayo Samson";
-const HNG_ID = "HNG-04020";
-const language = "JavaScript";
-
-console.log('Hello World, this is ' + name +' with HNGi7 ID ' + HNG_ID + ' using ' + language +' for Stage 2 task email ' + email);
-
-
-
 let email = "samsonfisayo05@gmail.com";
 let name = "Fisayo Samson";
 let id = "HNG-04020";
