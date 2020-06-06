8d8f91ae8b76e2be3c8f7cf6ba252ffb02d6b5f5
kelechi-oguh <kelechi.oguh505@gmail.com>
2020-06-06 17:05:51 +0100

    updated kaycee_script.py

--- scripts/kaycee_script.py
+++ scripts/kaycee_script.py
@@ -1,10 +1,7 @@
-def myOutput():
-    fullname = "Oguh Kelechi Henry"
-    _ID = "HNG-03565"
-    email = "Kelechi.oguh505@gmail.com"
-    language = "Python"
-    
-    output = "Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task".format(fullname, _ID, language)
-    return output
+email = "Kelechi.oguh505@gmail.com"
+fullname = "Oguh Kelechi Henry"
+my_id = "HNG-03565"
+language = "Python"
 
-print(myOutput())
+output = "Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task email {}".format(fullname, my_id, language, email)
+print(output)
