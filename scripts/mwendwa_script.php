02c494a19380c90eaf186ed857d760be2374d33b
William Mwendwa <50356076+mwendwa-will@users.noreply.github.com>
2020-06-06 13:17:03 +0300

    adding email

--- scripts/mwendwa_script.php
+++ scripts/mwendwa_script.php
@@ -7,7 +7,7 @@ $email = "williammwendwa@gmail.com";
 
 $language = "php";
 
-echo "Hello World, this is ".$name." with HNGi7 ID ".$id." using ".$language." for stage 2 task ";
+echo "Hello World, this is ".$name." with HNGi7 ID ".$id." using ".$language." for stage 2 task email ".$email."  ";
 
 
 
