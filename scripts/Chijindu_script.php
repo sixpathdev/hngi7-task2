7b2ac5fcd7bf0086917e83263a27ccb852433d4a
mavisvictor <66245780+mavisvictor@users.noreply.github.com>
2020-06-06 03:53:16 +0100

    Update and renamed in Old repo
    Server keep showing old code I can't even find ...local man is just tired

--- scripts/Chijindu_script.php
+++ scripts/Chijindu_script.php
@@ -1,7 +1,7 @@
 <?php 
 
  $email = "mavisvictor@gmail.com";
-  $name = "Chijindu Mavis";
+  $name = "Okonkwo Chijindu";
   $hng_id = "HNG-03664";
   $language = "php";
 echo "Hello World, this is $name with HNGi7 ID $hng_id using $language for stage 2 task email $email";
