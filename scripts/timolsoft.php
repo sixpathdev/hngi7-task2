d45a9c649affbb4643a33e636fb86372b8b68a15
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2020-06-06 15:58:10 +0100

    Delete timolsoft.php
    very wrong retry

--- scripts/timolsoft.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$name = "Timothy Olaniyi";
-$id = "HNG-06711"; 
-$lang = "PHP";
-
-echo "Hello World,This is   " . $name . " with HNGi7 ID  " . $id . "  using ". $lang . "  for stage 2 task 
-
-?>
