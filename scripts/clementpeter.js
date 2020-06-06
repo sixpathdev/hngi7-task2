789683af79e90c2fe7dec2c9a0774d38da73fee6
ClementPeter <ClementPeter@users.noreply.github.com>
2020-06-06 15:17:58 +0100

    clementpeter.js

--- scripts/clementpeter.js
+++ scripts/clementpeter.js
@@ -4,5 +4,5 @@ const email = "clementpeter2000@gmail.com";
 const language = "Javascript";
 
 console.log(
-    `Hello World, this is ${name} with HNGi7 ID ${hngId} using ${language} for stage 2 task email ${email}.`
+    `Hello World, this is ${name} with HNGi7 ID ${hngId} using ${language} for stage 2 task email ${email}`
 );
