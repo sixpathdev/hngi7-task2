5bfaa61146871b69735a7ae62c6a7a642da42b0f
Blessing M. Babilah <mbamah1999@gmail.com>
2020-06-06 08:17:47 +0000

    removed full stop

--- scripts/blessing-babilah.js
+++ scripts/blessing-babilah.js
@@ -3,6 +3,6 @@ const mail = 'mbamah1999@gmail.com';
 const fullname  = 'Blessing Babilah';
 const id = 'HNG-00958';
 const pLanguage = 'Javascript';
-const message = `Hello World, this is ${fullname} with HNGi7 ID ${id} using ${pLanguage} for stage 2 task email ${mail}.`
+const message = `Hello World, this is ${fullname} with HNGi7 ID ${id} using ${pLanguage} for stage 2 task email ${mail}`
 console.log(message);
 
