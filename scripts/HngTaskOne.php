a0d5e0bfc61a83eef0bbdf1616ef34d831a849e1
ThaDeveloper <41994892+Samson8@users.noreply.github.com>
2020-06-06 15:08:15 +0100

    Task updated

--- scripts/HngTaskOne.php
+++ scripts/HngTaskOne.php
@@ -5,5 +5,5 @@
         $status="Pass";
         $email="samsondamilola22@gmail.com";
 
-echo "Hello World this is ".$fullname . " with HNGi7 ID ". $hng_id . " using ". $language . " for stage 2 task email". $email. "<br>";
+echo "Hello World, this is ".$fullname . " with HNGi7 ID ". $hng_id . " using ". $language . " for stage 2 task email ". $email. "."."<br>";
 ?>
