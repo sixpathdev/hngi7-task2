bf759e55b3440c3ec18cd98f4fb0dc7bb6ab3ded
Kareem Olayinka <kareem.bincom@gmail.com>
2020-06-06 21:57:29 +0100

    task2 commit

---
+++ scripts/r_olayinka.php
@@ -0,0 +1,7 @@
+<?php
+$email = "olayinka.raheem16@gmail.com";
+$name = "Kareem Raheem";
+$hngid = "HNG-05431";
+$lang = "PHP";
+echo "Hello World, this is " . $name . " with HNGi7 ID " . $hngid . " using " .$lang. " for stage 2 task email " . $email ;
+?>
