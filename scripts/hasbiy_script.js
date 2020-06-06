a31cb011de757358730167668e940506e8257869
Hasbiy <58561237+Hasbiy@users.noreply.github.com>
2020-06-06 12:21:01 +0100

    Add files via upload

--- scripts/hasbiy_script.js
+++ scripts/hasbiy_script.js
@@ -6,8 +6,8 @@ const id = 'HNG-01932';
 
 const email = 'thasbiyallah@gmail.com';
 
-const language = 'javascript';
+const language = 'JavaScript';
 
-console.log("Hello World, this is " + name + " with HNGi7 ID " + id + " and email " + email +
+console.log("Hello World, this is " + name + " with HNGi7 ID " + id +
 
-" using " + language  + " for stage 2 task")
+" using " + language  + " for stage 2 task email "  + email)
