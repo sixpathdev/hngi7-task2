70c09a5f5dc6bf6ba173569e417c735cfc7c2991
SamPizh <48074449+SamPizh@users.noreply.github.com>
2020-06-06 23:43:03 +0100

    Create Pizh2.js

---
+++ scripts/Pizh2.js
@@ -0,0 +1,6 @@
+const email = "samsonfisayo05@gmail.com";
+const name = "Fisayo Samson";
+const HNG_ID = "HNG-04020";
+const language = "Javascript";
+
+console.log ('Hello World, this is ' + name +' with HNGi7 ID ' + HNG_ID + ' using ' + language + ' for stage 2 task email ' + email); 
