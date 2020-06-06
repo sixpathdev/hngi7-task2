445acee22630ae384fb1b1beb0c20fac7739fa90
Nimisha-Singh <45930712+Nimisha-Singh@users.noreply.github.com>
2020-06-06 20:20:37 +0530

    Add files via upload

---
+++ nimishasingh_script.py
@@ -0,0 +1,6 @@
+name = 'Nimisha Singh'
+HNG_ID = 'HNG-03372'
+email = 'singhnimisha1970@gmail.com'
+language = 'Python'
+
+print("Hello World, this is "+ name +" with HNGi7 ID " + HNG_ID + " using "+ language + " for stage 2 task email " + email)
