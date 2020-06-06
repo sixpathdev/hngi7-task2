86a07af978cdaa1bbb9a58441f9fc6638e699fb5
L-tech <35797372+L-tech@users.noreply.github.com>
2020-06-06 13:41:49 +0100

    Hng internship 7 task
    Task 2 submission

--- scripts/arome_dev_script.php
+++ scripts/arome_dev_script.php
@@ -1,11 +1,11 @@
 <?php
-function hgnInternship() {
+function hngInternship() {
     $email = "legendabrahamonoja@gmail.com";
-    $name = "Abraham Arome Onoja"; 
+    $name = "Abraham Onoja"; 
     $hngid = "HNG-01334"; 
-    $programming_lang = " PHP";
-    $output = "Hello world, this is $name with HNGi7 ID $hngid using $programming_lang for stage 2 task email $email";
+    $programming_lang = "PHP";
+    $output = "Hello World, this is $name with HNGi7 ID $hngid using $programming_lang for stage 2 task email $email";
     return $output;
 } 
-echo hgnInternship();
+echo hngInternship();
 ?>
