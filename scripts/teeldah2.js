acc087a305b6b65b8ad6b3a4fe96ff56f4686210
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2020-06-06 16:11:24 +0100

    Update teeldah2.js

--- scripts/teeldah2.js
+++ scripts/teeldah2.js
@@ -5,4 +5,4 @@ const language = "javascript",
 
       
 	
-console.log('Hello World, this is ${name} with HNGi7 ID ${hng_id} using ${language} for stage 2 task email ${email}');
+console.log(`Hello World, this is ${name} with HNGi7 ID ${hng_id} using ${language} for stage 2 task email ${email}`);
