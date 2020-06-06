350c1c42c08c6220ffc7830e5878a1e8563e83bf
Adewale <53665585+bravestLimit@users.noreply.github.com>
2020-06-07 00:23:41 +0100

    Create adewalesanusi.js
    My script.

---
+++ scripts/adewalesanusi.js
@@ -0,0 +1,6 @@
+const email = "adewaleabbey.as@gmail.com";
+const name = "Adewale sanusi";
+const hng_id = "HNG-05640";
+const language = "javascript";
+
+console.log(`Hello World, this is ${name} with HNGi7 ID ${hng_id} using ${language} for stage 2 task email ${email}`)
