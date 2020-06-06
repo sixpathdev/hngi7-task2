03a1ed14bcac085874fa691867b158672b6e96ce
Haybee96 <abeebayinla@gmail.com>
2020-06-06 06:42:15 +0100

    Modified abeeb_script.py

--- scripts/abeeb_script.py
+++ scripts/abeeb_script.py
@@ -1,8 +1,8 @@
-message = 'Hello World'
+
 my_name = 'Abeeb Ayinla'
 my_id = 'HNG-05437'
 language = 'Python'
 email = 'abeebayinla@gmail.com'
 
-print(message, ',', 'this is', my_name, 'with HNGi7 ID', my_id, 'using', language, 'for stage 2 task email', email)
+print('Hello World', ',', 'this is', my_name, 'with HNGi7 ID', my_id, 'using', language, 'for stage 2 task email', email)
 
