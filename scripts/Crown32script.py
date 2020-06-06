76ed0e31dfa327c7975e205b43ad735e62be67e1
David Asem <40046427+dyasem@users.noreply.github.com>
2020-06-06 23:25:25 +0000

    still helping

--- scripts/Crown32script.py
+++ scripts/Crown32script.py
@@ -1,5 +1,5 @@
 email = "oladunni32@gmail.com"
 name = "Oladunni Sunday Chris"
 hngi7_num = "HNG-01152"
-lan ="Python"
-print ("Hello World, this is " +  name + " with HNGi7 ID "  + hngi7_num +" using " +lan+ " for stage 2 task")
+lan = "Python"
+print(f'Hello World, this is {name} with HNGi7 ID {hngi7_num} using {lan} for stage 2 task email {email}')
