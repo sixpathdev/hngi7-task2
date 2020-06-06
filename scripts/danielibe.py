e85626b2d79755937c1e9f93bca089725359822a
daniel egeonu <degeonu23@gmail.com>
2020-06-06 10:46:31 +0100

    help last

---
+++ scripts/danielibe.py
@@ -0,0 +1,7 @@
+user_name = 'Egeonu daniel'
+hng_id = 'HNG-03145'
+user_email = 'degeonu23@gmail.com'
+user_language = 'python'
+
+output = 'Hello World, this is {user_name} with HNGi7 ID {hng_id} using {user_language} for stage 2 task degeonu23@gmail.com'
+print(output
