7ae6e9e2ad07f790c6b10c8482f1d2c4c5b77328
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2020-06-06 22:41:28 +0100

    Update tijazz.php

--- scripts/tijazz.php
+++ scripts/tijazz.php
@@ -3,7 +3,7 @@ function greeting($name, $id, $lang, $email){
     echo "Hello World, this is " . $name . " with HNGi7 ID " . $id . " using ". $lang . " for stage 2 task email " . $email;
 }
 
-greeting("Tijani Abdulazeez", "PHP", "HNG-06830", "Tijaniazeez92@gmail.com");
+greeting("Tijani Abdulazeez", "HNG-06830", "PHP", "Tijaniazeez92@gmail.com");
 
 
 ?>
