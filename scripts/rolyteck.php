349144f0f6ffc194f2617704edb9c1ade0ff9a12
Blessing Roland <30534732+Rolyteck@users.noreply.github.com>
2020-06-06 16:18:12 +0100

    Stage 2 task

---
+++ scripts/rolyteck.php
@@ -0,0 +1,8 @@
+
+ <?php
+$internmail ="Rolyconsult@gmail.com";
+$internname = "Roland Blessing";
+$internID = "HNG-02628";
+$language ="PHP";
+echo "Hello World, this is " .$internname . " with HNGi7 ID " .$internID . " using ".$language." for stage 2 task email ".$internmail;
+?>
