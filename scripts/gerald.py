5782663051f1bd301ee3a8abf5534bc9e132f556
fitzgerald kachi <57835051+geraldkachi@users.noreply.github.com>
2020-06-06 09:18:18 +0100

    Update gerald.py

--- scripts/gerald.py
+++ scripts/gerald.py
@@ -1,6 +1,6 @@
 name = 'Gerald Kachi'
 hng_id = 'HNG-03447'
-email = 'Fitzgeraldkachi@gmail.com'
+email = 'Fitzgeraldkachi@yahoo.com'
 language = 'Python'
 output = 'Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task email %s' %(name,hng_id,language,email)
 
