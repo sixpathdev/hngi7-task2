209f4d65e16e0d9a50d18ec6042da6bac1726cd2
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2020-06-06 17:57:59 +0100

    Update jaybeloved-04115_script.js

--- scripts/jaybeloved-04115_script.js
+++ scripts/jaybeloved-04115_script.js
@@ -4,4 +4,4 @@ let id = "HNG-04115";
 let langugeUsed = "JavaScript";
 let email = "info.jaybeloved@gmail.com"
 //Output
-console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${languageUsed} for stage 2 task email ${email}`);
+console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${langugeUsed} for stage 2 task email ${email}`);
