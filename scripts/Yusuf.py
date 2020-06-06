aa5a6be9c61b10ef63550498e8ae22bd026b8b0a
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2020-06-06 18:28:57 +0100

    Update Yusuf.py

--- scripts/Yusuf.py
+++ scripts/Yusuf.py
@@ -3,4 +3,4 @@ Myname= 'Yusuf Sodiq'
 MyID= 'HNG-02857'
 Lang= 'python'
 
-Print('Hello World, this is '+Myname+' with HNGi7 ID '+MyID+' using '+Lang+' for stage 2 task email '+Email+'')
+Print('Hello World, this is '+Myname+' with HNGi7 ID '+MyID+' using '+Lang+' for stage 2 task email '+Email)
