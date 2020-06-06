5b4cd056cce918b416b6929aa1add46aa60167fc
#73Qx <unonoq@gmail.com>
2020-06-06 14:08:47 +0100

    feat: added Sochima_task2script.php

--- scripts/Sochima_task2script.php
+++ scripts/Sochima_task2script.php
@@ -1,9 +1,9 @@
 <?php 
 
 	$email = "unonoq@gmail.com";
-	$name = "Sochima Obele James";
+	$name = "Sochima Obele";
 	$hng_id = "HNG-06638";
-	$language = "php";
+	$language = "PHP";
 
 		echo "Hello World, this is $name with HNGi7 ID $hng_id using $language for stage 2 task email $email";
 
