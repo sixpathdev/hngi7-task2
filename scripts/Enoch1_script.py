118cee139e08739c650ebddbefea4ed8c2e35627
Enoch01 <66218372+Enoch01@users.noreply.github.com>
2020-06-06 12:18:56 +0100

    Update Enoch1_script.py

--- scripts/Enoch1_script.py
+++ scripts/Enoch1_script.py
@@ -3,3 +3,4 @@ id = "HNG-00991";
 email = "enochowoeye001@gmail.com";
 Language= "Python"
 print('Hello World, this is '+name+' with HNGi7 ID '+id+' using '+Language+' for stage 2 task email '+email)
+ 
