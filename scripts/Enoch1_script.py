a14e01c8cdc02095d24270eae7415babb1d13b5b
Enoch01 <66218372+Enoch01@users.noreply.github.com>
2020-06-06 12:30:14 +0100

    Delete Enoch1_script.py

--- scripts/Enoch1_script.py
+++
@@ -1,6 +0,0 @@
-name = "Enoch Owoeye";
-id = "HNG-00991";
-email = "enochowoeye001@gmail.com";
-Language= "Python"
-print('Hello World, this is '+name+' with HNGi7 ID '+id+' using '+Language+' for stage 2 task email '+email)
-
