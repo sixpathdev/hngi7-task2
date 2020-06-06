f0159f1b5302d46a0587028db5a1f616526e8c47
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2020-06-06 18:41:16 +0100

    Delete AlexScript.js

--- scripts/AlexScript.js
+++
@@ -1,7 +0,0 @@
-const email     = 'alexanderaugustine0@gmail.com';
-const fullname  = 'Alex Augustine';
-const hng_id    = 'HNG-00955';
-const language  = 'JavaScript';
-
-let output = 'Hello world, this is ' +fullname+ ' with HNGi7 ID '+hng_id+ ' using ' +language+ ' for the stage 2 task email '+email+;
-console.log(output)
