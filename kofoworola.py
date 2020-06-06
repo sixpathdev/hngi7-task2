a35d2250ca261a999c31a45241e4133739126c48
kofoworola08 <66370394+kofoworola08@users.noreply.github.com>
2020-06-06 18:44:59 +0100

    Create kofoworola.py

---
+++ kofoworola.py
@@ -0,0 +1,6 @@
+full_name = "kofoworola shadare"
+hng_id = "HNG-06440"
+email = "kofoworolashadare@gmail.com"
+language = "python"
+string = "Hello World, this is {} with HNGi7 ID {} using {} for stage 2 task email{}"
+print(string.fromat(email, full_name, hng_id, language))
