cf639e1ea4e119e7dca6d4dca665e05d66d97d5a
William Mwendwa <50356076+mwendwa-will@users.noreply.github.com>
2020-06-06 20:21:20 +0300

    Add files via upload
    edit

--- scripts/mwendwa_script.php
+++ scripts/mwendwa_script.php
@@ -7,7 +7,7 @@ $id = " HNG-06573";
 
 $language = "php";
 
-echo "Hello World, this is ".$name." with HNGi7 ID ".$id." using ".$language." for stage 2 task email ".$email."";
+echo "Hello World, this is ".$name." with HNGi7 ID ".$id." using ".$language." for stage 2 task email ".$email;
 
 
 
