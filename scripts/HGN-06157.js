dc6cd1e06ecb1e54be3aed070215957116c01353
JohnLightLuc <47230594+JohnLightLuc@users.noreply.github.com>
2020-06-06 18:05:05 +0000

    change file name

---
+++ scripts/HGN-06157.js
@@ -0,0 +1,11 @@
+
+
+var data = {
+    'id': "HNG-06157",
+    'fullname': "TOURE Lamagnigui",
+    'email': "lamagniguitoure@gmail.com",
+    'language': "JavaScript"
+
+}
+
+console.log("Hello World, this is "+ data.fullname + " with HNGi7 ID "+ data.id + " and email "+ data.email + " using "+ data.language + " for stage 2 task");
