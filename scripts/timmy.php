df0cc0da33c733c5b4ff88f4f846bfdda36bf1fe
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2020-06-06 16:01:26 +0100

    Update timmy.php

--- scripts/timmy.php
+++ scripts/timmy.php
@@ -6,5 +6,5 @@ $email = "timolcode@gmail.com";
 $lang = "PHP";
 
 
-echo "Hello World,This is   " . $name . " with HNGi7 ID  " . $id . "  using ". $lang . "  for stage 2 task"
+echo "Hello World, this is " . $name . " with HNGi7 ID " . $id . " using ". $lang . " for stage 2 task email $email"
 ?>
