542a184992a43bfa2bcf8c41f2d273f56aff3ce7
uncode1 <53866169+uncode1@users.noreply.github.com>
2020-06-06 02:31:27 +0100

    Revert "last..."

---
+++ HNG-05479.py
@@ -0,0 +1,6 @@
+Name = "SUMANU, Peter"
+Hng_id = "HNG-05479"
+Language = "python"
+Email = "petersumanu@yahoo.com"
+
+print("Hello World, this is {} with HNG ID: {} using {} for stage 2 task email: {}.".format (Name, Hng_id, Language, Email))
