c1d9585001c27f088d9826dae40ccd2e6e0411f3
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2020-06-06 23:29:35 +0100

    Update pizh.js

--- scripts/pizh.js
+++ scripts/pizh.js
@@ -3,4 +3,4 @@ const name = "Fisayo Samson";
 const HNG_ID = "HNG-04020";
 const language = "JavaScript";
 
-console.log (' Hello World, this is ' + name +' with HNGi7 ID ' + HNG_ID + ' using ' + language +' for Stage 2 task email ' + email);
+console.log ('Hello World, this is ' + name +' with HNGi7 ID ' + HNG_ID + ' using ' + language +' for Stage 2 task email ' + email);
