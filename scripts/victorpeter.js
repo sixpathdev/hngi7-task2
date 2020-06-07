57f282866b61a1ca1d5d62e74f01184f85458e5c
Victor Peter <ukokjnr@gmail.com>
2020-06-07 02:44:16 +0100

    Added missing varrable declarations and semi colons.

--- scripts/victorpeter.js
+++ scripts/victorpeter.js
@@ -1,6 +1,6 @@
-email = "ukokjnr@gmail.com"
-name = "Victor Ukok"
-id = "HNG-03805"
-language = "JavaScript"
+var email = "ukokjnr@gmail.com";
+var name = "Victor Ukok";
+var id = "HNG-03805";
+var language = "JavaScript";
 console.log(`Hello World, this is ${name} with HNGi7 ID ${id} using ${language} for stage 2 task email ${email}`);
 
