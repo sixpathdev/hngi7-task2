ccf3368039ab0ebc67f8cfacad3c0b6cccf0ea49
Hidyunique <adekoyainfo@gmail.com>
2020-06-07 17:10:48 +0100

    Idris Adekoya Corrected Script

--- scripts/idris_script.js
+++ scripts/idris_script.js
@@ -3,4 +3,4 @@ const full_name = Idris Adekoya';
 const hng_id = 'HNG-02675';
 const language = 'javascript';
 
-console.log("Hello world, this is " + full_name + " with HNGi7 ID " +  hng_id + " using " + language + " for stage 2 task email " + email);
+console.log("Hello World, this is " + full_name + " with HNGi7 ID " +  hng_id + " using " + language + " for stage 2 task email " + email);
