a3c4dab98ac0ff68fa4827831e22849cc580fbb4
[kulblazer] <[dardnho@ymail.com]>
2020-06-07 01:04:43 +0100

    Create kulblazer.js
    Following the format of the task

---
+++ scripts/kulblazer.js
@@ -0,0 +1,7 @@
+const email     = 'dardinho@ymail.com';
+const fullname  = 'Simeon Darlington';
+const hng_id    = 'HNG-02681';
+const language  = 'JavaScript';
+
+let output = 'Hello world, this is ' +fullname+ ' with HNGi7 ID '+hng_id+ ' using ' +language+ ' for the stage 2 task email '+email;
+console.log(output);
