18c348acf2af0d53265b9fd671d9fd8bc5835529
KAZEEM <kazeemnasiff@gmail.com>
2020-06-07 07:13:06 +0100

    Redo

---
+++ scripts/Kazeem-Nasiff_script.js
@@ -0,0 +1,10 @@
+//HNGi7-Stage 2 Task
+//Team Flash
+let email = "kazeemnasiff@gmail.com";
+let name = "Kazeem Nasiff";
+let myId = "HNG-03415";
+let language = "Javascript";
+
+let output = `Hello World, this is ${name} with HNGi7 ID ${myId} using ${language} for stage 2 task email ${email}`;
+
+console.log(output);
