7fe50182ed08b5cf435ff8bfe95b3b471f0d1435
Haqq <66303519+Haqq@users.noreply.github.com>
2020-06-07 07:26:06 +0100

    Haqq

---
+++ scripts/py.py
@@ -0,0 +1,8 @@
+#Hello World, this is [fullname] with [HNG-ID] using [language] for stage 2 task and [email]
+#defining attributes
+full_name="Abubakri Ibrahim"
+hng_id="HNG-05601"
+language="python"
+email="vectorvector49@gmail.com"
+#Inputing into the sentence
+print("Hello World, this is",full_name,"with",hng_id,"using",language,"for stage 2 task and",email)
