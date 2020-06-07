281e289bf7f8ac5b19a9c663871787f8860f260e
Agu Chiamaka Adline <aguadline@gmail.com>
2020-06-07 08:36:41 -0700

    my script file

--- scripts/Agu-script.js
+++ scripts/Agu-script.js
@@ -3,4 +3,4 @@ var id="HNG-0537";
 var language="JavaScript";
 var email="aguadline@gmail.com";
 
-console.log(`Hello World,this is ${name} with HNGi7 id ${id} using ${language} for stage 2 email ${email}`);
+console.log(`Hello World,this is ${name} with HNGi7 id ${id} using ${language} for stage 2 task email ${email}`);
