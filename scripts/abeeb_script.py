123c3c28f9a0a53b81f9efd2665d1641826c7b5a
Haybee96 <abeebayinla@gmail.com>
2020-06-07 09:44:48 +0100

    Initial commit: abeeb_script.py

---
+++ scripts/abeeb_script.py
@@ -0,0 +1,8 @@
+name = 'AYNLA ABEEB'
+user_id = 'HNG-05437'
+language = 'PYTHON'
+email = 'ABEEBAYINLA@GMAIL.COM'
+
+print('Hello World' + ', ' + 'this is ' + name +
+' with HNGi7 ID ' + user_id + ' using ' + language +
+' for stage 2 task email ' + email)
