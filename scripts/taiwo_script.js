bb0fc79dfce00089dc2d139e2f434e9f00f7bc5f
Taiwo Oyewole <oyewoletaiwo94@gmail.com>
2020-06-07 20:13:19 +0100

    Taiwo HNG task (updated)

--- scripts/taiwo_script.js
+++ scripts/taiwo_script.js
@@ -4,5 +4,5 @@ const task = {
     email_address: 'oyewoletaiwo94@gmail.com',
     language: 'JavaScript'
 };
-const output = `Hello World, this is ${task.name} with HNGi7 ID ${task.id} using ${task.language} for stage 2 task email${task.email_address}`;
+const output = `Hello World, this is ${task.name} with HNGi7 ID ${task.id} using ${task.language} for stage 2 task email ${task.email_address}`;
 console.log(output);
